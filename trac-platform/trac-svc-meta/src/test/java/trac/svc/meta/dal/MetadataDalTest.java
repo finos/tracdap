c428f9c6ffa64872a3e5dc0576b689329d531fda
Martin Traverse <martin.traverse@accenture.com>
2019-11-09 19:10:37 +0000

    Working unit test to store and retrieve one item via JDBC metadata DAL

--- trac-platform/trac-svc-meta/src/test/java/trac/svc/meta/dal/MetadataDalTest.java
+++ trac-platform/trac-svc-meta/src/test/java/trac/svc/meta/dal/MetadataDalTest.java
@@ -1,78 +1,104 @@
 package trac.svc.meta.dal;
 
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
 import trac.common.metadata.*;
+import trac.svc.meta.exception.DuplicateItemError;
 import trac.svc.meta.dal.jdbc.JdbcMetadataDal;
 
+import com.google.common.io.Resources;
+
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Scanner;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 
-import org.junit.Rule;
-import org.junit.rules.ExternalResource;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.io.TempDir;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import com.mysql.cj.jdbc.MysqlDataSource;
-import ch.vorburger.mariadb4j.junit.MariaDB4jRule;
+import ch.vorburger.mariadb4j.DB;
 import ch.vorburger.mariadb4j.DBConfigurationBuilder;
 import ch.vorburger.mariadb4j.DBConfiguration;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
 
 class MetadataDalTest {
 
+    private static final String TEST_TENANT = "ACME_CORP";
+
+    private static DBConfiguration dbConfig;
+    private static DB db = null;
+
     private IMetadataDal dal;
 
-    private MariaDB4jRule databaseRule = new MariaDB4jRule(
-            DBConfigurationBuilder.newBuilder().build(),
-            "trac_test",
-            "sql/mysql/trac_metadata.ddl");
+    @BeforeAll
+    static void setupDb(@TempDir Path tempDir) throws Exception {
 
-    private DalSetupRule dalRule = new DalSetupRule(
-            databaseRule,
-            x -> this.dal = x);
+        var inputStream = MetadataDalTest.class.getResourceAsStream("/mysql/trac_metadata.ddl");
+        var scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name()).useDelimiter("\\A");
+        var deployScript = scanner.next();
 
-    @Rule
-    TestRule ordering = RuleChain
-            .outerRule(databaseRule)
-            .around(dalRule);
+        dbConfig = DBConfigurationBuilder.newBuilder()
+                .setPort(0)
+                .setDataDir(tempDir.toString())
+                .build();
 
-    static class DalSetupRule extends ExternalResource {
+        db = DB.newEmbeddedDB(dbConfig);
+        db.start();
 
-        Supplier<DBConfiguration> dbConfig;
-        Consumer<IMetadataDal> setter;
+        var source = new MysqlDataSource();
+        source.setServerName("localhost");
+        source.setPort(dbConfig.getPort());
 
-        DalSetupRule(MariaDB4jRule databaseRule, Consumer<IMetadataDal> setter) {
-            this.dbConfig = databaseRule::getDBConfiguration;
-            this.setter = setter;
-        }
+        try (var conn = source.getConnection(); var stmt = conn.createStatement()) {
 
-        @Override
-        protected void before() throws Throwable {
+            System.out.println("SQL >>> Deploying database schema");
 
-            MysqlDataSource source = new MysqlDataSource();
-            source.setServerName("localhost");
-            source.setPort(dbConfig.get().getPort());
-            source.setDatabaseName("trac_test");
+            stmt.execute("create database trac_test");
+            stmt.execute("use trac_test");
 
-            Executor executor = Runnable::run;
+            for (var deployCommand : deployScript.split(";"))
+                if (!deployCommand.isBlank()) {
+                    System.out.println("SQL >>>\n\n" + deployCommand.strip() + "\n");
+                    stmt.execute(deployCommand);
+                }
 
-            JdbcMetadataDal dal = new JdbcMetadataDal(source, executor);
-            setter.accept(dal);
+            stmt.execute(String.format("insert into tenant (tenant_id, tenant_code) values (1, '%s')", TEST_TENANT));
         }
     }
 
-    @Test
-    void testSaveNewObject_ok() throws Exception {
+    @AfterAll
+    static void teardownDb() throws Exception {
 
-        DataDefinition origDef = DataDefinition.newBuilder()
+        if (db != null)
+            db.stop();
+    }
+
+    @BeforeEach
+    void setupDal() throws Exception {
+
+        var source = new MysqlDataSource();
+        source.setServerName("localhost");
+        source.setPort(dbConfig.getPort());
+        source.setDatabaseName("trac_test");
+
+        var dal = new JdbcMetadataDal(source, Runnable::run);
+        dal.startup();
+
+        this.dal = dal;
+    }
+
+    private DataDefinition dummyDataDef() {
+
+        return DataDefinition.newBuilder()
                 .setHeader(ObjectHeader.newBuilder()
                         .setObjectType(ObjectType.DATA)
-                        .setId("")
+                        .setId(MetadataCodec.encode(UUID.randomUUID()))
                         .setVersion(1))
                 .addStorage("test-storage")
                 .setPath("path/to/test/dataset")
@@ -99,139 +125,162 @@ class MetadataDalTest {
                                 .setFieldOrder(4)
                                 .setBusinessKey(true)))
                 .build();
+    }
 
-        Tag origTag = Tag.newBuilder()
-                .setDataDefinition(origDef)
+    Tag dummyTag(DataDefinition dataDef) {
+
+        return Tag.newBuilder()
+                .setHeader(dataDef.getHeader())
                 .setTagVersion(1)
+                .setDataDefinition(dataDef)
                 .putAttrs("dataset_key", PrimitiveValue.newBuilder()
                         .setType(PrimitiveType.STRING)
                         .setStringValue("widget_orders")
                         .build())
-                .putAttrs("widget_class", PrimitiveValue.newBuilder()
+                .putAttrs("widget_type", PrimitiveValue.newBuilder()
                         .setType(PrimitiveType.STRING)
                         .setStringValue("non_standard_widget")
                         .build())
                 .build();
+    }
 
-        CompletableFuture<Tag> result = CompletableFuture.completedFuture(null)
-                .thenCompose(x -> dal.saveNewObject("test_tenant", origTag))
-                .thenCompose(x -> dal.loadTag("test_tenant", UUID.randomUUID(), 1, 1));
+    @Test
+    void testSaveNewObject_ok() throws Exception {
 
-        assertEquals(origTag, result.get());
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+        var origId = MetadataCodec.decode(origDef.getHeader().getId());
+
+        var future = CompletableFuture.completedFuture(0);
+        var fut2 = future.thenCompose(x -> dal.saveNewObject(TEST_TENANT, origTag));
+        var fut3 = fut2.thenCompose(x -> {
+            var t = dal.loadTag(TEST_TENANT, origId, 1, 1);
+            return t;
+        });
+
+        assertEquals(origTag, fut3.get());
     }
 
     @Test
     void testSaveNewObject_duplicate() {
 
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+
+        var future = CompletableFuture.completedFuture(0)
+                .thenCompose(x -> dal.saveNewObject(TEST_TENANT, origTag))
+                .thenCompose(x -> dal.saveNewObject(TEST_TENANT, origTag));
+
+        assertThrows(DuplicateItemError.class, future::get);
     }
 
     @Test
     void testSaveNewVersion_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewVersion_duplicate() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewVersion_missingObject() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewVersion_wrongObjectType() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewTag_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewTag_duplicate() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewTag_missingObject() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testSaveNewTag_missingVersion() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testPreallocate_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testPreallocate_duplicate() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testPreallocate_missingObjectId() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testPreallocate_wrongObjectType() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadOneExplicit_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadOneLatestVersion_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadOneLatestTag_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadOne_missingItems() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadOne_wrongObjectType() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadBatchExplicit_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadBatchLatestVersion_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadBatchLatestTag_ok() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadBatch_missingItems() {
-
+        fail("Not implemented");
     }
 
     @Test
     void testLoadBatch_wrongObjectType() {
-
+        fail("Not implemented");
     }
 }
