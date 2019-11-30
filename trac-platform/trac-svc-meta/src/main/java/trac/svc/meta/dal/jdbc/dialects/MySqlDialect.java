0290a41af4fc29195db27921675e6f6a62de7171
Martin Traverse <martin.traverse@accenture.com>
2019-11-30 18:06:48 +0000

    Check for synthetic errors in MySQL dialect

--- trac-platform/trac-svc-meta/src/main/java/trac/svc/meta/dal/jdbc/dialects/MySqlDialect.java
+++ trac-platform/trac-svc-meta/src/main/java/trac/svc/meta/dal/jdbc/dialects/MySqlDialect.java
@@ -2,6 +2,7 @@ package trac.svc.meta.dal.jdbc.dialects;
 
 import trac.svc.meta.dal.jdbc.JdbcDialect;
 import trac.svc.meta.dal.jdbc.JdbcErrorCode;
+import trac.svc.meta.dal.jdbc.JdbcException;
 
 import java.sql.SQLException;
 import java.util.HashMap;
@@ -11,12 +12,17 @@ import java.util.Map;
 public class MySqlDialect implements IDialect {
 
     private Map<Integer, JdbcErrorCode> errorCodes;
-
+    private Map<Integer, JdbcErrorCode> syntheticErrorCodes;
 
     MySqlDialect() {
 
         errorCodes = new HashMap<>();
         errorCodes.put(1062, JdbcErrorCode.INSERT_DUPLICATE);
+
+        syntheticErrorCodes = new HashMap<>();
+
+        for (var error : JdbcErrorCode.values())
+            syntheticErrorCodes.put(error.ordinal(), error);
     }
 
     @Override
@@ -27,6 +33,9 @@ public class MySqlDialect implements IDialect {
     @Override
     public JdbcErrorCode mapErrorCode(SQLException error) {
 
-        return errorCodes.getOrDefault(error.getErrorCode(), JdbcErrorCode.UNKNOWN_ERROR_CODE);
+        if (JdbcException.SYNTHETIC_ERROR.equals(error.getSQLState()))
+            return syntheticErrorCodes.getOrDefault(error.getErrorCode(), JdbcErrorCode.UNKNOWN_ERROR_CODE);
+        else
+            return errorCodes.getOrDefault(error.getErrorCode(), JdbcErrorCode.UNKNOWN_ERROR_CODE);
     }
 }
