e605740dbc3a8673bd05368bd2e71c71d0c31545
Martin Traverse <martin.traverse@accenture.com>
2019-12-03 21:50:48 +0000

    DAL - tests for saveNewTag + variants

--- trac-platform/trac-svc-meta/src/test/java/trac/svc/meta/dal/MetadataDalTest.java
+++ trac-platform/trac-svc-meta/src/test/java/trac/svc/meta/dal/MetadataDalTest.java
@@ -229,6 +229,17 @@ class MetadataDalTest {
                 .build();
     }
 
+    Tag nextTag(Tag previous) {
+
+        return previous.toBuilder()
+                .setTagVersion(previous.getTagVersion() + 1)
+                .putAttr("extra_attr", PrimitiveValue.newBuilder()
+                        .setType(PrimitiveType.STRING)
+                        .setStringValue("A new descriptive value")
+                        .build())
+                .build();
+    }
+
     Tag dummyTagForObjectType(ObjectType objectType) {
 
         if (objectType == ObjectType.DATA)
@@ -402,7 +413,7 @@ class MetadataDalTest {
 
         // First insert should succeed if they are run one by one
         assertThrows(DuplicateItemError.class, () -> unwrap(saveDup2));
-        assertDoesNotThrow(() -> unwrap(loadDup2));
+        assertEquals(nextTag, unwrap(loadDup2));
     }
 
     @Test
@@ -414,7 +425,7 @@ class MetadataDalTest {
         var nextTag = dummyTag(nextDef);
 
         // Save next version, single, without saving original
-        var saveNext =  dal.saveNewVersions(TEST_TENANT, Arrays.asList(nextTag, nextTag));
+        var saveNext =  dal.saveNewVersion(TEST_TENANT, nextTag);
         assertThrows(MissingItemError.class, () -> unwrap(saveNext));
 
         var modelTag = dummyTagForObjectType(ObjectType.MODEL);
@@ -454,23 +465,126 @@ class MetadataDalTest {
     }
 
     @Test
-    void testSaveNewTag_ok() {
-        fail("Not implemented");
+    void testSaveNewTag_ok() throws Exception {
+
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+        var nextDefTag1 = dummyTag(nextDataDef(origDef));
+        var nextDefTag2 = nextTag(origTag);
+        var origId = MetadataCodec.decode(origDef.getHeader().getId());
+
+        // Test saving tag v2 against object v2
+        var future = CompletableFuture.completedFuture(0)
+                .thenCompose(x -> dal.saveNewObject(TEST_TENANT, origTag))
+                .thenCompose(x -> dal.saveNewVersion(TEST_TENANT, nextDefTag1))
+                .thenCompose(x -> dal.saveNewTag(TEST_TENANT, nextDefTag2))
+                .thenCompose(x -> dal.loadTag(TEST_TENANT, origId, 2, 2));
+
+        assertEquals(nextDefTag2, unwrap(future));
+
+        // Save multiple - this test saves tag v2 against object v1
+        var multi1 = dummyTagForObjectType(ObjectType.DATA);
+        var multi2 = dummyTagForObjectType(ObjectType.MODEL);
+
+        var multi1v2 = nextTag(multi1);
+        var multi2v2 = nextTag(multi2);
+
+        var future2 = CompletableFuture.completedFuture(0)
+                .thenCompose(x -> dal.saveNewObjects(TEST_TENANT, Arrays.asList(multi1, multi2)))
+                .thenCompose(x -> dal.saveNewTags(TEST_TENANT, Arrays.asList(multi1v2, multi2v2)));
+
+        assertDoesNotThrow((ThrowingSupplier<Void>) future2::get);
+
+        var result1 = dal.loadTag(TEST_TENANT, MetadataCodec.decode(multi1.getHeader().getId()), 1, 2);
+        var result2 = dal.loadTag(TEST_TENANT, MetadataCodec.decode(multi2.getHeader().getId()), 1, 2);
+
+        assertEquals(multi1v2, unwrap(result1));
+        assertEquals(multi2v2, unwrap(result2));
     }
 
     @Test
-    void testSaveNewTag_duplicate() {
-        fail("Not implemented");
+    void testSaveNewTag_duplicate() throws Exception {
+
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+        var nextTag = nextTag(origTag);
+        var origId = MetadataCodec.decode(origDef.getHeader().getId());
+
+        var saveOrig = dal.saveNewObject(TEST_TENANT, origTag);
+        var saveDup =  dal.saveNewTags(TEST_TENANT, Arrays.asList(nextTag, nextTag));
+        var loadDup = dal.loadTag(TEST_TENANT, origId, 1, 2);
+
+        unwrap(saveOrig);
+        assertThrows(DuplicateItemError.class, () -> unwrap(saveDup));
+        assertThrows(MissingItemError.class, () -> unwrap(loadDup));
+
+        var saveDup2 = CompletableFuture.completedFuture(0)
+                .thenCompose(x -> dal.saveNewVersion(TEST_TENANT, nextTag))
+                .thenCompose(x -> dal.saveNewVersion(TEST_TENANT, nextTag));
+
+        var loadDup2 = dal.loadTag(TEST_TENANT, origId, 1, 2);
+
+        // First insert should succeed if they are run one by one
+        assertThrows(DuplicateItemError.class, () -> unwrap(saveDup2));
+        assertEquals(nextTag, unwrap(loadDup2));
     }
 
     @Test
-    void testSaveNewTag_missingObject() {
-        fail("Not implemented");
+    void testSaveNewTag_missingObject() throws Exception {
+
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+        var nextTag = nextTag(origTag);
+
+        // Save next tag, single, without saving object
+        var saveNext =  dal.saveNewTag(TEST_TENANT, nextTag);
+        assertThrows(MissingItemError.class, () -> unwrap(saveNext));
+
+        var modelTag = dummyTagForObjectType(ObjectType.MODEL);
+        var nextModelTag = dummyTag(nextModelDef(modelTag.getModelDefinition()));
+
+        // Save next tag, multiple, one item does not have an object
+        var saveOrig = dal.saveNewObject(TEST_TENANT, origTag);
+        var saveNextMulti = dal.saveNewTags(TEST_TENANT, Arrays.asList(nextTag, nextModelTag));
+
+        unwrap(saveOrig);
+        assertThrows(MissingItemError.class, () -> unwrap(saveNextMulti));
     }
 
     @Test
-    void testSaveNewTag_missingVersion() {
-        fail("Not implemented");
+    void testSaveNewTag_missingVersion() throws Exception {
+
+        var origDef = dummyDataDef();
+        var origTag = dummyTag(origDef);
+        var nextDef = nextDataDef(origDef);
+        var nextDefTag1 = dummyTag(nextDef);
+        var nextDefTag2 = nextTag(nextDefTag1);
+
+        // Save next tag (single) on an unknown object version
+        // This is an error, even for tag v1
+        // saveNewVersion must be called first
+        var saveObj = dal.saveNewObject(TEST_TENANT, origTag);
+        var saveNext1 =  dal.saveNewTag(TEST_TENANT, nextDefTag1);
+        var saveNext2 =  dal.saveNewTag(TEST_TENANT, nextDefTag2);
+
+        unwrap(saveObj);
+        assertThrows(MissingItemError.class, () -> unwrap(saveNext1));
+        assertThrows(MissingItemError.class, () -> unwrap(saveNext2));
+
+        var modelTag = dummyTagForObjectType(ObjectType.MODEL);
+        var nextModelTag = dummyTag(nextModelDef(modelTag.getModelDefinition()));
+        var nextModelTag2 = nextTag(nextModelTag);
+
+        // Save object 1 version 2, and object 2 version 1
+        unwrap(dal.saveNewVersion(TEST_TENANT, nextDefTag1));
+        unwrap(dal.saveNewObject(TEST_TENANT, origTag));
+
+        // Save next tag (multiple), second item is missing the required object version
+        var saveNextMulti = dal.saveNewTags(TEST_TENANT, Arrays.asList(nextDefTag2, nextModelTag2));
+        assertThrows(MissingItemError.class, () -> unwrap(saveNextMulti));
+
+        // Saving the valid tag by itself should not throw
+        assertDoesNotThrow(() -> dal.saveNewTag(TEST_TENANT, nextDefTag2));
     }
 
     @Test
