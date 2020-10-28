d743c15882b11d6756d2ec472fdd6268c385c33e
Martin Traverse <martin.traverse@gmail.com>
2020-10-28 18:13:26 +0000

    Test stubs for tag update service

---
+++ trac-services/trac-svc-meta/src/test/java/com/accenture/trac/svc/meta/services/TagUpdateServiceTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2020 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.accenture.trac.svc.meta.services;
+
+import org.junit.Test;
+
+
+public class TagUpdateServiceTest {
+
+
+    // Exhaustive tests of individual operations
+
+    void createAttr_ok() {
+
+    }
+
+    void createAttr_alreadyExists() {
+
+    }
+
+    void replaceAttr_ok() {
+
+    }
+
+    void replaceAttr_doesNotExist() {
+
+    }
+
+    void replaceAttr_wrongType() {
+
+    }
+
+    void replaceAttr_singleToMulti() {
+
+    }
+
+    void appendAttr_ok() {
+
+    }
+
+    void appendAttr_doesNotExist() {
+
+    }
+
+    void appendAttr_wrongType() {
+
+    }
+
+    void deleteAttr_ok() {
+
+    }
+
+    void deleteAttr_doesNotExist() {
+
+    }
+
+    void clearAllAttr() {
+
+    }
+
+
+    // Update sequencing tests
+
+    @Test
+    void sequencing_ok() {
+
+    }
+
+    @Test
+    void sequencing_deleteRecreate() {
+
+    }
+
+    @Test
+    void sequencing_duplicateCreateFails() {
+
+    }
+
+    @Test
+    void sequencing_appendMultiple() {
+
+    }
+
+
+    // Invalid updates
+
+    @Test
+    void invalidUpdate_badOperation() {
+
+    }
+
+    @Test
+    void invalidUpdate_badAttrName() {
+
+    }
+
+    @Test
+    void invalidUpdate_badValue() {
+
+    }
+}
