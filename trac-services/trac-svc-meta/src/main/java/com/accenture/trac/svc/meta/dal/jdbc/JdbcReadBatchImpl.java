f3f00bb38a43f1063bfb80e2e2467f6f855d6d9c
Martin Traverse <martin.traverse@accenture.com>
2020-11-05 18:58:06 +0000

    Fix boolean type for Oracle dialect in read batch

--- trac-services/trac-svc-meta/src/main/java/com/accenture/trac/svc/meta/dal/jdbc/JdbcReadBatchImpl.java
+++ trac-services/trac-svc-meta/src/main/java/com/accenture/trac/svc/meta/dal/jdbc/JdbcReadBatchImpl.java
@@ -623,7 +623,7 @@ class JdbcReadBatchImpl {
                 stmt.setLong(1, objectFk[i]);
                 stmt.setNull(2, Types.INTEGER);
                 stmt.setNull(3, Types.TIMESTAMP);
-                stmt.setNull(4, Types.BOOLEAN);
+                stmt.setNull(4, dialect.booleanType());
 
                 switch (criteria) {
 
@@ -707,7 +707,7 @@ class JdbcReadBatchImpl {
                 stmt.setLong(1, definitionFk[i]);
                 stmt.setNull(2, Types.INTEGER);
                 stmt.setNull(3, Types.TIMESTAMP);
-                stmt.setNull(4, Types.BOOLEAN);
+                stmt.setNull(4, dialect.booleanType());
 
                 switch (criteria) {
 
