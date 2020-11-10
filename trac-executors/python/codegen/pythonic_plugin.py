5c668fbe0f82a8d1e092f45868b698378528ba20
Martin Traverse <martin.traverse@accenture.com>
2020-11-10 20:18:56 +0000

    Generate a valid package tree for Pythonic metadata classes (includes __init__ for empty parent packages)

--- trac-executors/python/codegen/pythonic_plugin.py
+++ trac-executors/python/codegen/pythonic_plugin.py
@@ -54,7 +54,32 @@ class PythonicPlugin:
             package_files = generator.generate_package(package, files)
             generated_response.file.extend(package_files)
 
+        # Generate package-level files for empty packages
+        # (this creates a valid package tree from the root package)
+
+        proto_packages = set(map(lambda f: f.package, self._request.proto_file))
+        all_packages = self.expand_parent_packages(proto_packages)
+
+        for package in all_packages:
+            if package not in proto_packages:
+                package_files = generator.generate_package(package, [])
+                generated_response.file.extend(package_files)
+
         return generated_response
+
+    def expand_parent_packages(self, packages):
+
+        all_packages = set()
+
+        for package in packages:
+
+            package_path = package.split(".")
+
+            for i in range(len(package_path)):
+                parent_package = ".".join(package_path[:i+1])
+                all_packages.add(parent_package)
+
+        return all_packages
 
 
 if __name__ == "__main__":
