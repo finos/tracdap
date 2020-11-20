90f5740f935f4c73ea7e783b137f57e46a3e877d
Martin Traverse <martin.traverse@accenture.com>
2020-11-20 19:29:18 +0000

    Update comments in trac.rt.metadata placeholder file

--- trac-runtime/python/src/trac/rt/metadata/__init__.py
+++ trac-runtime/python/src/trac/rt/metadata/__init__.py
@@ -11,11 +11,23 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
 
 
 # Code gen creates outputs under artificial locations:
 #
-#   trac_gen.domain.trac.metadata - TRAC metadata domain objects for use in the API
+#   trac_gen.domain.trac.metadata - TRAC domain objects for the runtime API
 #   trac_gen.proto.trac.metadata - Output of Google's native protoc for Python
 #
 # The generator respects the package layout of the source .proto files, however
@@ -24,8 +36,8 @@
 # around this by copying the output of the generator under the correct locations
 # in the package, which will be as follows:
 #
-#   trac_gen.domain.trac.metadata -> trac.metadata
-#   trac_gen.proto.trac.metadata -> trac.impl.proto
+#   trac_gen.domain.trac.metadata -> trac.rt.metadata
+#   trac_gen.proto.trac.metadata -> trac.rt.proto
 #
 # This proxy import means the API will work as expected during development
 # without needing to run packaging jobs.
