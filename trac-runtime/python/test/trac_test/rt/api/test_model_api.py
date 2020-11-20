fa5aae7a1e027c8bbccc152ab85818b8f686f6a7
Martin Traverse <martin.traverse@accenture.com>
2020-11-20 19:03:38 +0000

    Move runtime tests under .rt namespace

--- trac-runtime/python/test/trac_test/rt/api/test_model_api.py
+++ trac-runtime/python/test/trac_test/rt/api/test_model_api.py
@@ -12,7 +12,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import trac.api as trac
+import trac.rt.api as trac
 import typing as tp
 
 import unittest
