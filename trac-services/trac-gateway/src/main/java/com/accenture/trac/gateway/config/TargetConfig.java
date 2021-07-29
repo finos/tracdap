2b65cfb60fd3f1c375e830edf91b767d07385c20
Martin Traverse <martin.traverse@accenture.com>
2021-07-29 17:34:41 +0100

    Classes to describe config for the gateway

---
+++ trac-services/trac-gateway/src/main/java/com/accenture/trac/gateway/config/TargetConfig.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.accenture.trac.gateway.config;
+
+public class TargetConfig {
+
+    private String host;
+    private short port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public short getPort() {
+        return port;
+    }
+
+    public void setPort(short port) {
+        this.port = port;
+    }
+}
