1d34e474564127f3476fa9af1525cb533cb7caed
Martin Traverse <martin.traverse@accenture.com>
2021-09-04 18:13:50 +0100

    Use deferred annotations in runtime API modules

--- trac-runtime/python/src/trac/rt/api/core_api.py
+++ trac-runtime/python/src/trac/rt/api/core_api.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import annotations
+
 import abc as _abc
 import typing as _tp
 import logging as _logging
--- trac-runtime/python/src/trac/rt/api/define_func.py
+++ trac-runtime/python/src/trac/rt/api/define_func.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+from __future__ import annotations
+
 import typing as _tp
 import dataclasses as _dc
 import inspect as _inspect
