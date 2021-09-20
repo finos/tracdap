23d49ba2e6c05f98ee49c09a871b3abd52206fac
Martin Traverse <martin.traverse@accenture.com>
2021-09-20 16:21:03 +0100

    Move storage interface into data lib

--- trac-libs/trac-lib-data/src/main/java/com/accenture/trac/common/storage/FileStat.java
+++ trac-libs/trac-lib-data/src/main/java/com/accenture/trac/common/storage/FileStat.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.accenture.trac.svc.data.core;
+package com.accenture.trac.common.storage;
 
 import java.time.Instant;
 import java.util.Optional;
