0c39ab08a785d4f6fe0cd72c689d93f4dcb66af2
Martin Traverse <martin.traverse@accenture.com>
2021-10-01 00:39:56 +0100

    Add a series of round trip tests at the Data API level for file read/write

--- trac-services/trac-svc-data/src/test/java/com/accenture/trac/svc/data/api/DataReadApiTest.java
+++ trac-services/trac-svc-data/src/test/java/com/accenture/trac/svc/data/api/DataReadApiTest.java
@@ -18,18 +18,22 @@ package com.accenture.trac.svc.data.api;
 
 import com.accenture.trac.api.*;
 import com.accenture.trac.common.eventloop.ExecutionRegister;
+import com.accenture.trac.common.eventloop.IExecutionContext;
 import com.accenture.trac.common.storage.StorageManager;
 import com.accenture.trac.common.util.Concurrent;
 import com.accenture.trac.common.util.GrpcStreams;
+import com.accenture.trac.metadata.ObjectDefinition;
 import com.accenture.trac.metadata.ObjectType;
-import com.accenture.trac.metadata.TagHeader;
+import com.accenture.trac.metadata.TagSelector;
 import com.accenture.trac.svc.data.service.DataReadService;
 import com.accenture.trac.svc.data.service.DataWriteService;
 
+import com.google.common.collect.Streams;
 import com.google.protobuf.ByteString;
 import io.grpc.inprocess.InProcessChannelBuilder;
 import io.grpc.inprocess.InProcessServerBuilder;
 import io.grpc.netty.NettyChannelBuilder;
+import io.grpc.stub.StreamObserver;
 import io.grpc.testing.GrpcCleanupRule;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -38,17 +42,32 @@ import io.netty.util.concurrent.DefaultThreadFactory;
 import org.junit.Rule;
 import org.junit.jupiter.api.*;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Flow;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.accenture.trac.common.metadata.MetadataUtil.selectorFor;
+import static com.accenture.trac.test.concurrent.ConcurrentTestHelpers.*;
+import static com.accenture.trac.test.storage.StorageTestHelpers.readFile;
 
-@Disabled
+
 public class DataReadApiTest {
+
+    private static final String TEST_TENANT = "ACME_CORP";
+    private static final Duration TEST_TIMEOUT = Duration.ofSeconds(10);
 
     @Rule
     public final GrpcCleanupRule grpcCleanup = new GrpcCleanupRule();
 
     private static StorageManager storage;
+    private IExecutionContext execContext;
 
     private TracMetadataApiGrpc.TracMetadataApiBlockingStub metaApi;
 
@@ -101,49 +120,228 @@ public class DataReadApiTest {
     }
 
     @Test
-    public void test1() {
+    void testRoundTrip_basic() throws Exception {
 
-        var request = FileReadRequest.newBuilder().build();
-        var responseStream = dataApi.readFile(request);
+        var random = new Random();
+        var content = List.of(
+                new byte[4096],
+                new byte[4096],
+                new byte[4096]);
 
-        var fileContent = ByteString.EMPTY;
+        for (var buf: content)
+            random.nextBytes(buf);
 
-        while (responseStream.hasNext()) {
-
-            var responseMsg = responseStream.next();
-
-            fileContent = fileContent.concat(responseMsg.getContent());
-        }
-
-        Assertions.assertEquals("Hello World!", fileContent.toStringUtf8());
+        roundTripTest(content, true);
+        roundTripTest(content, false);
     }
 
     @Test
-    public void test2() throws Exception {
+    void testRoundTrip_heterogeneousChunks() throws Exception {
 
-        var content = ByteString.copyFromUtf8("Hello world!\n");
-        var writeRequest = FileWriteRequest.newBuilder()
-                .setName("test_file.txt")
-                .setExtension("txt")
-                .setMimeType("text/plain")
-                .setContent(content)
+        var random = new Random();
+        var content = List.of(
+                new byte[3],
+                new byte[10000],
+                new byte[42],
+                new byte[4097],
+                new byte[1],
+                new byte[2000]);
+
+        for (var buf: content)
+            random.nextBytes(buf);
+
+        roundTripTest(content, true);
+        roundTripTest(content, false);
+    }
+
+    @Test
+    void testRoundTrip_megabyteChunk() throws Exception {
+
+        var content = List.of(new byte[1024 * 1024]);
+
+        var random = new Random();
+        random.nextBytes(content.get(0));
+
+        roundTripTest(content, true);
+        roundTripTest(content, false);
+    }
+
+    @Test
+    void testRoundTrip_singleByte() throws Exception {
+
+        var content = List.of(new byte[1]);
+        content.get(0)[0] = 0;
+
+        roundTripTest(content, true);
+        roundTripTest(content, false);
+    }
+
+    @Test
+    void testRoundTrip_smallTextFile() throws Exception {
+
+        var contentText = "Hello world!\n";
+        var contentBytes = contentText.getBytes(StandardCharsets.UTF_8);
+        var content = List.of(contentBytes);
+
+        roundTripTest(content, true);
+        roundTripTest(content, false);
+    }
+
+    private void roundTripTest(List<byte[]> content, boolean dataInChunkZero) throws Exception {
+
+        // Set up a request stream and client streaming call, wait for the call to complete
+
+        var createFileRequest = fileWriteRequest(content, dataInChunkZero);
+        var createFile = clientStreaming(dataApiStreams::createFile, createFileRequest);
+
+        waitFor(TEST_TIMEOUT, createFile);
+        var objHeader = resultOf(createFile);
+
+        // Fetch metadata for the file and storage objects that should be created
+
+        var fileDef = fetchDefinition(TEST_TENANT, selectorFor(objHeader), ObjectDefinition::getFile);
+        var storageDef = fetchDefinition(TEST_TENANT, fileDef.getStorageId(), ObjectDefinition::getStorage);
+
+        var dataItem = fileDef.getDataItem();
+        var storageItem = storageDef.getDataItemsOrThrow(dataItem);
+        var incarnation = storageItem.getIncarnations(0);
+        var copy = incarnation.getCopies(0);
+
+        Assertions.assertEquals(ObjectType.FILE, objHeader.getObjectType());
+        Assertions.assertEquals(1, objHeader.getObjectVersion());
+        Assertions.assertEquals(1, objHeader.getTagVersion());
+
+        // TODO: More assert checks on stored metadata
+
+        // Use storage impl directly to check file has arrived in the storage back end
+
+        var storageImpl = storage.getFileStorage(copy.getStorageKey());
+        var exists = storageImpl.exists(copy.getStoragePath());
+        var size = storageImpl.size(copy.getStoragePath());
+        var storedContent = readFile(copy.getStoragePath(), storageImpl, execContext);
+
+        waitFor(TEST_TIMEOUT, exists, size, storedContent);
+
+        var originalSize = content.stream()
+                .mapToLong(b -> b.length)
+                .sum();
+
+        var originalBytes = ByteString.copyFrom(
+            content.stream()
+            .map(ByteString::copyFrom)
+            .collect(Collectors.toList()));
+
+        var storedBytes = ByteString.copyFrom(
+            resultOf(storedContent).nioBuffer());
+
+        Assertions.assertTrue(resultOf(exists));
+        Assertions.assertEquals(originalSize, resultOf(size));
+        Assertions.assertEquals(originalBytes, storedBytes);
+
+        resultOf(storedContent).release();
+
+        // Set up a server-streaming request to read the file back
+
+        var readRequest = FileReadRequest.newBuilder()
+                .setTenant(TEST_TENANT)
+                .setSelector(selectorFor(objHeader))
                 .build();
 
-        var messages = Stream.of(writeRequest);
-        var requestStream = Concurrent.publish(messages);
-        var response = new CompletableFuture<TagHeader>();
+        var readResponse = Concurrent.<FileReadResponse>hub();
+        var readResponse0 = Concurrent.first(readResponse);
+        var readByteStream = Concurrent.map(readResponse, FileReadResponse::getContent);
+        var readBytes = Concurrent.fold(readByteStream, ByteString::concat, ByteString.EMPTY);
 
-        var responseGrpc = GrpcStreams.resultObserver(response);
-        var requestGrpc = dataApiStreams.createFile(responseGrpc);
-        requestStream.subscribe(GrpcStreams.relay(requestGrpc));
+        serverStreaming(dataApiStreams::readFile, readRequest, readResponse);
 
-        var result = response.get();
-        Assertions.assertNotNull(result);
-        Assertions.assertEquals(ObjectType.FILE, result.getObjectType());
-        Assertions.assertNotNull(result.getObjectId());
-        Assertions.assertEquals(1, result.getObjectVersion());
-        Assertions.assertEquals(1, result.getTagVersion());
+        waitFor(TEST_TIMEOUT, readResponse0, readBytes);
+        var roundTripTag = resultOf(readResponse0).getFileTag();
+        var roundTripBytes = resultOf(readBytes);
 
-        // TODO: Read metadata and confirm name, ext, type, size
+        // TODO: Compare tag / definition
+
+        Assertions.assertEquals(originalBytes, roundTripBytes);
+    }
+
+    private Flow.Publisher<FileWriteRequest>
+    fileWriteRequest(List<byte[]> content, boolean dataInChunkZero) {
+
+        var chunkZeroBytes = dataInChunkZero
+                ? ByteString.copyFrom(content.get(0))
+                : ByteString.EMPTY;
+
+        var requestZero = FileWriteRequest.newBuilder()
+                .setTenant(TEST_TENANT)
+                .setName("test_file.dat")
+                .setExtension("dat")
+                .setMimeType("application/octet-stream")
+                .setContent(chunkZeroBytes)
+                .build();
+
+        var remainingContent = dataInChunkZero
+                ? content.subList(1, content.size())
+                : content;
+
+        var requestStream = remainingContent.stream().map(bytes ->
+                FileWriteRequest.newBuilder()
+                .setContent(ByteString.copyFrom(bytes))
+                .build());
+
+        return Concurrent.publish(Streams.concat(
+                Stream.of(requestZero),
+                requestStream));
+    }
+
+    private <TReq, TResp>
+    Flow.Publisher<TResp> serverStreaming(
+            BiConsumer<TReq, StreamObserver<TResp>> grpcMethod,
+            TReq request) {
+
+        var response = Concurrent.<TResp>hub();
+        var responseGrpc = GrpcStreams.relay(response);
+
+        grpcMethod.accept(request, responseGrpc);
+
+        return response;
+    }
+
+    private <TReq, TResp>
+    void serverStreaming(
+            BiConsumer<TReq, StreamObserver<TResp>> grpcMethod,
+            TReq request, Flow.Subscriber<TResp> response) {
+
+        var responseGrpc = GrpcStreams.relay(response);
+        grpcMethod.accept(request, responseGrpc);
+    }
+
+    private <TReq, TResp>
+    CompletableFuture<TResp> clientStreaming(
+            Function<StreamObserver<TResp>, StreamObserver<TReq>> grpcMethod,
+            Flow.Publisher<TReq> requestPublisher) {
+
+        var response = new CompletableFuture<TResp>();
+
+        var responseGrpc = GrpcStreams.unaryResult(response);
+        var requestGrpc = grpcMethod.apply(responseGrpc);
+        var requestSubscriber = GrpcStreams.relay(requestGrpc);
+
+        requestPublisher.subscribe(requestSubscriber);
+
+        return response;
+    }
+
+    private <TDef>
+    TDef fetchDefinition(
+            String tenant, TagSelector selector,
+            Function<ObjectDefinition, TDef> defTypeFunc) {
+
+        var tag = metaApi.readObject(MetadataReadRequest.newBuilder()
+                .setTenant(tenant)
+                .setSelector(selector)
+                .build());
+
+        var objDef = tag.getDefinition();
+
+        return defTypeFunc.apply(objDef);
     }
 }
