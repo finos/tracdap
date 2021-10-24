ad22a1a6186c48d01789b285a22b8fd0de90fa3a
Martin Traverse <martin.traverse@accenture.com>
2021-10-24 01:27:50 +0100

    Sketch ideas for data core

---
+++ trac-services/trac-svc-data/src/main/java/com/accenture/trac/svc/data/service/DataCore.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.accenture.trac.svc.data.service;
+
+import io.netty.buffer.ByteBuf;
+import org.apache.arrow.vector.VectorSchemaRoot;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Flow;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+
+public class DataCore {
+
+    CompletableFuture<Boolean> writeData(Flow.Publisher<ByteBuf> byteStream) {
+
+        var signal = new CompletableFuture<Boolean>();
+
+        var codec = (IDataCodec) null;
+        var decoder = codec.decoder(null);
+
+        var storage = (IDataStorage) null;
+        var writer = storage.writeDelta(null, signal);
+
+        byteStream.subscribe(decoder);
+        decoder.subscribe(writer);
+
+        return signal;
+    }
+
+    Flow.Publisher<ByteBuf> readDelta() {
+
+        var codec = (IDataCodec) null;
+        var encoder = codec.encoder(null);
+
+        var storage = (IDataStorage) null;
+        var reader = storage.readDelta(null);
+
+        reader.subscribe(encoder);
+
+        return encoder;
+    }
+
+    interface IDataStorage {
+
+        Flow.Publisher<VectorSchemaRoot> readDelta(
+                Supplier<VectorSchemaRoot> recycler);
+
+        Flow.Subscriber<VectorSchemaRoot> writeDelta(
+                Consumer<VectorSchemaRoot> recycler,
+                CompletableFuture<Boolean> signal);
+    }
+
+    interface IDataCodec {
+
+        Flow.Processor<VectorSchemaRoot, ByteBuf> encoder(
+                Consumer<VectorSchemaRoot> recycler);
+
+        Flow.Processor<ByteBuf, VectorSchemaRoot> decoder(
+                Supplier<VectorSchemaRoot> recycler);
+    }
+
+    class CsvDataCodec implements IDataCodec {
+
+        @Override
+        public Flow.Processor<VectorSchemaRoot, ByteBuf> encoder(Consumer<VectorSchemaRoot> recycler) {
+            return null;
+        }
+
+        @Override
+        public Flow.Processor<ByteBuf, VectorSchemaRoot> decoder(Supplier<VectorSchemaRoot> recycler) {
+            return null;
+        }
+    }
+
+    class CsvEncoder implements Flow.Processor<VectorSchemaRoot, ByteBuf> {
+
+        @Override
+        public void subscribe(Flow.Subscriber<? super ByteBuf> subscriber) {
+
+        }
+
+        @Override
+        public void onSubscribe(Flow.Subscription subscription) {
+
+        }
+
+        @Override
+        public void onNext(VectorSchemaRoot item) {
+
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
+    }
+}
