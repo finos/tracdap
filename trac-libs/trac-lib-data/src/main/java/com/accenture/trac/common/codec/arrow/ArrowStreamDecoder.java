d146be5f90d36afeb685d2c3bef7782c17fbbec0
Martin Traverse <martin.traverse@accenture.com>
2021-11-12 00:37:14 +0000

    Add explicit format validation for the arrow stream format

--- trac-libs/trac-lib-data/src/main/java/com/accenture/trac/common/codec/arrow/ArrowStreamDecoder.java
+++ trac-libs/trac-lib-data/src/main/java/com/accenture/trac/common/codec/arrow/ArrowStreamDecoder.java
@@ -29,9 +29,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 
 public class ArrowStreamDecoder extends BaseDecoder {
+
+    // Safeguard max allowed size for first (schema) message - 16 MiB should be ample
+    private static final int CONTINUATION_MARKER = 0xffffffff;
+    private static final int MAX_FIRST_MESSAGE_SIZE = 16 * 1024 * 1024;
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
@@ -55,6 +61,11 @@ public class ArrowStreamDecoder extends BaseDecoder {
         try (var stream = new ByteSeekableChannel(chunk);
              var reader = new ArrowStreamReader(stream, arrowAllocator)) {
 
+            // Arrow does not attempt to validate the stream before reading
+            // This quick validation peeks at the start of the stream for a basic sanity check
+            // It should be enough to flag e.g. if data has been sent in a totally different format
+            validateStartOfStream(stream);
+
             var schema = reader.getVectorSchemaRoot().getSchema();
             emitBlock(DataBlock.forSchema(schema));
 
@@ -70,6 +81,15 @@ public class ArrowStreamDecoder extends BaseDecoder {
                 root.clear();
             }
         }
+        catch (NotAnArrowStream e) {
+
+            // A nice clean validation exception
+
+            var errorMessage = "Arrow stream decoding failed, content does not look like an Arrow stream";
+
+            log.error(errorMessage, e);
+            doTargetError(new EDataCorruption(errorMessage, e));
+        }
         catch (IllegalArgumentException | IndexOutOfBoundsException | IOException  e) {
 
             // These errors occur if the data stream contains bad values for vector sizes, offsets etc.
@@ -77,7 +97,7 @@ public class ArrowStreamDecoder extends BaseDecoder {
 
             // Decoders work on a stream of buffers, "real" IO exceptions should not occur
 
-            var errorMessage = "Arrow file decoding failed, content is garbled";
+            var errorMessage = "Arrow stream decoding failed, content is garbled";
 
             log.error(errorMessage, e);
             doTargetError(new EDataCorruption(errorMessage, e));
@@ -100,4 +120,35 @@ public class ArrowStreamDecoder extends BaseDecoder {
 
         // No-op, current version of arrow file decoder buffers the full input
     }
+
+    private void validateStartOfStream(ByteSeekableChannel stream) throws IOException {
+
+        // https://arrow.apache.org/docs/format/Columnar.html#encapsulated-message-format
+
+        // Arrow streams are a series of messages followed by optional body data
+        // Each message is preceded by a continuation marker and message size
+        // Just sanity checking these two values should catch some serious decode failures
+        // E.g. if a data stream contains a totally different format
+
+        // Record the stream position, so it can be restored after the validation
+        long pos = stream.position();
+
+        var headerBytes = new byte[8];
+        var headerBuf = ByteBuffer.wrap(headerBytes);
+        headerBuf.order(ByteOrder.LITTLE_ENDIAN);
+
+        stream.position(0);
+        stream.read(headerBuf);
+
+        var continuation = headerBuf.getInt(0);
+        var messageSize = headerBuf.getInt(4);
+
+        if (continuation != CONTINUATION_MARKER || messageSize <=0 || messageSize > MAX_FIRST_MESSAGE_SIZE)
+            throw new NotAnArrowStream();
+
+        // Restore original stream position
+        stream.position(pos);
+    }
+
+    private static class NotAnArrowStream extends RuntimeException { }
 }
