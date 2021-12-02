70a9d0b678c2e8eddb60a3fdf07d3927831fc74f
Martin Traverse <martin.traverse@accenture.com>
2021-12-02 13:51:08 +0000

    Wrap gRPC status errors from upstream calls to provide more meaningful logging

--- trac-libs/trac-lib-common/src/main/java/com/accenture/trac/common/grpc/GrpcErrorMapping.java
+++ trac-libs/trac-lib-common/src/main/java/com/accenture/trac/common/grpc/GrpcErrorMapping.java
@@ -39,19 +39,27 @@ public class GrpcErrorMapping {
         if (error instanceof CompletionException)
             error = error.getCause();
 
-        // Status runtime exception is a gRPC exception that is already propagated in the event stream
-        // This is most likely the result of an error when calling into another TRAC service
-        // For now, pass these errors on directly
-        // At some point there may (or may not) be benefit in wrapping/transforming upstream errors
-        // E.g. to handle particular types of expected exception
-        // However a lot of error response translate directly
-        // E.g. for metadata not found or permission denied lower down the stack - there is little benefit to wrapping
+        // Status exceptions will come from failed gRPC calls to other services
+        // We probably want to propagate the upstream error message back to the client/user
+        // To produce a meaningful stacktrace, wrap the upstream error with one created in this function
 
-        if (error instanceof StatusRuntimeException)
-            return (StatusRuntimeException) error;
+        if (error instanceof StatusException) {
 
-        if (error instanceof StatusException)
-            return ((StatusException) error).getStatus().asRuntimeException();
+            var upstreamError = (StatusException) error;
+
+            return upstreamError.getStatus()
+                    .withCause(upstreamError)
+                    .asRuntimeException();
+        }
+
+        if (error instanceof StatusRuntimeException) {
+
+            var upstreamError = (StatusRuntimeException) error;
+
+            return upstreamError.getStatus()
+                    .withCause(upstreamError)
+                    .asRuntimeException();
+        }
 
         // Make sure internal errors are always reported as internal and the error description is masked
 
