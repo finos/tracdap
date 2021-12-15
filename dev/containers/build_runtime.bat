6535999928f7602e3df7b5f6b1cc053423225e68
Martin Traverse <martin.traverse@accenture.com>
2021-12-15 14:37:52 +0000

    Windows end-to-end docker build of the Python runtime

--- dev/containers/build_runtime.bat
+++ dev/containers/build_runtime.bat
@@ -1,5 +1,31 @@
+@echo off
+
+set BUILD_IMAGE=python:3.10
+set TRAC_IMAGE="trac/runtime-python"
+
+REM Get repo root dir
+for %%A IN ("%~dp0..") do set REPO_DIR=%%~dpA
+
+
+REM Look up TRAC version
+echo Looking up TRAC version...
 
 git fetch --tags
 git remote | findstr /i "upstream" > nul && git fetch upstream --tags
 
-docker run --mount type=bind,source=%~dp0..\..,target=/mnt/trac python:3.10 /mnt/trac/dev/containers/build_runtime_inner.sh
+for /f "tokens=* USEBACKQ" %%v in (
+    `powershell -ExecutionPolicy Bypass -File %REPO_DIR%dev\version.ps1`) do (
+    set TRAC_VERSION=%%v)
+
+REM Docker cannot handle "+dev" version suffixes
+REM For anything that is not a tagged release, use version DEVELOPMENT for the image tag
+
+if not %TRAC_VERSION:+dev=%==%TRAC_VERSION% (
+    set TRAC_VERSION=DEVELOPMENT)
+
+echo TRAC version = %TRAC_VERSION%
+
+
+@echo on
+docker run --mount type=bind,source=%REPO_DIR%,target=/mnt/trac %BUILD_IMAGE% /mnt/trac/dev/containers/build_runtime_inner.sh
+docker build -t "%TRAC_IMAGE%:%TRAC_VERSION%" "%REPO_DIR%trac-runtime\python"
