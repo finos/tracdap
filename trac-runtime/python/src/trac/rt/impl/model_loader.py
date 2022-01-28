16516bfc155ca9be840e413cbff97eef8fa0d6a2
Martin Traverse <martin.traverse@accenture.com>
2022-01-28 16:58:28 +0000

    Add caching to model loader

--- trac-runtime/python/src/trac/rt/impl/model_loader.py
+++ trac-runtime/python/src/trac/rt/impl/model_loader.py
@@ -25,12 +25,14 @@ import trac.rt.config as _cfg
 
 import trac.rt.impl.model_repos as _repos
 import trac.rt.impl.model_shim as _shim
+import trac.rt.impl.util as _util
 
 
 class _ModelLoaderState:
 
     def __init__(self, scratch_dir: _tp.Union[pathlib.Path, str]):
         self.scratch_dir = scratch_dir
+        self.cache: _tp.Dict[str, _api.TracModel.__class__] = dict()
 
 
 class ModelLoader:
@@ -38,6 +40,7 @@ class ModelLoader:
     def __init__(self, sys_config: _cfg.RuntimeConfig):
         self.__repos = _repos.RepositoryManager(sys_config)
         self.__scopes: _tp.Dict[str, _ModelLoaderState] = dict()
+        self.__log = _util.logger_for_object(self)
 
     def create_scope(self, scope: str, model_scratch_dir: _tp.Union[str, pathlib.Path, _tps.NoneType] = None):
 
@@ -52,13 +55,25 @@ class ModelLoader:
     def load_model_class(self, scope: str, model_def: _meta.ModelDefinition) -> _api.TracModel.__class__:
 
         state = self.__scopes[scope]
-        repo = self.__repos.get_repository(model_def.repository)
 
+        model_key = f"{model_def.repository}#{model_def.path}#{model_def.version}#{model_def.entryPoint}"
+        model_class = state.cache.get(model_key)
+
+        if model_class is not None:
+            return model_class
+
+        self.__log.info(f"Loading model [{model_def.entryPoint}] (version=[{model_def.version}], scope=[{scope}])...")
+
+        repo = self.__repos.get_repository(model_def.repository)
         checkout_dir = pathlib.Path(state.scratch_dir).joinpath(model_def.repository)
         checkout = repo.checkout_model(model_def, checkout_dir)
 
         with _shim.ModelShim.use_checkout(checkout):
-            return _shim.ModelShim.load_model_class(model_def.entryPoint)
+
+            model_class = _shim.ModelShim.load_model_class(model_def.entryPoint)
+
+            state.cache[model_key] = model_class
+            return model_class
 
     def scan_model(self, model_class: _api.TracModel.__class__) -> _meta.ModelDefinition:
 
