c2afaec507c89fd93f6497dbee4d17f35224d60a
Martin Traverse <martin.traverse@accenture.com>
2022-02-09 11:07:52 +0000

    Update the top level runtime class to support embedded config objects

--- trac-runtime/python/src/trac/rt/exec/runtime.py
+++ trac-runtime/python/src/trac/rt/exec/runtime.py
@@ -19,16 +19,17 @@ import sys
 import pathlib
 import typing as tp
 
-import trac.rt.api as api
-import trac.rt.config as config
+import trac.rt.api as _api
+import trac.rt.config as _cfg
+import trac.rt._version as _version
 import trac.rt.exceptions as _ex
-import trac.rt.impl.config_parser as cfg
+import trac.rt.impl.config_parser as _cparse
 import trac.rt.impl.util as util
 import trac.rt.impl.models as _models
 import trac.rt.impl.storage as _storage
 
-import trac.rt.exec.actors as actors
-import trac.rt.exec.engine as engine
+import trac.rt.exec.actors as _actors
+import trac.rt.exec.engine as _engine
 import trac.rt.exec.dev_mode as _dev_mode
 
 
@@ -36,20 +37,23 @@ class TracRuntime:
 
     def __init__(
             self,
-            sys_config_path: tp.Union[str, pathlib.Path],
-            job_config_path: tp.Optional[tp.Union[str, pathlib.Path]] = None,
+            sys_config: tp.Union[str, pathlib.Path, _cfg.RuntimeConfig],
+            job_config: tp.Union[str, pathlib.Path, _cfg.JobConfig, None] = None,
             dev_mode: bool = False,
-            model_class: tp.Optional[api.TracModel.__class__] = None):
+            model_class: tp.Optional[_api.TracModel.__class__] = None):
 
-        trac_version = 'DEVELOPMENT VERSION'  # TODO: Bring in version from the version script
+        trac_version = _version.__version__
         python_version = sys.version.replace("\n", "")
-        mode = "batch" if job_config_path else "service"
+        mode = "batch" if job_config else "service"
+
+        sys_config_path = "[embedded]" if isinstance(sys_config, _cfg.RuntimeConfig) else sys_config
+        job_config_path = "[embedded]" if isinstance(job_config, _cfg.JobConfig) else job_config
 
         print(f">>> TRAC Python Runtime {trac_version} starting in {mode} mode at {dt.datetime.now()}")
         print(f">>> Python installation: {python_version} ({sys.exec_prefix})")
         print(f">>> System config: {sys_config_path}")
 
-        if job_config_path:
+        if job_config:
             print(f">>> Job config: {job_config_path}")
 
         if dev_mode:
@@ -59,29 +63,23 @@ class TracRuntime:
         self._log = util.logger_for_object(self)
         self._log.info(f"TRAC Python Runtime {trac_version}")
 
+        self._job_config_path = job_config_path
         self._sys_config_path = sys_config_path
         self._sys_config_dir = pathlib.Path(self._sys_config_path).parent
-
-        if job_config_path:
-            self._job_config_path = job_config_path
-            self._batch_mode = True
-        else:
-            self._job_config_path = None
-            self._batch_mode = False
-
-        self._sys_config: tp.Optional[config.RuntimeConfig] = None
-        self._job_config: tp.Optional[config.JobConfig] = None
-
+        self._batch_mode = bool(job_config is not None)
         self._dev_mode = dev_mode
         self._model_class = model_class
+
+        self._sys_config = sys_config if isinstance(sys_config, _cfg.RuntimeConfig) else None
+        self._job_config = job_config if isinstance(job_config, _cfg.JobConfig) else None
 
         # Top level resources
         self._models: tp.Optional[_models.ModelLoader] = None
         self._storage: tp.Optional[_storage.StorageManager] = None
 
         # The execution engine
-        self._engine: tp.Optional[engine.TracEngine] = None
-        self._system: tp.Optional[actors.ActorSystem] = None
+        self._engine: tp.Optional[_engine.TracEngine] = None
+        self._system: tp.Optional[_actors.ActorSystem] = None
 
     # ------------------------------------------------------------------------------------------------------------------
     # Runtime service control
@@ -95,7 +93,7 @@ class TracRuntime:
 
         # Do not call self.stop()
         # In batch mode, the engine will stop itself once the job is complete, so no need to stop twice
-        # It may be necessary to call self.stop() in service mode, depending how the stop is triggered (e.g. interrupt)
+        # It may be necessary to call self.stop() in service mode, depending on how stop is triggered (e.g. interrupt)
 
         pass
 
@@ -107,18 +105,26 @@ class TracRuntime:
 
             # Plugins will be loaded here, before config
 
-            # self._log.info("Loading system config...")
-            sys_config_dev_mode = _dev_mode.DEV_MODE_SYS_CONFIG if self._dev_mode else None
-            sys_config_parser = cfg.ConfigParser(config.RuntimeConfig, sys_config_dev_mode)
-            sys_config_raw = sys_config_parser.load_raw_config(self._sys_config_path, config_file_name="system")
-            self._sys_config = sys_config_parser.parse(sys_config_raw, self._sys_config_path)
+            # Load sys and job config (or use embedded)
 
-            if self._batch_mode:
-                # self._log.info("Loading job config...")
+            if self._sys_config is None:
+                sys_config_dev_mode = _dev_mode.DEV_MODE_SYS_CONFIG if self._dev_mode else None
+                sys_config_parser = _cparse.ConfigParser(_cfg.RuntimeConfig, sys_config_dev_mode)
+                sys_config_raw = sys_config_parser.load_raw_config(self._sys_config_path, config_file_name="system")
+                self._sys_config = sys_config_parser.parse(sys_config_raw, self._sys_config_path)
+            else:
+                self._log.info("Using embedded system config")
+
+            if self._job_config is None and self._batch_mode:
                 job_config_dev_mode = _dev_mode.DEV_MODE_JOB_CONFIG if self._dev_mode else None
-                job_config_parser = cfg.ConfigParser(config.JobConfig, job_config_dev_mode)
+                job_config_parser = _cparse.ConfigParser(_cfg.JobConfig, job_config_dev_mode)
                 job_config_raw = job_config_parser.load_raw_config(self._job_config_path, config_file_name="job")
                 self._job_config = job_config_parser.parse(job_config_raw, self._job_config_path)
+            elif self._batch_mode:
+                self._log.info("Using embedded job config")
+
+            # Dev mode translation is controlled by the dev mode flag
+            # I.e. it can be applied to embedded configs
 
             if self._dev_mode:
 
@@ -140,9 +146,12 @@ class TracRuntime:
             self._models = _models.ModelLoader(self._sys_config)
             self._storage = _storage.StorageManager(self._sys_config, self._sys_config_dir)
 
-            self._engine = engine.TracEngine(self._sys_config, self._models, self._storage, batch_mode=self._batch_mode)
-            self._system = actors.ActorSystem(self._engine, system_thread="engine")
+            self._engine = _engine.TracEngine(
+                self._sys_config,
+                self._models, self._storage,
+                batch_mode=self._batch_mode)
 
+            self._system = _actors.ActorSystem(self._engine, system_thread="engine")
             self._system.start(wait=wait)
 
         except Exception as e:
