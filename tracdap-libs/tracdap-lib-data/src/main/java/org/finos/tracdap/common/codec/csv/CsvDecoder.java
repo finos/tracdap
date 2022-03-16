2f0f6805f2b2c18fd552feb39a8dac9af8f92241
Martin Traverse <martin.traverse@accenture.com>
2022-03-16 19:15:49 +0000

    Allow nulls in CSV decoder

--- tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/codec/csv/CsvDecoder.java
+++ tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/codec/csv/CsvDecoder.java
@@ -83,8 +83,12 @@ public class CsvDecoder extends BaseDecoder {
     protected void decodeChunk(ByteBuf chunk) {
 
         var csvFactory = new CsvFactory()
-                .enable(CsvParser.Feature.TRIM_SPACES)
-                .enable(CsvParser.Feature.FAIL_ON_MISSING_COLUMNS);
+                // Require strict adherence to the schema
+                .enable(CsvParser.Feature.FAIL_ON_MISSING_COLUMNS)
+                // Always allow nulls during parsing (they will be rejected later for non-nullable fields)
+                .enable(CsvParser.Feature.EMPTY_STRING_AS_NULL)
+                // Permissive handling of extra space (strings with leading/trailing spaces must be quoted anyway)
+                .enable(CsvParser.Feature.TRIM_SPACES);
 
         try (var stream = new ByteBufInputStream(chunk);
              var parser = (CsvParser) csvFactory.createParser((InputStream) stream)) {
