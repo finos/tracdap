b52349b5fb1c1c8a565c8b2b40b02633e4ecace8
Martin Traverse <martin.traverse@accenture.com>
2022-03-17 19:19:15 +0000

    Use a script to infer the NPM release tag from the version number

---
+++ dev/version_tag.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+# Copyright 2022 Accenture Global Solutions Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# For NPM Publish, we need to specify a release tag
+# This ia a quick way to generate the tag based on the full version string
+
+VERSION=$1
+
+if echo $VERSION | grep -qv "^v[0-9]\+\.[0-9]\+\.[0-9]\+"; then
+  echo "invalid"
+elif echo $VERSION | grep -q "^v[0-9]\+\.[0-9]\+\.[0-9]\+$"; then
+  echo "latest"
+elif echo $VERSION | grep -q "dev"; then
+  echo "dev"
+elif echo $VERSION | grep -q "alpha"; then
+  echo "alpha"
+elif  echo $VERSION | grep -q "beta"; then
+  echo "beta"
+elif echo $VERSION | grep -q "rc"; then
+  echo "rc"
+else
+  echo "invalid"
+fi
