4b8a81ff3538b9e595869a88bad38682c5574774
Martin Traverse <martin.traverse@accenture.com>
2022-03-29 15:39:29 +0100

    Stub validators for models, flows and jobs

---
+++ tracdap-libs/tracdap-lib-validation/src/main/java/org/finos/tracdap/common/validation/static_/ModelValidator.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.common.validation.static_;
+
+import org.finos.tracdap.common.validation.core.ValidationContext;
+import org.finos.tracdap.common.validation.core.ValidationType;
+import org.finos.tracdap.common.validation.core.Validator;
+import org.finos.tracdap.metadata.ModelDefinition;
+
+import org.slf4j.LoggerFactory;
+
+
+@Validator(type = ValidationType.STATIC)
+public class ModelValidator {
+
+    @Validator
+    public static ValidationContext model(ModelDefinition msg, ValidationContext ctx) {
+
+        LoggerFactory.getLogger(ModelValidator.class).warn("Validator not implemented");
+
+        return ctx;
+    }
+}
