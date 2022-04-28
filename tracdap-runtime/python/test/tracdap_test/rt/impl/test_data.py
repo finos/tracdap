55c9ea05ac6f34e6660f31dc25c14757a6aa0a51
Martin Traverse <martin.traverse@accenture.com>
2022-04-28 18:53:36 +0100

    Add edge case tests for data mapping

--- tracdap-runtime/python/test/tracdap_test/rt/impl/test_data.py
+++ tracdap-runtime/python/test/tracdap_test/rt/impl/test_data.py
@@ -15,6 +15,8 @@
 import datetime as dt
 import decimal
 import unittest
+import sys
+import math
 
 import pyarrow as pa
 
@@ -58,6 +60,19 @@ class DataMappingTest(unittest.TestCase):
                 dt.datetime(2000, 1, 3, 2, 2, 2), dt.datetime(2000, 1, 4, 3, 3, 3)]
         }
 
+    @staticmethod
+    def one_field_schema(field_type: _meta.BasicType):
+
+        field_name = f"{field_type.name.lower()}_field"
+
+        trac_schema = _meta.SchemaDefinition(
+            _meta.SchemaType.TABLE,
+            _meta.PartType.PART_ROOT,
+            _meta.TableSchema(fields=[
+                _meta.FieldSchema(field_name, fieldType=field_type)]))
+
+        return _types.trac_to_arrow_schema(trac_schema)
+
     def test_round_trip_basic(self):
 
         sample_schema = self.sample_schema()
@@ -91,8 +106,126 @@ class DataMappingTest(unittest.TestCase):
         self.assertEqual(sample_schema, rt.schema)
         self.assertEqual(table, rt)
 
-    def test_round_trip_edge_cases(self):
-        pass
+    def test_edge_cases_integer(self):
+
+        schema = self.one_field_schema(_meta.BasicType.INTEGER)
+        table = pa.Table.from_pydict({"integer_field": [  # noqa
+            0,
+            sys.maxsize,
+            -sys.maxsize - 1
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_float(self):
+
+        schema = self.one_field_schema(_meta.BasicType.FLOAT)
+        table = pa.Table.from_pydict({"float_field": [  # noqa
+            0.0,
+            sys.float_info.min,
+            sys.float_info.max,
+            sys.float_info.epsilon,
+            -sys.float_info.epsilon,
+            math.inf,
+            -math.inf
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_float_nan(self):
+
+        # For NaN, a special test that checks math.isnan on the round-trip result
+        # Because math.nan != math.nan
+
+        schema = self.one_field_schema(_meta.BasicType.FLOAT)
+        table = pa.Table.from_pydict({"float_field": [math.nan]}, schema)  # noqa
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_decimal(self):
+
+        # TRAC basic decimal has precision 38, scale 12
+        # Should allow for 26 places before the decimal place and 12 after
+
+        schema = self.one_field_schema(_meta.BasicType.DECIMAL)
+        table = pa.Table.from_pydict({"decimal_field": [  # noqa
+            decimal.Decimal(0.0),
+            decimal.Decimal(1.0) * decimal.Decimal(1.0).shift(25),
+            decimal.Decimal(1.0) / decimal.Decimal(1.0).shift(12),
+            decimal.Decimal(-1.0) * decimal.Decimal(1.0).shift(25),
+            decimal.Decimal(-1.0) / decimal.Decimal(1.0).shift(12)
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_string(self):
+
+        schema = self.one_field_schema(_meta.BasicType.STRING)
+        table = pa.Table.from_pydict({"string_field": [  # noqa
+            "", " ", "  ", "\t", "\r\n", "  \r\n   ",
+            "a, b\",", "'@@'", "[\"\"%^&", "£££", "#@",
+            "Olá Mundo", "你好，世界", "Привет, мир", "नमस्ते दुनिया",
+            "𝜌 = ∑ 𝑃𝜓 | 𝜓 ⟩ ⟨ 𝜓 |"
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_date(self):
+
+        schema = self.one_field_schema(_meta.BasicType.DATE)
+        table = pa.Table.from_pydict({"date_field": [  # noqa
+            dt.date(1970, 1, 1),
+            dt.date(2000, 1, 1),
+            dt.date(2038, 1, 20),
+            dt.date.max,
+            dt.date.min
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+    def test_edge_cases_datetime(self):
+
+        schema = self.one_field_schema(_meta.BasicType.DATETIME)
+        table = pa.Table.from_pydict({"datetime_field": [  # noqa
+            dt.datetime(1970, 1, 1),
+            dt.datetime(2000, 1, 1),
+            dt.datetime(2038, 1, 20),
+            dt.datetime.max,
+            dt.datetime.min
+        ]}, schema)
+
+        df = _data.DataMapping.arrow_to_pandas(table)
+        rt = _data.DataMapping.pandas_to_arrow(df, schema)
+
+        self.assertEqual(schema, rt.schema)
+        self.assertEqual(table, rt)
+
+
+class DataConformanceTest(unittest.TestCase):
 
     def test_coercion_decimal(self):
         pass
