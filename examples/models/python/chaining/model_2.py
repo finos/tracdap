98f0bdbf954014570a7d84f8bfb9f797386ef029
Martin Traverse <martin.traverse@accenture.com>
2022-05-06 18:18:44 +0100

    Stub example files for model chaining example

---
+++ examples/models/python/chaining/model_2.py
@@ -0,0 +1,31 @@
+#  Copyright 2022 Accenture Global Solutions Limited
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import typing as tp
+import tracdap.rt.api as trac
+
+
+class SecondModel(trac.TracModel):
+
+    def define_parameters(self) -> tp.Dict[str, trac.ModelParameter]:
+        pass
+
+    def define_inputs(self) -> tp.Dict[str, trac.ModelInputSchema]:
+        pass
+
+    def define_outputs(self) -> tp.Dict[str, trac.ModelOutputSchema]:
+        pass
+
+    def run_model(self, ctx: trac.TracContext):
+        pass
