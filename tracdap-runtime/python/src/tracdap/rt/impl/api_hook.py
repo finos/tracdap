586bbd9fb8f1b42857dc2c42cbdd543c69ac7407
Martin Traverse <martin.traverse@accenture.com>
2022-06-07 13:43:08 +0100

    Add an API guard (signature checking) to the runtime API hook

--- tracdap-runtime/python/src/tracdap/rt/impl/api_hook.py
+++ tracdap-runtime/python/src/tracdap/rt/impl/api_hook.py
@@ -11,8 +11,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import importlib
-import importlib.resources as resources
+
+import inspect
+import logging
 import typing as _tp
 import types as _ts
 import pathlib as _path
@@ -26,7 +27,159 @@ import tracdap.rt.impl.util as _util
 from tracdap.rt.api.hook import *
 
 
+class ApiGuard:
+
+    # The metaclass for generic types varies between versions of the typing library
+    # To work around this, detect the correct metaclass by inspecting a generic type variable
+    __generic_metaclass = type(_tp.List[object])
+
+    _log: logging.Logger
+
+    @classmethod
+    def validate_signature(cls, method_name: str, method: inspect.Signature, *args, **kwargs):
+
+        positional_index = 0
+
+        for param_name, param in method.parameters.items():
+
+            values = cls._select_arg(method_name, param, positional_index, *args, **kwargs)
+            positional_index += len(values)
+
+            for value in values:
+                cls._validate_arg(method_name, param, value)
+
+    @classmethod
+    def _select_arg(
+            cls, method_name: str, parameter: inspect.Parameter, positional_index,
+            *args, **kwargs) -> _tp.List[_tp.Any]:
+
+        if parameter.kind == inspect.Parameter.POSITIONAL_ONLY:
+
+            if positional_index < len(args):
+                return [args[positional_index]]
+
+            else:
+                err = f"Invalid API call [{method_name}()]: Missing required parameter [{parameter.name}]"
+                cls._log.error(err)
+                raise _ex.ERuntimeValidation(err)
+
+        if parameter.kind == inspect.Parameter.KEYWORD_ONLY:
+
+            if parameter.name in kwargs:
+                return [kwargs[parameter.name]]
+
+            else:
+                err = f"Invalid API call [{method_name}()]: Missing required parameter [{parameter.name}]"
+                cls._log.error(err)
+                raise _ex.ERuntimeValidation(err)
+
+        if parameter.kind == inspect.Parameter.POSITIONAL_OR_KEYWORD:
+
+            if positional_index < len(args):
+                return [args[positional_index]]
+
+            elif parameter.name in kwargs:
+                return [kwargs[parameter.name]]
+
+            else:
+                err = f"Invalid API call [{method_name}()]: Missing required parameter [{parameter.name}]"
+                cls._log.error(err)
+                raise _ex.ERuntimeValidation(err)
+
+        if parameter.kind == inspect.Parameter.VAR_POSITIONAL:
+
+            if positional_index < len(args):
+                return [args[i] for i in range(positional_index, len(args))]
+            else:
+                return []
+
+        if parameter.kind == inspect.Parameter.VAR_KEYWORD:
+
+            raise _ex.ETracInternal("Validation of VAR_KEYWORD params is not supported yet")
+
+        raise _ex.EUnexpected("Invalid method signature in runtime API (this is a bug)")
+
+    @classmethod
+    def _validate_arg(cls, method_name: str, parameter: inspect.Parameter, value: _tp.Any):
+
+        if not cls._validate_arg_inner(parameter.annotation, value):
+
+            expected_type = cls._type_name(parameter.annotation)
+            actual_type = cls._type_name(type(value)) if value is not None else str(None)
+
+            err = f"Invalid API call [{method_name}()]: Wrong type for [{parameter.name}]" \
+                  + f" (expected [{expected_type}], got [{actual_type}])"
+
+            cls._log.error(err)
+            raise _ex.ERuntimeValidation(err)
+
+    @classmethod
+    def _validate_arg_inner(cls, param_type: _tp.Type, value: _tp.Any) -> bool:
+
+        if param_type == _tp.Any:
+            return True
+
+        if isinstance(param_type, cls.__generic_metaclass):
+
+            origin = _util.get_origin(param_type)
+            args = _util.get_args(param_type)
+
+            # The generic type "Named" is defined in the TRAC API so needs to be supported
+            # This type is used for passing named intermediate values between the define_ methods
+            if origin is Named:
+                named_type = args[0]
+                return isinstance(value, Named) and isinstance(value.item, named_type)
+
+            # _tp.Union also covers _tp.Optional, which is shorthand for _tp.Union[_type, None]
+            if origin is _tp.Union:
+
+                for union_type in args:
+                    if cls._validate_arg_inner(union_type, value):
+                        return True
+
+                return False
+
+            raise _ex.ETracInternal(f"Validation of [{origin.__name__}] generic parameters is not supported yet")
+
+        # Validate everything else as a concrete type
+
+        return isinstance(value, param_type)
+
+    @classmethod
+    def _type_name(cls, type_var: _tp.Type) -> str:
+
+        if isinstance(type_var, cls.__generic_metaclass):
+
+            origin = _util.get_origin(type_var)
+            args = _util.get_args(type_var)
+
+            if origin is Named:
+                named_type = cls._type_name(args[0])
+                return f"Named[{named_type}]"
+
+            if origin is _tp.Union:
+                return "|".join(map(cls._type_name, args))
+
+            raise _ex.ETracInternal(f"Validation of [{origin.__name__}] generic parameters is not supported yet")
+
+        return type_var.__name__
+
+
+ApiGuard._log = _util.logger_for_class(ApiGuard)
+
+
 class RuntimeHookImpl(RuntimeHook):  # noqa
+
+    __define_parameter_signature: inspect.Signature
+    __define_parameters_signature: inspect.Signature
+    __define_field_signature: inspect.Signature
+    __define_schema_signature: inspect.Signature
+    __load_schema_signature: inspect.Signature
+    __define_input_table_signature: inspect.Signature
+    __define_output_table_signature: inspect.Signature
+
+    def __init__(self):
+        self._prepare_signatures()
 
     @classmethod
     def register_impl(cls):
@@ -47,6 +200,10 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
             label: str, default_value: _tp.Optional[_tp.Any] = None) \
             -> Named[_meta.ModelParameter]:
 
+        ApiGuard.validate_signature(
+            self.define_parameter.__name__, self.__define_parameter_signature,
+            param_name, param_type, label, default_value)
+
         if isinstance(param_type, _meta.TypeDescriptor):
             param_type_descriptor = param_type
         else:
@@ -58,6 +215,10 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
             self, *params: _tp.Union[Named[_meta.ModelParameter], _tp.List[Named[_meta.ModelParameter]]]) \
             -> _tp.Dict[str, _meta.ModelParameter]:
 
+        ApiGuard.validate_signature(
+            self.define_parameters.__name__, self.__define_parameters_signature,
+            *params)
+
         if len(params) == 1 and isinstance(params[0], list):
             return {p.itemName: p.item for p in params[0]}
         else:
@@ -68,6 +229,10 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
             business_key: bool = False, categorical: bool = False,
             format_code: _tp.Optional[str] = None, field_order: _tp.Optional[int] = None) \
             -> _meta.FieldSchema:
+
+        ApiGuard.validate_signature(
+            self.define_field.__name__, self.__define_field_signature,
+            field_name, field_type, label, business_key, categorical, format_code, field_order)
 
         return _meta.FieldSchema(
             field_name,
@@ -83,6 +248,10 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
             schema_type: _meta.SchemaType = _meta.SchemaType.TABLE) \
             -> _meta.SchemaDefinition:
 
+        ApiGuard.validate_signature(
+            self.define_schema.__name__, self.__define_schema_signature,
+            *fields, schema_type)
+
         if schema_type == _meta.SchemaType.TABLE:
 
             table_schema = self._build_table_schema(*fields)
@@ -95,11 +264,19 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
             schema_type: _meta.SchemaType = _meta.SchemaType.TABLE) \
             -> _meta.SchemaDefinition:
 
+        ApiGuard.validate_signature(
+            self.load_schema.__name__, self.__load_schema_signature,
+            package, schema_file, schema_type)
+
         return _schemas.SchemaLoader.load_schema(package, schema_file)
 
     def define_input_table(
             self, *fields: _tp.Union[_meta.FieldSchema, _tp.List[_meta.FieldSchema]]) \
             -> _meta.ModelInputSchema:
+
+        ApiGuard.validate_signature(
+            self.define_input_table.__name__, self.__define_input_table_signature,
+            *fields)
 
         schema_def = self.define_schema(*fields, schema_type=_meta.SchemaType.TABLE)
         return _meta.ModelInputSchema(schema=schema_def)
@@ -107,6 +284,10 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
     def define_output_table(
             self, *fields: _tp.Union[_meta.FieldSchema, _tp.List[_meta.FieldSchema]]) \
             -> _meta.ModelOutputSchema:
+
+        ApiGuard.validate_signature(
+            self.define_output_table.__name__, self.__define_output_table_signature,
+            *fields)
 
         schema_def = self.define_schema(*fields, schema_type=_meta.SchemaType.TABLE)
         return _meta.ModelOutputSchema(schema=schema_def)
@@ -126,3 +307,13 @@ class RuntimeHookImpl(RuntimeHook):  # noqa
                 field.fieldOrder = index
 
         return _meta.TableSchema([*fields_])
+
+    def _prepare_signatures(self):
+
+        self.__define_parameter_signature = inspect.signature(self.define_parameter)
+        self.__define_parameters_signature = inspect.signature(self.define_parameters)
+        self.__define_field_signature = inspect.signature(self.define_field)
+        self.__define_schema_signature = inspect.signature(self.define_schema)
+        self.__load_schema_signature = inspect.signature(self.load_schema)
+        self.__define_input_table_signature = inspect.signature(self.define_input_table)
+        self.__define_output_table_signature = inspect.signature(self.define_output_table)
