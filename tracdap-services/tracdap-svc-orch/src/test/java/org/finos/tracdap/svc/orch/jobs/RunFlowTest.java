bb23e06cc866c00cbd4c37a43a27e18e97511ae6
Martin Traverse <martin.traverse@accenture.com>
2022-07-15 11:10:07 +0100

    Include E2E tests using models from a Git repo when running in CI

--- tracdap-services/tracdap-svc-orch/src/test/java/org/finos/tracdap/svc/orch/jobs/RunFlowTest.java
+++ tracdap-services/tracdap-svc-orch/src/test/java/org/finos/tracdap/svc/orch/jobs/RunFlowTest.java
@@ -27,6 +27,7 @@ import org.finos.tracdap.test.helpers.PlatformTest;
 import com.google.protobuf.ByteString;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -44,13 +45,24 @@ import static org.finos.tracdap.svc.orch.jobs.Helpers.runJob;
 @Tag("integration")
 @Tag("int-e2e")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class RunFlowTest {
+public abstract class RunFlowTest {
 
     private static final String TEST_TENANT = "ACME_CORP";
     private static final String E2E_CONFIG = "config/trac-e2e.yaml";
 
     private static final String LOANS_INPUT_PATH = "examples/models/python/data/inputs/loan_final313_100_shortform.csv";
     private static final String CURRENCY_INPUT_PATH = "examples/models/python/data/inputs/currency_data_sample.csv";
+
+    protected abstract String useTracRepo();
+
+    public static class LocalRepoTest extends RunFlowTest {
+        protected String useTracRepo() { return "TRAC_LOCAL_REPO"; }
+    }
+
+    @EnabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true", disabledReason = "Only run in CI")
+    public static class GitRepoTest extends RunFlowTest {
+        protected String useTracRepo() { return "TRAC_GIT_REPO"; }
+    }
 
     @RegisterExtension
     private static final PlatformTest platform = PlatformTest.forConfig(E2E_CONFIG)
@@ -233,7 +245,7 @@ public class RunFlowTest {
 
         var importModel = ImportModelJob.newBuilder()
                 .setLanguage("python")
-                .setRepository("UNIT_TEST_REPO")
+                .setRepository(useTracRepo())
                 .setPath(path)
                 .setEntryPoint(entryPoint)
                 .setVersion(version)
