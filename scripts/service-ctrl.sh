f335ffb61b18a51d4b155bde9e10892c41e88339
Martin Traverse <martin.traverse@accenture.com>
2022-09-10 20:44:35 +0100

    Start work on the startup script for Linux / macOS

--- scripts/service-ctrl.sh
+++ scripts/service-ctrl.sh
@@ -14,3 +14,78 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+APPLICATION_NAME="$applicationName"
+APPLICATION_CLASS="$mainClassName"
+
+
+# These environment variables are required to start the service
+
+CONFIG_FILE="\${CONFIG_FILE:?Missing required environment variable CONFIG_FILE}"
+
+
+# Optional environment variables, set if needed, defaults are normally fine
+
+ENABLE_PLUGINS=\${ENABLE_PLUGINS:=true}
+ENABLE_PLUGINS_EXT=\${ENABLE_PLUGINS_EXT:=false}
+
+
+# Standard directory locations, relative to the install dir
+
+PLUGIN_DIR="plugins"
+PLUGIN_EXT_DIR="plugins_ext"
+LOG_DIR="log"
+RUN_DIR="run"
+
+
+# JVM settings
+
+DEFAULT_JVM_OPTS=$defaultJvmOpts
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+APP_HOME=\$(cd `dirname \$0` && cd .. && pwd)
+
+CORE_CLASSPATH=\$(cat <<-CLASSPATH_END
+${classpath.replace(":", ":\\\n")}
+CLASSPATH_END)
+
+
+start() {
+
+    echo "Starting application: \${APPLICATION_NAME}"
+    echo
+
+    echo "Application install location: \${APP_HOME}"
+    echo "Application config: \${CONFIG_FILE}"
+    echo
+
+    java -cp \$CORE_CLASSPATH \$APPLICATION_CLASS --config "\${CONFIG_FILE}"
+}
+
+stop() {
+    echo "This will stop \${APPLICATION_CLASS}"
+}
+
+case "\$1" in
+    start)
+       start
+       ;;
+    stop)
+       stop
+       ;;
+    restart)
+       stop
+       start
+       ;;
+    status)
+       # code to check status of app comes here
+       # example: status program_name
+       ;;
+    *)
+       echo "Usage: \$0
+       } {start|stop|status|restart}"
+esac
+
+exit 0
