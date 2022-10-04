fc06329521cf21495e5e379323e7158fe202fc6c
Martin Traverse <martin.traverse@accenture.com>
2022-10-04 21:43:13 +0100

    Fixes for Linux start scripts running on RHEL (#177)
    * Fixes for Linux start scripts running on RHEL
    
    * Add exception for CVE in Jackson databind 2.13.4,
    as soon as 2.14 is released we can upgrade and remove this exception
    
    * Fix read JAVA_OPTS in Linux start script

--- dist/scripts/service-ctrl.sh
+++ dist/scripts/service-ctrl.sh
@@ -122,7 +122,8 @@ fi
 
 CLASSPATH=\$(cat <<-CLASSPATH_END
 ${classpath.replace(":", ":\\\n")}
-CLASSPATH_END)
+CLASSPATH_END
+)
 
 # Discover standard plugins
 
@@ -149,7 +150,8 @@ fi
 
 CORE_JAVA_OPTS=\$(cat <<-JAVA_OPTS_END
 ${defaultJvmOpts.replace("'", "").replace(' "-', '\n"-').replace('"', '')}
-JAVA_OPTS_END)
+JAVA_OPTS_END
+)
 
 # Set java properties used for logging
 
@@ -287,7 +289,7 @@ status() {
 
         PID=`cat "\${PID_FILE}"`
 
-        if [ `ps -p \$PID > /dev/null` ]; then
+        if `ps -p \$PID > /dev/null`; then
             echo "\${APPLICATION_NAME} is up"
         else
             echo "\${APPLICATION_NAME} is down"
@@ -325,7 +327,7 @@ kill_all() {
 
     FOUND=0
 
-    for PID in `ps -A | grep "[j]ava" | grep "\${APPLICATION_CLASS}" | awk '{print \$1;}'`; do
+    for PID in `ps -Ao "pid,command" | grep "[j]ava" | grep "\${APPLICATION_CLASS}" | awk '{print \$1;}'`; do
         echo "Killing PID \$PID..."
         kill -KILL \$PID
         FOUND=1
