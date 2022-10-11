7c54f4e0849bf816aec42e25ccb718046fee030a
Martin Traverse <martin.traverse@accenture.com>
2022-10-11 21:11:44 +0100

    Helpful error messages for some more common errors

--- tracdap-runtime/python/src/tracdap/rt/_impl/shim.py
+++ tracdap-runtime/python/src/tracdap/rt/_impl/shim.py
@@ -20,6 +20,7 @@ import pathlib
 import sys
 import contextlib
 import functools as fn
+import traceback as tb
 
 import inspect
 import importlib as _il
@@ -365,8 +366,21 @@ class ShimLoader:
 
             return class_
 
+        except _ex.EModelLoad:
+            raise
+
         except ModuleNotFoundError as e:
             err = f"Loading classes failed in module [{module_name}]: {str(e)}"
+            cls._log.error(err)
+            raise _ex.EModelLoad(err) from e
+
+        except NameError as e:
+            err = f"Loading classes failed in module [{module_name}]: {str(e)} ({cls._last_frame(e)}"
+            cls._log.error(err)
+            raise _ex.EModelLoad(err) from e
+
+        except Exception as e:
+            err = f"Loading classes failed in module [{module_name}]: Unexpected error"
             cls._log.error(err)
             raise _ex.EModelLoad(err) from e
 
@@ -406,6 +420,9 @@ class ShimLoader:
 
             return load_func(module, resource_name)
 
+        except _ex.EModelLoad:
+            raise
+
         except ModuleNotFoundError as e:
             err = f"Loading resources failed in module [{module_name}]: {str(e)}"
             cls._log.error(err)
@@ -413,6 +430,16 @@ class ShimLoader:
 
         except FileNotFoundError as e:
             err = f"Loading resources failed in module [{module_name}]: Resource not found for [{resource_name}]"
+            cls._log.error(err)
+            raise _ex.EModelLoad(err) from e
+
+        except NameError as e:
+            err = f"Loading classes failed in module [{module_name}]: {str(e)} ({cls._last_frame(e)}"
+            cls._log.error(err)
+            raise _ex.EModelLoad(err) from e
+
+        except Exception as e:
+            err = f"Loading classes failed in module [{module_name}]: Unexpected error"
             cls._log.error(err)
             raise _ex.EModelLoad(err) from e
 
@@ -434,6 +461,13 @@ class ShimLoader:
                 err = f"Loading resources is not allowed inside run_model()"
                 cls._log.error(err)
                 raise _ex.ERuntimeValidation(err)
+
+    @classmethod
+    def _last_frame(cls, error: Exception):
+        trace = tb.extract_tb(error.__traceback__)
+        last_frame = trace[len(trace) - 1]
+        filename = pathlib.PurePath(last_frame.filename).name
+        return f"{filename} line {last_frame.lineno}, {last_frame.line}"
 
 
 ShimLoader._log = _util.logger_for_class(ShimLoader)
