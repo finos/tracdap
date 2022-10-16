df9629a3fb3721e483e6968742d07e603a803a61
Martin Traverse <martin.traverse@accenture.com>
2022-10-16 06:54:05 +0100

    New codec base classes to work with data pipelines

--- tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/codec/BufferDecoder.java
+++ tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/codec/BufferDecoder.java
@@ -17,29 +17,9 @@
 package org.finos.tracdap.common.codec;
 
 import org.finos.tracdap.common.data.DataPipeline;
+import org.finos.tracdap.common.data.pipeline.BaseDataProducer;
 
-import org.apache.arrow.vector.types.pojo.Schema;
-import org.apache.arrow.memory.BufferAllocator;
-
-import java.util.List;
-import java.util.Map;
-
-public interface ICodec {
-
-    interface Encoder extends DataPipeline.DataConsumer, DataPipeline.ByteProducer {}
-    interface Decoder extends DataPipeline.ByteConsumer, DataPipeline.DataProducer {}
-
-    List<String> options();
-
-    String defaultFileExtension();
-
-    Encoder getEncoder(
-            BufferAllocator arrowAllocator,
-            Schema schema,
-            Map<String, String> options);
-
-    Decoder getDecoder(
-            BufferAllocator arrowAllocator,
-            Schema schema,
-            Map<String, String> options);
-}
+public abstract class BufferDecoder extends BaseDataProducer implements
+        DataPipeline.ByteBufferConsumer,
+        DataPipeline.DataStreamProducer,
+        ICodec.Decoder {}
