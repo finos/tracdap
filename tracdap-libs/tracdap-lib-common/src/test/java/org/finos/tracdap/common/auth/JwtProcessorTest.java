70c6120009b23faf6f37c866c81acfc2728b02f1
Martin Traverse <martin.traverse@accenture.com>
2022-11-08 22:32:04 +0000

    Some basic functional tests for JWT processing / validation (i.e. JWT auth)

---
+++ tracdap-libs/tracdap-lib-common/src/test/java/org/finos/tracdap/common/auth/JwtProcessorTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2022 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.common.auth;
+
+import org.finos.tracdap.common.exception.EAuthorization;
+
+import com.auth0.jwt.HeaderParams;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+
+
+public class JwtProcessorTest {
+
+    static KeyPair keyPair;
+    JwtProcessor jwt;
+
+    @BeforeAll
+    static void createKeyPair() throws Exception {
+
+        var keySize = 1024;
+        var keyGen = KeyPairGenerator.getInstance("RSA");
+        var random = SecureRandom.getInstance("SHA1PRNG");
+
+        keyGen.initialize(keySize, random);
+
+        keyPair = keyGen.generateKeyPair();
+    }
+
+    @BeforeEach
+    void setupJwt() {
+
+        jwt = JwtProcessor.useKeyPair(keyPair);
+    }
+
+    @Test
+    void jwtRoundTrip() {
+
+        var userInfo = new UserInfo();
+        userInfo.setUserId("fb2876");
+        userInfo.setDisplayName("Fred Blogs Jnr.");
+
+        var token = jwt.encodeToken(userInfo, Duration.of(1, ChronoUnit.HOURS));
+        var rtSession = jwt.decodeAndValidate(token);
+        var rtUserInfo = rtSession.getUserInfo();
+
+        Assertions.assertEquals(userInfo.getUserId(), rtUserInfo.getUserId());
+        Assertions.assertEquals(userInfo.getDisplayName(), rtUserInfo.getDisplayName());
+    }
+
+    @Test
+    void signatureMissing() throws Exception {
+
+        var algorithm = Algorithm.none();
+
+        var issuer = "trac_gateway";
+        var issueTime = Instant.now();
+        var expiryTime = issueTime.plusSeconds(60);
+
+        var header = Map.of(
+                HeaderParams.TYPE, "jwt",
+                HeaderParams.ALGORITHM, algorithm);
+
+        var tokenBuilder = JWT.create()
+                .withHeader(header)
+                .withIssuer(issuer)
+                .withIssuedAt(issueTime)
+                .withExpiresAt(expiryTime)
+                .withSubject("cookie_monster")
+                .withClaim("name", "The Monster who LOOOVES cookies!");
+
+        // Signing with algorithm NONE creates a token with no signature
+        var token = tokenBuilder.sign(algorithm).trim();
+
+        // Decoding an unsigned token should be an auth error
+        Assertions.assertThrows(EAuthorization.class, () -> jwt.decodeAndValidate(token));
+    }
+
+    @Test
+    void signatureNotTrusted() throws Exception {
+
+        // Tokens created with different key pairs should fail to decode and validated
+
+        var keySize = 1024;
+        var keyGen = KeyPairGenerator.getInstance("RSA");
+        var random = SecureRandom.getInstance("SHA1PRNG");
+        keyGen.initialize(keySize, random);
+
+        var altKeyPair = keyGen.generateKeyPair();
+        var altJwt = JwtProcessor.useKeyPair(altKeyPair);
+
+        var userInfo = new UserInfo();
+        userInfo.setUserId("fb2876");
+        userInfo.setDisplayName("Fred Blogs Jnr.");
+
+        var token = jwt.encodeToken(userInfo, Duration.of(1, ChronoUnit.HOURS));
+        var altToken = altJwt.encodeToken(userInfo, Duration.of(1, ChronoUnit.HOURS));
+
+        Assertions.assertThrows(EAuthorization.class, () -> altJwt.decodeAndValidate(token));
+        Assertions.assertThrows(EAuthorization.class, () -> jwt.decodeAndValidate(altToken));
+    }
+
+    @Test
+    void sessionExpired() throws Exception {
+
+        // Attempting to decode and validate after a token expires is an auth error
+
+        var userInfo = new UserInfo();
+        userInfo.setUserId("fb2876");
+        userInfo.setDisplayName("Fred Blogs Jnr.");
+
+        var token = jwt.encodeToken(userInfo, Duration.of(1, ChronoUnit.SECONDS));
+
+        Thread.sleep(2000);
+
+        Assertions.assertThrows(EAuthorization.class, () -> jwt.decodeAndValidate(token));
+    }
+}
