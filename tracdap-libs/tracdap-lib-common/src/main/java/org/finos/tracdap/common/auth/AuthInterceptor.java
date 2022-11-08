6b70a273d8d6d08503aa6e01123b69e4cedeeecf
Martin Traverse <martin.traverse@accenture.com>
2022-11-08 23:28:46 +0000

    Add an auth interceptor for gPRC, using the JwtValidator to test authentication

--- tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/auth/AuthInterceptor.java
+++ tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/auth/AuthInterceptor.java
@@ -17,13 +17,44 @@
 package org.finos.tracdap.common.auth;
 
 import io.grpc.*;
+import org.finos.tracdap.common.config.ConfigKeys;
+import org.finos.tracdap.common.config.ConfigManager;
+import org.finos.tracdap.common.exception.EStartup;
+import org.finos.tracdap.config.AuthenticationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 public class AuthInterceptor implements ServerInterceptor {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
+    private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
+
+    private final JwtValidator jwt;
+
+    public static AuthInterceptor setupAuth(AuthenticationConfig authConfig, ConfigManager configManager) {
+
+        if (configManager.hasSecret(ConfigKeys.TRAC_AUTH_PUBLIC_KEY)) {
+
+            var publicKey = configManager.loadPublicKey(ConfigKeys.TRAC_AUTH_PUBLIC_KEY);
+            var jwt = JwtValidator.configure(authConfig, publicKey);
+
+            return new AuthInterceptor(jwt);
+        }
+        else {
+
+            // Allowing the service to run without validating authentication is hugely risky
+            // Especially because claims can still be added to JWT
+            // The auth-tool utility makes it really easy to set up auth keys on local JKS
+
+            var error = "Root authentication keys are not available, the service will not start";
+            log.error(error);
+            throw new EStartup(error);
+        }
+    }
+
+    AuthInterceptor(JwtValidator jwt) {
+        this.jwt = jwt;
+    }
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT>
@@ -43,17 +74,23 @@ public class AuthInterceptor implements ServerInterceptor {
             call.close(Status.UNAUTHENTICATED, trailers);
         }
 
-        var sessionInfo = JwtHelpers.decodeAndValidate(token);
+        var sessionInfo = jwt.decodeAndValidate(token);
+
+        // If the auth session is invalid, this is still an UNAUTHENTICATED condition
+        // PERMISSION_DENIED would be if auth succeeds, but roles / permissions deny access to resources
 
         if (!sessionInfo.isValid()) {
 
-            log.error("Authentication is not valid: {}", sessionInfo.getErrorMessage());
+            log.error("Authentication failed: {}", sessionInfo.getErrorMessage());
 
-            var status = Status.PERMISSION_DENIED.withDescription(sessionInfo.getErrorMessage());
+            var status = Status.UNAUTHENTICATED.withDescription(sessionInfo.getErrorMessage());
             var trailers = new Metadata();
 
             call.close(status, trailers);
         }
+
+        // Authentication succeeded!
+        // Put the user info object into the context to make it available in the service implementation
 
         var userInfo = sessionInfo.getUserInfo();
         var ctx = Context.current().withValue(AuthConstants.AUTH_CONTEXT_KEY, userInfo);
