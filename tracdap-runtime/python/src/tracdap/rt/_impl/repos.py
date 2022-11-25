b7e31f9b8290de7759ef143854205131e40b4d2a
Martin Traverse <martin.traverse@accenture.com>
2022-11-25 20:35:01 +0000

    PyPi repo testing

--- tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
+++ tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
@@ -16,7 +16,11 @@ from __future__ import annotations
 
 import subprocess
 import subprocess as sp
+import urllib.parse
+
 import time
+
+import requests
 
 import tracdap.rt.metadata as _meta
 import tracdap.rt.config as _cfg
@@ -134,7 +138,8 @@ class GitRepository(IModelRepository):
 
         for git_cmd in git_cmds:
 
-            self._log.info(f"git {' '.join(git_cmd)}")
+            safe_cmd = map(_util.log_safe, git_cmd)
+            self._log.info(f"git {' '.join(safe_cmd)}")
 
             cmd = [*git_cli, *git_cmd]
             cmd_result = sp.run(cmd, cwd=checkout_dir, stdout=sp.PIPE, stderr=sp.PIPE, timeout=self.GIT_TIMEOUT_SECONDS)
@@ -167,12 +172,136 @@ class GitRepository(IModelRepository):
         return self.package_path(model_def, checkout_dir)
 
 
+class PyPiRepository(IModelRepository):
+
+    SIMPLE_PACKAGE_URL = "{SIMPLE_REPO_URL/{PACKAGE_NAME}"
+    JSON_PACKAGE_URL = "{JSON_REPO_URL}/{PACKAGE_NAME}/json"
+    JSON_PACKAGE_VERSION_URL = "{JSON_REPO_URL}/{PACKAGE_NAME}/{PACKAGE_VERSION}/json"
+
+    PIP_INDEX_KEY = "pip-index"
+    PIP_INDEX_URL_KEY = "pip-index-url"
+    REPO_TOKEN_KEY = "token"
+    REPO_USER_KEY = "username"
+    REPO_PASS_KEY = "password"
+
+    def __init__(self, repo_config: _cfg.PluginConfig):
+
+        self._log = _util.logger_for_object(self)
+
+        self._repo_config = repo_config
+
+        if self.PIP_INDEX_KEY not in self._repo_config.properties:
+            raise _ex.EConfigParse(f"Missing required property [{self.PIP_INDEX_KEY}] in PyPi repository config")
+
+    def checkout_key(self, model_def: _meta.ModelDefinition):
+        return model_def.version
+
+    def package_path(
+            self, model_def: _meta.ModelDefinition,
+            checkout_dir: pathlib.Path) -> tp.Optional[pathlib.Path]:
+
+        return checkout_dir.joinpath(model_def.path)
+
+    def get_repo_credentials(self):
+
+        if self.REPO_TOKEN_KEY in self._repo_config.properties:
+            return self._repo_config.properties[self.REPO_TOKEN_KEY]
+
+        if self.REPO_USER_KEY in self._repo_config.properties and self.REPO_PASS_KEY in self._repo_config.properties:
+            username = self._repo_config.properties[self.REPO_USER_KEY]
+            password = self._repo_config.properties[self.REPO_PASS_KEY]
+            return f"{username}:{password}"
+
+        return None
+
+    def do_checkout(self, model_def: _meta.ModelDefinition, checkout_dir: pathlib.Path) -> tp.Optional[pathlib.Path]:
+
+        repo_props = self._repo_config.properties
+        pip_index = repo_props.get(self.PIP_INDEX_KEY)
+
+        if pip_index is None:
+            raise _ex.EConfigParse(f"Missing required property [{self.PIP_INDEX_KEY}] in PyPi repository config")
+
+        pypi_json_root_url = urllib.parse.urlparse(pip_index)
+
+        credentials = self.get_repo_credentials()
+
+        if credentials is not None:
+
+            if pypi_json_root_url.username is None:
+                location = f"{credentials}@{pypi_json_root_url.netloc}"
+            else:
+                location_sep = pypi_json_root_url.netloc.index("@")
+                location = f"{credentials}@{pypi_json_root_url.netloc[location_sep:]}"
+
+            pypi_json_root_url = pypi_json_root_url._replace(netloc=location)
+
+        elif pypi_json_root_url.username:
+            credentials_sep = pypi_json_root_url.netloc.index("@")
+            credentials = pypi_json_root_url.netloc[:credentials_sep]
+
+        self._log.info(
+            f"Checkout from {_util.log_safe_url(pypi_json_root_url)}, " +
+            f"package = [{model_def.package}], version = [{model_def.version}]")
+
+        json_package_version_url = self.JSON_PACKAGE_VERSION_URL.format(
+            JSON_REPO_URL=pypi_json_root_url.geturl(),
+            PACKAGE_NAME=model_def.package,
+            PACKAGE_VERSION=model_def.version)
+
+        json_headers = {"accept": "application/json"}
+
+        package_version_req = requests.get(json_package_version_url, headers=json_headers)
+
+        if package_version_req.status_code != requests.codes.OK:
+            status_code_name = requests.codes.name[package_version_req.status_code]
+            message = f"Package lookup failed: [{package_version_req.status_code}] {status_code_name}"
+            self._log.error(message)
+            raise _ex.EModelRepo(message)  # todo status code for access, not found etc
+
+        package_version_obj = package_version_req.json()
+        package_version_info = package_version_obj.get("info") or {}
+        summary = package_version_info.get("summary") or "(summary not available)"
+
+        self._log.info(summary)
+
+        urls = package_version_obj.get("urls") or []
+        bdist_urls = list(filter(lambda d: d.get("packagetype") == "bdist_wheel", urls))
+
+        if not bdist_urls:
+            message = "No compatible packages found"
+            self._log.error(message)
+            raise _ex.EModelRepo(message)
+
+        if len(bdist_urls) > 1:
+            message = "Multiple compatible packages found (specialized distributions are not supported yet)"
+            self._log.error(message)
+            raise _ex.EModelRepo(message)
+
+        version_url_info = bdist_urls[0]
+        version_filename = version_url_info.get("filename")
+        version_url = urllib.parse.urlparse(version_url_info.get("url"))
+
+        if credentials is not None:
+            version_location = f"{credentials}@{version_url.netloc}"
+            version_url = version_url._replace(netloc=version_location)  # noqa
+
+        self._log.info(f"Downloading [{version_filename}]")
+
+        package_download_req = requests.get(version_url.geturl())
+
+        self._log.info(f"response = {package_download_req.status_code}, size = {len(package_download_req.content)}")
+
+        # TODO: Unfinished
+
+
 class RepositoryManager:
 
     __repo_types: tp.Dict[str, tp.Callable[[_cfg.PluginConfig], IModelRepository]] = {
         "integrated": IntegratedSource,
         "local": LocalRepository,
-        "git": GitRepository
+        "git": GitRepository,
+        "pypi": PyPiRepository
     }
 
     @classmethod
