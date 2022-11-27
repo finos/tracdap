230b86aba46adab7006395d90a250647906b8826
Martin Traverse <martin.traverse@accenture.com>
2022-11-27 16:24:22 +0000

    Unzip .whl downloads

--- tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
+++ tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
@@ -17,8 +17,9 @@ from __future__ import annotations
 import subprocess
 import subprocess as sp
 import urllib.parse
-
 import time
+import zipfile
+import io
 
 import requests
 
@@ -174,9 +175,7 @@ class GitRepository(IModelRepository):
 
 class PyPiRepository(IModelRepository):
 
-    SIMPLE_PACKAGE_URL = "{SIMPLE_REPO_URL/{PACKAGE_NAME}"
-    JSON_PACKAGE_URL = "{JSON_REPO_URL}/{PACKAGE_NAME}/json"
-    JSON_PACKAGE_VERSION_URL = "{JSON_REPO_URL}/{PACKAGE_NAME}/{PACKAGE_VERSION}/json"
+    JSON_PACKAGE_PATH = "{}/{}/{}/json"
 
     PIP_INDEX_KEY = "pip-index"
     PIP_INDEX_URL_KEY = "pip-index-url"
@@ -200,19 +199,38 @@ class PyPiRepository(IModelRepository):
             self, model_def: _meta.ModelDefinition,
             checkout_dir: pathlib.Path) -> tp.Optional[pathlib.Path]:
 
-        return checkout_dir.joinpath(model_def.path)
+        return checkout_dir
 
-    def get_repo_credentials(self):
+    def get_repo_credentials(self, url, properties):
 
         if self.REPO_TOKEN_KEY in self._repo_config.properties:
-            return self._repo_config.properties[self.REPO_TOKEN_KEY]
+            return properties[self.REPO_TOKEN_KEY]
 
-        if self.REPO_USER_KEY in self._repo_config.properties and self.REPO_PASS_KEY in self._repo_config.properties:
-            username = self._repo_config.properties[self.REPO_USER_KEY]
-            password = self._repo_config.properties[self.REPO_PASS_KEY]
+        if self.REPO_USER_KEY in properties and self.REPO_PASS_KEY in properties:
+            username = properties[self.REPO_USER_KEY]
+            password = properties[self.REPO_PASS_KEY]
             return f"{username}:{password}"
 
+        if url.username:
+            credentials_sep = url.netloc.index("@")
+            return url.netloc[:credentials_sep]
+
         return None
+
+    @staticmethod
+    def apply_credentials(url, credentials):
+
+        if credentials is None:
+            return url
+
+        if url.username is None:
+            location = f"{credentials}@{url.netloc}"
+
+        else:
+            location_sep = url.netloc.index("@")
+            location = f"{credentials}@{url.netloc[location_sep:]}"
+
+        return url._replace(netloc=location)
 
     def do_checkout(self, model_def: _meta.ModelDefinition, checkout_dir: pathlib.Path) -> tp.Optional[pathlib.Path]:
 
@@ -222,50 +240,34 @@ class PyPiRepository(IModelRepository):
         if pip_index is None:
             raise _ex.EConfigParse(f"Missing required property [{self.PIP_INDEX_KEY}] in PyPi repository config")
 
-        pypi_json_root_url = urllib.parse.urlparse(pip_index)
+        json_root_url = urllib.parse.urlparse(pip_index)
+        json_package_path = self.JSON_PACKAGE_PATH.format(json_root_url.path, model_def.package, model_def.version)
+        json_package_url = json_root_url._replace(path=json_package_path)
 
-        credentials = self.get_repo_credentials()
-
-        if credentials is not None:
-
-            if pypi_json_root_url.username is None:
-                location = f"{credentials}@{pypi_json_root_url.netloc}"
-            else:
-                location_sep = pypi_json_root_url.netloc.index("@")
-                location = f"{credentials}@{pypi_json_root_url.netloc[location_sep:]}"
-
-            pypi_json_root_url = pypi_json_root_url._replace(netloc=location)
-
-        elif pypi_json_root_url.username:
-            credentials_sep = pypi_json_root_url.netloc.index("@")
-            credentials = pypi_json_root_url.netloc[:credentials_sep]
-
-        self._log.info(
-            f"Checkout from {_util.log_safe_url(pypi_json_root_url)}, " +
-            f"package = [{model_def.package}], version = [{model_def.version}]")
-
-        json_package_version_url = self.JSON_PACKAGE_VERSION_URL.format(
-            JSON_REPO_URL=pypi_json_root_url.geturl(),
-            PACKAGE_NAME=model_def.package,
-            PACKAGE_VERSION=model_def.version)
+        credentials = self.get_repo_credentials(json_root_url, self._repo_config.properties)
+        json_root_url = self.apply_credentials(json_package_url, credentials)
 
         json_headers = {"accept": "application/json"}
 
-        package_version_req = requests.get(json_package_version_url, headers=json_headers)
+        self._log.info(
+            f"Checkout {_util.log_safe_url(json_root_url)}, " +
+            f"package = [{model_def.package}], version = [{model_def.version}]")
 
-        if package_version_req.status_code != requests.codes.OK:
-            status_code_name = requests.codes.name[package_version_req.status_code]
-            message = f"Package lookup failed: [{package_version_req.status_code}] {status_code_name}"
+        package_req = requests.get(json_package_url.geturl(), headers=json_headers)
+
+        if package_req.status_code != requests.codes.OK:
+            status_code_name = requests.codes.name[package_req.status_code]
+            message = f"Package lookup failed: [{package_req.status_code}] {status_code_name}"
             self._log.error(message)
             raise _ex.EModelRepo(message)  # todo status code for access, not found etc
 
-        package_version_obj = package_version_req.json()
-        package_version_info = package_version_obj.get("info") or {}
-        summary = package_version_info.get("summary") or "(summary not available)"
+        package_obj = package_req.json()
+        package_info = package_obj.get("info") or {}
+        summary = package_info.get("summary") or "(summary not available)"
 
         self._log.info(summary)
 
-        urls = package_version_obj.get("urls") or []
+        urls = package_obj.get("urls") or []
         bdist_urls = list(filter(lambda d: d.get("packagetype") == "bdist_wheel", urls))
 
         if not bdist_urls:
@@ -278,21 +280,27 @@ class PyPiRepository(IModelRepository):
             self._log.error(message)
             raise _ex.EModelRepo(message)
 
-        version_url_info = bdist_urls[0]
-        version_filename = version_url_info.get("filename")
-        version_url = urllib.parse.urlparse(version_url_info.get("url"))
+        package_url_info = bdist_urls[0]
+        package_filename = package_url_info.get("filename")
+        package_url = urllib.parse.urlparse(package_url_info.get("url"))
 
-        if credentials is not None:
-            version_location = f"{credentials}@{version_url.netloc}"
-            version_url = version_url._replace(netloc=version_location)  # noqa
+        package_url = self.apply_credentials(package_url, credentials)
 
-        self._log.info(f"Downloading [{version_filename}]")
+        self._log.info(f"Downloading [{package_filename}]")
 
-        package_download_req = requests.get(version_url.geturl())
+        download_req = requests.get(package_url.geturl())
+        content = download_req.content
+        elapsed = download_req.elapsed
 
-        self._log.info(f"response = {package_download_req.status_code}, size = {len(package_download_req.content)}")
+        self._log.info(f"Downloaded [{len(content) / 1024:.1f}] KB in [{elapsed.total_seconds():.1f}] seconds")
 
-        # TODO: Unfinished
+        download_whl = zipfile.ZipFile(io.BytesIO(download_req.content))
+        download_whl.extractall(checkout_dir)
+
+        self._log.info(f"Unpacked [{len(download_whl.filelist)}] files")
+        self._log.info(f"PyPI checkout succeeded for {model_def.package} {model_def.version}")
+
+        return self.package_path(model_def, checkout_dir)
 
 
 class RepositoryManager:
