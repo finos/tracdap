d498d6bdaad75afe836444783e79eefee10b8df5
Martin Traverse <martin.traverse@accenture.com>
2022-12-22 10:46:13 +0000

    WIP latest new auth components

---
+++ tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/AuthProcessor.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2022 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.common.auth.external;
+
+
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.cookie.Cookie;
+import io.netty.handler.codec.http.cookie.ServerCookieDecoder;
+
+import org.finos.tracdap.common.auth.internal.JwtValidator;
+import org.finos.tracdap.common.auth.internal.SessionInfo;
+import org.finos.tracdap.common.auth.internal.UserInfo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AuthProcessor <THeaders extends AuthHeaders> {
+
+    private static final String TRAC_AUTH_TOKEN_HEADER = "trac_auth_token";
+    private static final String TRAC_AUTH_SESSION_EXPIRY_HEADER = "trac_auth_session_expiry";
+    private static final String TRAC_USER_ID_HEADER = "trac_user_idn";
+    private static final String TRAC_USER_NAME_HEADER = "trac_user_name";
+
+    private static final String BEARER_PREFIX = "bearer ";
+
+    private static final boolean API_ROUTE = true;
+    private static final boolean WEB_ROUTE = false;
+
+    private static final List<String> TRAC_GRPC_FILTERED_HEADERS = List.of(
+            TRAC_AUTH_TOKEN_HEADER,
+            TRAC_AUTH_SESSION_EXPIRY_HEADER,
+            TRAC_USER_ID_HEADER,
+            TRAC_USER_NAME_HEADER,
+            HttpHeaderNames.AUTHORIZATION.toString(),
+            HttpHeaderNames.COOKIE.toString());
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final boolean routeeType;
+
+    private final JwtValidator jwt = null;
+
+
+    public AuthProcessor(boolean routeType) {
+        this.routeeType = routeType;
+    }
+
+
+    public SessionInfo checkExistingAuth(AuthHeaders headers) {
+
+//        var cookies = extractCookies(headers);
+//
+//        var token = findTokenInHeaders(headers);
+//        var session = jwt.decodeAndValidate(token);
+
+        return null;
+    }
+
+    public SessionInfo newSession(UserInfo userInfo) {
+        return null;
+    }
+
+    public SessionInfo updateSession(SessionInfo sessionInfo) {
+        return null;
+    }
+
+    public THeaders filterRequestHeaders(THeaders headers) {
+
+        var cookies = extractCookies(headers);
+
+        var filteredHeaders = filterHeaders(headers);
+        var filteredCookies = filterCookies(cookies);
+
+        return null;
+    }
+
+    public THeaders filterResponseHeaders(THeaders headers) {
+        return null;
+    }
+
+    public THeaders addHeadersForPlatform(THeaders header, SessionInfo session) {
+        return null;
+    }
+
+    public THeaders addHeadersForBrowser(THeaders header, SessionInfo session) {
+        return null;
+    }
+
+    public THeaders addHeadersForApi(THeaders headers, SessionInfo session) {
+        return null;
+    }
+
+    private String findTokenInHeaders(AuthHeaders headers, List<Cookie> cookies) {
+
+        var tracAuthHeader = findHeader(headers, TRAC_AUTH_TOKEN_HEADER);
+        if (tracAuthHeader != null) return tracAuthHeader;
+
+        var tracAuthCookie = findCookie(cookies, TRAC_AUTH_TOKEN_HEADER);
+        if (tracAuthCookie != null) return tracAuthCookie;
+
+        var authorizationHeader = findHeader(headers, HttpHeaderNames.AUTHORIZATION.toString());
+        if (authorizationHeader != null) return authorizationHeader;
+
+        var authorizationCookie = findCookie(cookies, HttpHeaderNames.AUTHORIZATION.toString());
+        if (authorizationCookie != null) return authorizationCookie;
+
+        // Run out of places to look!
+
+        return null;
+    }
+
+    private String findHeader(AuthHeaders headers, String headerName) {
+
+        if (headers.contains(headerName))
+            return headers.get(headerName).toString();
+
+        return null;
+    }
+
+    private String findCookie(List<Cookie> cookies, String cookieName) {
+
+        for (var cookie : cookies) {
+            if (cookie.name().equals(cookieName)) {
+                // TODO: Other fields in the cooke can make it specific, domain etc.
+                return cookie.value();
+            }
+        }
+
+        return null;
+    }
+
+    private List<Cookie> extractCookies(THeaders headers) {
+
+        var cookieHeaders = headers.getAll(HttpHeaderNames.COOKIE);
+        var cookies = new ArrayList<Cookie>();
+
+        for (var header : cookieHeaders)
+            cookies.addAll(ServerCookieDecoder.LAX.decodeAll(header.toString()));
+
+        return cookies;
+    }
+
+    private THeaders filterHeaders(THeaders headers) {
+
+        var filtered = headers; //  todo (THeaders) headers.getClass().getConstructor().newInstance();
+
+        for (var header : headers) {
+
+            var headerName = header.getKey().toString().toLowerCase();
+
+            if (TRAC_GRPC_FILTERED_HEADERS.contains(headerName))
+                continue;
+
+            filtered.add(header.getKey(), header.getValue());
+        }
+
+        return filtered;
+    }
+
+    private List<Cookie> filterCookies(List<Cookie> cookies) {
+
+        var filtered = new ArrayList<Cookie>();
+
+        for (var cookie : cookies) {
+
+            var cookieName = cookie.name().toLowerCase();
+
+            if (TRAC_GRPC_FILTERED_HEADERS.contains(cookieName))
+                continue;
+
+            filtered.add(cookie);
+        }
+
+        return filtered;
+    }
+}
---
+++ tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/Http1Auth.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2022 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.common.auth.external;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import io.netty.handler.codec.http.*;
+
+import io.netty.util.ReferenceCountUtil;
+import org.finos.tracdap.common.auth.internal.SessionInfo;
+import org.finos.tracdap.common.exception.EAuthorization;
+import org.finos.tracdap.common.exception.EUnexpected;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+
+public class Http1Auth extends ChannelDuplexHandler {
+
+    public static final boolean FRONT_FACING = true;
+    public static final boolean BACK_FACING = false;
+
+    public static final boolean BROWSER_ROUTE = false;
+    public static final boolean API_ROUTE = true;
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final int connId;
+    private final boolean authDirection;
+    private final boolean routeType;
+
+    private final AuthProcessor<HeaderDecorator> authProcessor;
+    private final IAuthProviderNew browserAuthProvider;
+    private final IAuthProviderNew apiAuthProvider;
+
+    private AuthResult authResult = AuthResult.FAILED();
+    private SessionInfo session;
+
+
+    public Http1Auth(
+            int connId, boolean direction, boolean routeType,
+            IAuthProviderNew browserAuthProvider,
+            IAuthProviderNew apiAuthProvider) {
+
+        this.connId = connId;
+        this.authDirection = direction;
+        this.routeType = routeType;
+
+        this.authProcessor = new AuthProcessor<>(routeType);
+        this.browserAuthProvider = browserAuthProvider;
+        this.apiAuthProvider = apiAuthProvider;
+    }
+
+    @Override
+    public void channelRead(@Nonnull ChannelHandlerContext ctx, @Nonnull Object msg) {
+
+        bidiMessageHandler(ctx, msg, null, FRONT_FACING);
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+
+        bidiMessageHandler(ctx, msg, promise, BACK_FACING);
+    }
+
+    private void bidiMessageHandler(ChannelHandlerContext ctx, Object msg, ChannelPromise promise, boolean msgDirection) {
+
+        try {
+            // HTTP/1 auth works purely on the request object
+
+            if ((msg instanceof HttpRequest)) {
+                var request = (HttpRequest) msg;
+                processRequest(ctx, request, promise);
+            }
+
+            // If authorization failed, drop messages silently after the request
+
+            if (authResult.getCode() != AuthResultCode.AUTHORIZED)
+                return;
+
+            if (msg instanceof HttpResponse) {
+                var response = (HttpResponse) msg;
+                processResponse(ctx, response, promise);
+            }
+
+            // Content messages need special handling, to account for "full" HTTP request / response messages
+            // These include the headers and body all in one message
+            // Since the headers are already processed, we need to separate the body
+
+            if (msg instanceof HttpContent) {
+
+                var originalMsg = (HttpContent) msg;
+                var content = originalMsg.content().retain();
+
+                var relayMsg = (msg instanceof LastHttpContent)
+                        ? new DefaultLastHttpContent(content)
+                        : new DefaultHttpContent(content);
+
+                bidiSend(ctx, relayMsg, promise, msgDirection);
+            }
+
+            // Any other message types are unexpected, are not impossible and should be allowed
+            // E.g. signalling with custom events or control frames for alternate protocols
+            // Anyway we know we have request, response and auth already
+
+            else if (!(msg instanceof HttpRequest) && !(msg instanceof HttpResponse)) {
+
+                bidiSend(ctx, msg, promise, msgDirection);
+            }
+        }
+        finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void bidiSend(ChannelHandlerContext ctx, Object msg, ChannelPromise promise, boolean direction) {
+
+        if (direction == FRONT_FACING)
+            ctx.fireChannelRead(msg);
+        else
+            ctx.write(msg, promise);
+    }
+
+    private void bidiFlush(ChannelHandlerContext ctx, boolean direction) {
+
+        if (direction == FRONT_FACING)
+            ctx.fireChannelReadComplete();
+        else
+            ctx.flush();
+    }
+
+    private void processRequest(ChannelHandlerContext ctx, HttpRequest request, ChannelPromise promise) {
+
+        // TRAC auth token takes priority -look for this first, then try to validate
+
+        var headers = new HeaderDecorator(request.headers());
+
+        session = authProcessor.checkExistingAuth(headers);
+
+        // If the TRAC token is not available or not valid, fall back to a primary auth mechanism (if possible)
+
+        if (session != null) {
+            authResult = AuthResult.AUTHORIZED(session.getUserInfo());
+        }
+        else if (routeType == BROWSER_ROUTE && browserAuthProvider != null) {
+            authResult = browserAuthProvider.attemptAuth(ctx, headers);
+        }
+        else if (routeType == API_ROUTE  && apiAuthProvider != null) {
+            authResult = apiAuthProvider.attemptAuth(ctx, headers);
+        }
+        else {
+            authResult = AuthResult.FAILED();
+        }
+
+        // If necessary, create or update the user session depending on the auth result
+
+        if (authResult.getCode() == AuthResultCode.AUTHORIZED) {
+
+            if (session == null)
+                session = authProcessor.newSession(authResult.getUserInfo());
+            else
+                session = authProcessor.updateSession(session);
+
+            // Since authorization succeeded we can forward on the request message
+            forwardRequest(ctx, request, promise);
+        }
+
+        else {
+
+            rejectRequest(ctx, request, promise);
+        }
+    }
+
+    private void forwardRequest(ChannelHandlerContext ctx, HttpRequest request, ChannelPromise promise) {
+
+        // Strip out any existing auth headers and other noise
+        // This message is heading to the core platform, so it only needs the TRAC auth info
+
+        var original = new HeaderDecorator(request.headers());
+        var filtered = authProcessor.filterRequestHeaders(original);
+        var headers = authProcessor.addHeadersForPlatform(filtered, session);
+
+        var modifiedRequest = new DefaultHttpRequest(
+                request.protocolVersion(), request.method(),
+                request.uri(), headers);
+
+        // For front-facing handlers requests come on the read side, back-facing is reversed
+
+        bidiSend(ctx, modifiedRequest, promise, authDirection);
+    }
+
+    private void rejectRequest(ChannelHandlerContext ctx, HttpRequest request, ChannelPromise promise) {
+
+        switch (authResult.getCode()) {
+
+            case REDIRECTED:
+
+                // primary auth provider already responded to the client
+                // We don't need to do anything, just drop the messages
+
+                // What should we do with the promise on a back-facing channel?
+                // It should fail, but if the user is redirecting, we don't want to crash and hang up the connection!
+
+                if (authDirection == BACK_FACING)
+                    promise.setFailure(new EAuthorization("Authorization in progress, please retry after"));
+
+                break;
+
+            case FAILED:
+
+                // Basic unauthorized response
+                // Could try to put the auth result message here maybe?
+
+                var responseHeaders = new DefaultHttpHeaders();
+                var response = new DefaultHttpResponse(
+                        request.protocolVersion(),
+                        HttpResponseStatus.UNAUTHORIZED,
+                        responseHeaders);
+
+                bidiSend(ctx, response, promise, authDirection);
+                bidiFlush(ctx, authDirection);
+
+                break;
+
+            default:
+
+                throw new EUnexpected();
+        }
+    }
+
+    private void processResponse(ChannelHandlerContext ctx, HttpResponse response, ChannelPromise promise) {
+
+        // First filter out any auth-related headers from the response
+
+        var original = new HeaderDecorator(response.headers());
+        var filtered = authProcessor.filterResponseHeaders(original);
+
+        // Now add in headers for the TRAC token mechanism
+        // Headers are different from browsers and API clients
+
+        HeaderDecorator headers;
+
+        if (routeType == BROWSER_ROUTE)
+            headers = authProcessor.addHeadersForBrowser(filtered, session);
+        else
+            headers = authProcessor.addHeadersForApi(filtered, session);
+
+        var modifiedResponse = new DefaultHttpResponse(response.protocolVersion(), response.status(), headers);
+
+        // This is a response message, so invert the direction flag !!
+
+        bidiSend(ctx, modifiedResponse, promise, !authDirection);
+    }
+
+    public static final class HeaderDecorator extends DefaultHttpHeaders implements AuthHeaders{
+
+        public HeaderDecorator(HttpHeaders headers) {
+            for (var header : headers)
+                add((CharSequence) header.getKey(), header.getValue());
+        }
+
+        @Override
+        public void add(CharSequence header, CharSequence value) {
+            super.add(header, value);
+        }
+
+        @Override
+        public boolean contains(CharSequence header) {
+            return super.contains(header);
+        }
+
+        @Override
+        public String get(CharSequence header) {
+            return super.get(header);
+        }
+
+        @Override
+        public List<String> getAll(CharSequence header) {
+            return super.getAll(header);
+        }
+
+        @Override
+        public void forEach(Consumer<? super Map.Entry<String, String>> action) {
+            super.forEach(action);
+        }
+
+        @Override
+        public Spliterator<Map.Entry<String, String>> spliterator() {
+            return super.spliterator();
+        }
+    }
+}
