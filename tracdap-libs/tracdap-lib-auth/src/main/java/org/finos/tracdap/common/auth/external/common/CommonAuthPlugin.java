3fd9da8561a091c2a8e1a284a488abd8d270ed89
Martin Traverse <martin.traverse@accenture.com>
2022-12-30 20:55:27 +0000

    Remove old auth handler and provider interface

--- tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/common/CommonAuthPlugin.java
+++ tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/common/CommonAuthPlugin.java
@@ -20,7 +20,6 @@ import org.finos.tracdap.common.auth.external.IAuthProvider;
 import org.finos.tracdap.common.exception.EUnexpected;
 import org.finos.tracdap.common.plugin.PluginServiceInfo;
 import org.finos.tracdap.common.plugin.TracPlugin;
-import org.finos.tracdap.common.auth.external.OldAuthProviderInterface;
 
 import java.util.List;
 import java.util.Properties;
