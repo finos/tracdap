889c864a3d916a1117a827d76da91a5a2e511afb
Martin Traverse <martin.traverse@accenture.com>
2022-12-31 14:18:32 +0000

    Fix refresh of invalid sessions

--- tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/Http1AuthHandler.java
+++ tracdap-libs/tracdap-lib-auth/src/main/java/org/finos/tracdap/common/auth/external/Http1AuthHandler.java
@@ -135,43 +135,45 @@ public class Http1AuthHandler extends ChannelDuplexHandler {
         token = AuthLogic.findTracAuthToken(headers, AuthLogic.SERVER_COOKIE);
         session = (token != null) ? jwtProcessor.decodeAndValidate(token) : null;
 
-        // If the TRAC token is not available or not valid, fall back to a primary auth mechanism (if possible)
-
-        // We want to know if this is request is for web browsing or an API call
-        // It will affect how primary auth is done and how auth responses are sent back
-        // This method is crude but effective, to be 100% we need the matcher used int the routers...
-
-        var isApi =
-                headers.contains(HttpHeaderNames.CONTENT_TYPE) &&
-                headers.get(HttpHeaderNames.CONTENT_TYPE).startsWith("application/");
-
+        // If the token gives a valid session then authentication has succeeded
         if (session != null && session.isValid()) {
-            authResult = AuthResult.AUTHORIZED(session.getUserInfo());
-        }
-        else {
-            // Only one auth provider available atm, for both browser and API routes
-            authResult = authProvider.attemptAuth(ctx, headers);
-        }
 
-        // If necessary, create or update the user session depending on the auth result
+            // Check to see if the token needs refreshing
+            var sessionUpdate = AuthLogic.refreshSession(session, authConfig);
 
-        if (authResult.getCode() == AuthResultCode.AUTHORIZED) {
-
-            // Session update is created for new sessions, or sessions that have ticket past their refresh time
-            var sessionUpdate = (session == null)
-                    ? AuthLogic.newSession(authResult.getUserInfo(), authConfig)
-                    : AuthLogic.refreshSession(session, authConfig);
-
-            // If the session has been updated, regenerate the token and store the new details
             if (sessionUpdate != session) {
                 token = jwtProcessor.encodeToken(sessionUpdate);
                 session = sessionUpdate;
             }
+
+            authResult = AuthResult.AUTHORIZED(session.getUserInfo());
         }
+
+        // If the TRAC token is not available or not valid, fall back to the primary auth mechanism
+        else {
+
+            if (session == null)
+                log.info("conn = {}, authentication required (no session available)", connId);
+            else
+                log.info("conn = {}, authentication required ({})", connId, session.getErrorMessage());
+
+            // Only one auth provider available atm, for both browser and API routes
+            authResult = authProvider.attemptAuth(ctx, headers);
+
+            // If primary auth succeeded, set up the session token
+            if (authResult.getCode() == AuthResultCode.AUTHORIZED) {
+
+                session = AuthLogic.newSession(authResult.getUserInfo(), authConfig);
+                token = jwtProcessor.encodeToken(session);
+            }
+        }
+
+        // Send a basic error response for authentication failures for now
+        // If the result is REDIRECTED the auth provider already responded, so no need to respond again here
 
         if (authResult.getCode() == AuthResultCode.FAILED) {
 
-            log.error("conn = {}, authentication failed", connId);
+            log.error("conn = {}, authentication failed ({})", connId, authResult.getMessage());
 
             // Basic unauthorized response
             // Could try to put the auth result message here maybe?
@@ -182,8 +184,6 @@ public class Http1AuthHandler extends ChannelDuplexHandler {
                     HttpResponseStatus.UNAUTHORIZED,
                     responseHeaders);
 
-            // Sending a response, so invert the auth direction
-
             ctx.write(response);
             ctx.flush();
             ctx.close();
@@ -192,6 +192,10 @@ public class Http1AuthHandler extends ChannelDuplexHandler {
         // Decide whether to send the auth response as headers or cookies
         // Always send cookies for browser routes
         // For API routes the client can set a header to prefer cookies in the response
+
+        var isApi =
+                headers.contains(HttpHeaderNames.CONTENT_TYPE) &&
+                headers.get(HttpHeaderNames.CONTENT_TYPE).startsWith("application/");
 
         wantCookies = !isApi || headers.contains(AuthLogic.TRAC_AUTH_COOKIES_HEADER);
     }
