d63779ba1ae075c4f9d8d0b7d8f89285f647c0c8
Martin Traverse <martin.traverse@accenture.com>
2023-01-10 14:21:14 +0000

    Implement HTML format for PyPI simple protocol

--- tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
+++ tracdap-runtime/python/src/tracdap/rt/_impl/repos.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+import html.parser
 import json.decoder
 import re
 import subprocess
@@ -286,6 +287,7 @@ class PyPiRepository(IModelRepository):
             package_filename, package_url = self._pypi_json_query(model_def)
 
         self._log.info(f"Downloading [{package_filename}]")
+        self._log.info(f"GET: {_util.log_safe_url(package_url)}")
 
         download_req = requests.get(package_url.geturl())
         content = download_req.content
@@ -306,31 +308,44 @@ class PyPiRepository(IModelRepository):
         # PEP describing PyPI simple protocol
         # https://peps.python.org/pep-0691/
 
-        simple_root_url = urllib.parse.urlparse(self._pip_index_url)
-        simple_content_type = self._pypi_simple_content_type()
-        simple_headers = {"accept": simple_content_type}
+        try:
 
-        credentials = _get_credentials(simple_root_url, self._repo_config)
+            simple_root_url = urllib.parse.urlparse(self._pip_index_url)
+            simple_content_type = self._pypi_simple_content_type()
+            simple_headers = {"accept": simple_content_type}
 
-        package_req = self._pypi_package_query(
-            self.SIMPLE_PACKAGE_PATH, simple_root_url, simple_headers,
-            credentials, model_def)
+            credentials = _get_credentials(simple_root_url, self._repo_config)
 
-        received_content_type = package_req.headers.get("content-type")
+            self._log.info(f"Query package: [{model_def.package}]")
 
-        if received_content_type == self.PIP_SIMPLE_TYPE_JSON:
-            filename, url = self._pypi_simple_parse_json(package_req, model_def)
-        elif received_content_type == self.PIP_SIMPLE_TYPE_HTML:
-            filename, url = self._pypi_simple_parse_html(package_req, model_def)
-        else:
-            err = f"Invalid response from package repository: Content type = [{received_content_type}]"
-            self._log.error(err)
-            raise _ex.EModelRepo(err)
+            package_req = self._pypi_package_query(
+                self.SIMPLE_PACKAGE_PATH, simple_root_url, simple_headers,
+                credentials, model_def)
 
-        package_url = urllib.parse.urlparse(url)
-        package_url = _apply_credentials(package_url, credentials)
+            received_content_type = package_req.headers.get("content-type")
 
-        return filename, package_url
+            if received_content_type == self.PIP_SIMPLE_TYPE_JSON:
+                filename, url = self._pypi_simple_parse_response(model_def, package_req.json())
+
+            elif received_content_type == self.PIP_SIMPLE_TYPE_HTML:
+                package_parser = _PypiSimpleHtmlParser(model_def.package, package_req.url)
+                package_parser.feed(package_req.text)
+                filename, url = self._pypi_simple_parse_response(model_def, package_parser.response)
+
+            else:
+                err = f"Invalid response from package repository: Content type = [{received_content_type}]"
+                self._log.error(err)
+                raise _ex.EModelRepo(err)
+
+            package_url = urllib.parse.urlparse(url)
+            package_url = _apply_credentials(package_url, credentials)
+
+            return filename, package_url
+
+        except json.JSONDecodeError as e:
+            msg = f"Invalid response from model repository: {str(e)}"
+            self._log.error(msg)
+            raise _ex.EModelRepo("Invalid response from model repository") from e
 
     def _pypi_simple_content_type(self):
 
@@ -344,105 +359,47 @@ class PyPiRepository(IModelRepository):
 
         return self.PIP_SIMPLE_CONTENT_TYPES[self.PIP_SIMPLE_FORMAT_DEFAULT]
 
-    def _pypi_simple_parse_json(self, request, model_def):
+    def _pypi_simple_parse_response(self, model_def, package_obj):
 
-        try:
+        package_name = package_obj.get("name") or model_def.package
+        files = package_obj.get("files") or []
 
-            package_obj = request.json()
-            package_name = package_obj.get("name") or ""
-            files = package_obj.get("files") or []
+        name_pattern = package_name.replace("-", "_")  # dash is replaced by underscore in wheel names
+        version_pattern = model_def.version.replace(".", "\\.")
+        file_pattern = re.compile(f"^{name_pattern}-{version_pattern}-(?P<target>.*)\\.whl")
 
-            name_pattern = package_name.replace("-", "_")  # dash is replaced by underscore in wheel names
-            version_pattern = model_def.version.replace(".", "\\.")
-            file_pattern = re.compile(f"^{name_pattern}-{version_pattern}-(?P<target>.*)\\.whl")
+        matches = []
 
-            matches = []
+        for file_info in files:
 
-            for file_info in files:
+            filename = file_info.get("filename") or ""
+            yanked = file_info.get("yanked") or False
+            match = file_pattern.match(filename)
 
-                filename = file_info.get("filename") or ""
-                yanked = file_info.get("yanked")
-                match = file_pattern.match(filename)
+            if match and not yanked:
 
-                if match and not yanked:
+                url = file_info.get("url")
+                target = match.group("target")
 
-                    url = file_info.get("url")
-                    target = match.group("target")
+                match_info = filename, url, target
+                matches.append(match_info)
 
-                    match_info = filename, url, target
-                    matches.append(match_info)
+        if len(matches) == 0:
+            message = f"No package found for [{package_name}] version [{model_def.version}]"
+            self._log.error(message)
+            raise _ex.EModelRepo(message)
 
-            if len(matches) == 0:
-                message = f"No package found for [{package_name}] version [{model_def.version}]"
-                self._log.error(message)
-                raise _ex.EModelRepo(message)
+        if len(matches) > 1:
+            message = f"Multiple packages found for [{package_name}] version [{model_def.version}]" + \
+                      f" (targets: " + ", ".join(map(lambda m: m[2], matches)) + ")"
+            self._log.error(message)
+            raise _ex.EModelRepo(message)
 
-            if len(matches) > 1:
-                message = f"Multiple packages found for [{package_name}] version [{model_def.version}]" + \
-                          f" (targets: " + ", ".join(map(lambda m: m[2], matches)) + ")"
-                self._log.error(message)
-                raise _ex.EModelRepo(message)
+        filename, url, target = matches[0]
 
-            filename, url, target = matches[0]
+        self._log.info(f"Found package [{package_name}] version [{model_def.version}], target = [{target}]")
 
-            self._log.info(f"Found package [{package_name}] version [{model_def.version}], target = [{target}]")
-
-            return filename, url
-
-        except json.decoder.JSONDecodeError as e:
-
-            self._log.error(f"Invalid response from package repository: {str(e)}")
-            raise _ex.EModelRepo("Invalid response from package repository") from e
-
-    def _pypi_simple_parse_html(self, request, model_def):
-
-        try:
-
-            package_obj = request.text
-            package_name = package_obj.get("name") or ""
-            files = package_obj.get("files") or []
-
-            name_pattern = package_name.replace("-", "_")  # dash is replaced by underscore in wheel names
-            version_pattern = model_def.version.replace(".", "\\.")
-            file_pattern = re.compile(f"^{name_pattern}-{version_pattern}-(?P<target>.*)\\.whl")
-
-            matches = []
-
-            for file_info in files:
-
-                filename = file_info.get("filename") or ""
-                yanked = file_info.get("yanked")
-                match = file_pattern.match(filename)
-
-                if match and not yanked:
-
-                    url = file_info.get("url")
-                    target = match.group("target")
-
-                    match_info = filename, url, target
-                    matches.append(match_info)
-
-            if len(matches) == 0:
-                message = f"No package found for [{package_name}] version [{model_def.version}]"
-                self._log.error(message)
-                raise _ex.EModelRepo(message)
-
-            if len(matches) > 1:
-                message = f"Multiple packages found for [{package_name}] version [{model_def.version}]" + \
-                          f" (targets: " + ", ".join(map(lambda m: m[2], matches)) + ")"
-                self._log.error(message)
-                raise _ex.EModelRepo(message)
-
-            filename, url, target = matches[0]
-
-            self._log.info(f"Found package [{package_name}] version [{model_def.version}], target = [{target}]")
-
-            return filename, url
-
-        except json.decoder.JSONDecodeError as e:  # todo
-
-            self._log.error(f"Invalid response from package repository: {str(e)}")
-            raise _ex.EModelRepo("Invalid response from package repository") from e
+        return filename, url
 
     def _pypi_json_query(self, model_def: _meta.ModelDefinition):
 
@@ -450,6 +407,8 @@ class PyPiRepository(IModelRepository):
         json_headers = {"accept": "application/json"}
 
         credentials = _get_credentials(json_root_url, self._repo_config)
+
+        self._log.info(f"Query package: [{model_def.package}], version = [{model_def.version}]")
 
         package_req = self._pypi_package_query(
             self.JSON_PACKAGE_PATH, json_root_url, json_headers,
@@ -487,7 +446,7 @@ class PyPiRepository(IModelRepository):
         package_path = package_path_template.format(root_url.path, model_def.package, model_def.version)
         package_url = root_url._replace(path=package_path)
 
-        self._log.info(f"Package query: {_util.log_safe_url(package_url)}")
+        self._log.info(f"GET: {_util.log_safe_url(package_url)}")
 
         package_req = requests.get(package_url.geturl(), headers=headers)
 
@@ -497,6 +456,98 @@ class PyPiRepository(IModelRepository):
             raise _ex.EModelRepo(message)  # todo status code for access, not found etc
 
         return package_req
+
+
+class _PypiSimpleHtmlParser(html.parser.HTMLParser):
+
+    def __init__(self, package_name: str, package_url: str):
+
+        super().__init__()
+
+        self._package_url = package_url
+        self._stack = []
+        self._data = None
+
+        # Empty response object as per PEP-691
+        # https://peps.python.org/pep-0691/#project-detail
+
+        self.response = {
+            "meta": {"api-version": None},
+            "name": package_name,
+            "files": []
+        }
+
+    def handle_starttag(self, tag: str, attrs: list[tuple[str, str | None]]):
+
+        element = (tag, attrs)
+        self._stack.append(element)
+        self._data = None
+
+        if tag == "meta" and any(map(lambda a: a[0] == "name" and a[1] == "pypi:repository-version", attrs)):
+            self._record_meta(attrs)
+
+    def handle_endtag(self, tag: str):
+
+        stack_tag, stack_attrs = self._stack.pop()
+
+        if tag == "a" and stack_tag == "a" and self._data is not None:
+            self._record_link(self._data, stack_attrs)
+
+        while stack_tag != tag and len(self._stack) > 0:
+            stack_tag, _ = self._stack.pop()
+
+    def handle_data(self, data: str):
+
+        stripped_data = data.strip()
+        self._data = stripped_data if len(stripped_data) > 0 else None
+
+    def _record_meta(self, attrs: list[tuple[str, str | None]]):
+
+        content_attr = list(filter(lambda a: a[0] == "content", attrs))
+
+        if len(content_attr) == 1:
+            content, version = content_attr[0]
+            self.response["meta"]["api-version"] = version
+
+    def _record_link(self, filename, attrs: list[tuple[str, str | None]]):
+
+        href_attr = list(filter(lambda a: a[0] == "href", attrs))
+        yanked_attr = list(filter(lambda a: a[0] == "data-yanked", attrs))
+        python_version_attr = list(filter(lambda a: a[0] == "data-requires-python", attrs))
+
+        if len(href_attr) != 1:
+            return
+
+        _, href_url = href_attr[0]
+        is_yanked = any(yanked_attr)
+        _, required_python = python_version_attr[0] if len(python_version_attr) == 1 else (None, None)
+
+        if "#" in href_url:
+            sep = href_url.index("#")
+            hash_info = href_url[sep + 1:].split("=")
+            href_url = href_url[:sep]
+        else:
+            hash_info = None
+
+        # URL in the HREF attribute can be absolute or relative
+        file_url = urllib.parse.urljoin(self._package_url, href_url)
+
+        if hash_info and len(hash_info) == 2:
+            hashes = {hash_info[0]: hash_info[1]}
+        else:
+            hashes = {}
+
+        file_info = {
+            "filename": filename,
+            "url": file_url,
+            "hashes": hashes,
+            "yanked": is_yanked
+        }
+
+        self.response["files"].append(file_info)
+
+
+# TODO: Move Git and PyPI repos into _plugins
 
 
 class RepositoryManager:
