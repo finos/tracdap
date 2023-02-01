e8bd6e909cd505c5dc9f70d502a3bcf52182adb4
Martin Traverse <martin.traverse@accenture.com>
2023-02-01 00:50:50 +0000

    Implementation of Git in Python using the dulwich library

--- tracdap-runtime/python/src/tracdap/rt/_plugins/repo_git.py
+++ tracdap-runtime/python/src/tracdap/rt/_plugins/repo_git.py
@@ -13,10 +13,14 @@
 #  limitations under the License.
 
 import pathlib
+import re
 import subprocess as sp
 import typing as tp
 import urllib.parse
 import time
+
+import dulwich.porcelain as porcelain
+import dulwich.index as git_index
 
 import tracdap.rt.metadata as meta
 import tracdap.rt.exceptions as ex
@@ -32,7 +36,11 @@ from . import _helpers
 class GitRepository(IModelRepository):
 
     REPO_URL_KEY = "repoUrl"
+    PURE_PYTHON_KEY = "purePython"
+
     GIT_TIMEOUT_SECONDS = 30
+
+    PURE_PYTHON_CONFIG_PATTERN = re.compile("^purePython\\.([^.]+)\\.(.+)")
 
     def __init__(self, properties: tp.Dict[str, str]):
 
@@ -40,6 +48,7 @@ class GitRepository(IModelRepository):
         self._log = _helpers.logger_for_object(self)
 
         repo_url_prop = _helpers.get_plugin_property(self._properties, self.REPO_URL_KEY)
+        pure_python_prop = _helpers.get_plugin_property(self._properties, self.PURE_PYTHON_KEY)
 
         if not repo_url_prop:
             raise ex.EConfigParse(f"Missing required property [{self.REPO_URL_KEY}] in Git repository config")
@@ -48,6 +57,11 @@ class GitRepository(IModelRepository):
         credentials = _helpers.get_http_credentials(repo_url, self._properties)
 
         self._repo_url = _helpers.apply_http_credentials(repo_url, credentials)
+
+        if pure_python_prop:
+            self._pure_python = pure_python_prop.strip().lower() == "true"
+        else:
+            self._pure_python = False
 
     def package_path(
             self, model_def: meta.ModelDefinition,
@@ -62,6 +76,19 @@ class GitRepository(IModelRepository):
             f"group = [{model_def.packageGroup}], package = [{model_def.package}], version = [{model_def.version}]")
 
         self._log.info(f"Checkout location: [{checkout_dir}]")
+
+        if self._pure_python:
+            package_path = self._do_python_checkout(model_def, checkout_dir)
+        else:
+            package_path = self._do_native_checkout(model_def, checkout_dir)
+
+        self._log.info(f"Git checkout succeeded for {model_def.package} {model_def.version}")
+
+        return package_path
+
+    def _do_native_checkout(self, model_def: meta.ModelDefinition, checkout_dir: pathlib.Path) -> pathlib.Path:
+
+        self._log.info(f"Checkout mechanism: [native]")
 
         git_cli = ["git", "-C", str(checkout_dir)]
 
@@ -91,7 +118,7 @@ class GitRepository(IModelRepository):
         for git_cmd in git_cmds:
 
             safe_cmd = map(_helpers.log_safe, git_cmd)
-            self._log.info(f"git {' '.join(safe_cmd)}")
+            self._log.info(f"=> git {' '.join(safe_cmd)}")
 
             cmd = [*git_cli, *git_cmd]
             cmd_result = sp.run(cmd, cwd=checkout_dir, stdout=sp.PIPE, stderr=sp.PIPE, timeout=self.GIT_TIMEOUT_SECONDS)
@@ -119,9 +146,56 @@ class GitRepository(IModelRepository):
                 self._log.error(error_msg)
                 raise ex.EModelRepo(error_msg)
 
-        self._log.info(f"Git checkout succeeded for {model_def.package} {model_def.version}")
+        return self.package_path(model_def, checkout_dir)
+
+    def _do_python_checkout(self, model_def: meta.ModelDefinition, checkout_dir: pathlib.Path) -> pathlib.Path:
+
+        self._log.info(f"Checkout mechanism: [python]")
+
+        repo = porcelain.init(checkout_dir)
+
+        self._apply_dulwich_config(repo)
+
+        porcelain.remote_add(repo, "origin", self._repo_url.geturl())
+        porcelain.fetch(repo, "origin")
+
+        commit_key = self._ref_key(model_def.version)
+        tag_key = self._ref_key(f"refs/tags/{model_def.version}")
+        branch_key = self._ref_key(f"refs/remotes/origin/{model_def.version}")
+
+        if commit_key in repo:
+            repo[b"HEAD"] = commit_key
+        elif tag_key in repo:
+            repo[b"HEAD"] = repo[tag_key]
+        elif branch_key in repo:
+            repo[b"HEAD"] = repo[branch_key]
+        else:
+            raise ex.EModelRepo(f"Model version not found: [{model_def.version}]")
+
+        index_file = repo.index_path()
+        tree = repo[b"HEAD"].tree
+        git_index.build_index_from_tree(repo.path, index_file, repo.object_store, tree)
 
         return self.package_path(model_def, checkout_dir)
+
+    def _apply_dulwich_config(self, repo: porcelain.Repo):
+
+        config = repo.get_config()
+
+        for key, value in self._properties.items():
+
+            match = self.PURE_PYTHON_CONFIG_PATTERN.match(key)
+
+            if match:
+                section = match.group(1)
+                name = match.group(2)
+                config.set(section, name, value)
+
+        config.write_to_path()
+
+    @staticmethod
+    def _ref_key(key):
+        return bytes(key, "ascii")
 
 
 # Register plugin
