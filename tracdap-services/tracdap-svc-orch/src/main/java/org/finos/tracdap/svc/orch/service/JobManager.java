d27d9412e16181321968ed8fe67de4e5bc0ef9b0
Martin Traverse <martin.traverse@accenture.com>
2023-02-04 19:36:49 +0000

    New job processor and job manager implementation

---
+++ tracdap-services/tracdap-svc-orch/src/main/java/org/finos/tracdap/svc/orch/service/JobManager.java
@@ -0,0 +1,356 @@
+/*
+ * Copyright 2023 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.svc.orch.service;
+
+import org.finos.tracdap.common.exception.*;
+import org.finos.tracdap.common.exec.ExecutorJobInfo;
+import org.finos.tracdap.common.metadata.MetadataUtil;
+import org.finos.tracdap.metadata.JobStatusCode;
+import org.finos.tracdap.svc.orch.cache.IJobCache;
+
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+public class JobManager {
+
+    public static final Duration LAUNCH_DURATION = Duration.of(2, ChronoUnit.MINUTES);
+
+    public static final int DEFAULT_JOB_LIMIT = 6;
+    public static final Duration DEFAULT_CACHE_POLL_INTERVAL = Duration.of(2, ChronoUnit.SECONDS);
+    public static final Duration DEFAULT_EXECUTOR_POLL_INTERVAL = Duration.of(30, ChronoUnit.SECONDS);
+    public static final Duration STARTUP_DELAY = Duration.of(10, ChronoUnit.SECONDS);
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final JobProcessor processor;
+    private final IJobCache<JobState> cache;
+    private final EventExecutorGroup javaExecutor;
+
+    private final int jobLimit;
+    private final Duration cachePollInterval;
+    private final Duration executorPollInterval;
+
+    private ScheduledFuture<?> cachePollingTask = null;
+    private ScheduledFuture<?> executorPollingTask = null;
+
+    public JobManager(
+            JobProcessor processor,
+            IJobCache<JobState> cache,
+            EventExecutorGroup javaExecutor) {
+
+        this.processor = processor;
+        this.cache = cache;
+        this.javaExecutor = javaExecutor;
+
+        jobLimit = DEFAULT_JOB_LIMIT;
+        cachePollInterval = DEFAULT_CACHE_POLL_INTERVAL;
+        executorPollInterval = DEFAULT_EXECUTOR_POLL_INTERVAL;
+    }
+
+    public void start() {
+
+        try {
+
+            log.info("Starting job manager service...");
+
+            // Delay initial polls by the polling interval
+            // This is to prevent polling while the service is still starting
+
+            cachePollingTask = javaExecutor.scheduleAtFixedRate(
+                    this::pollCache,
+                    STARTUP_DELAY.getSeconds(),
+                    cachePollInterval.getSeconds(),
+                    TimeUnit.SECONDS);
+
+            executorPollingTask = javaExecutor.scheduleAtFixedRate(
+                    this::pollExecutor,
+                    STARTUP_DELAY.getSeconds(),
+                    executorPollInterval.getSeconds(),
+                    TimeUnit.SECONDS);
+
+            log.info("Job manager service started OK");
+        }
+        catch (RejectedExecutionException e) {
+
+            log.error("Job manager service failed to start: {}", e.getMessage(), e);
+            throw new EStartup("Job manager service failed to start", e);
+        }
+    }
+
+    public void stop() {
+
+        log.info("Stopping job manager service...");
+
+        if (cachePollingTask != null) {
+            cachePollingTask.cancel(false);
+        }
+
+        if (executorPollingTask != null) {
+            executorPollingTask.cancel(false);
+        }
+    }
+
+    public void pollCache() {
+
+        try {
+
+            // Process all available job updates
+            // Any updates that get scheduled twice will be ignored as superseded
+
+            // We could put some more intelligent logic here, e.g. an update queue / capacity
+            // Or filter down the query so not all nodes attempt all updates
+            // But the ticket.superseded() mechanism should be sufficient unless the load is extreme
+
+            var updatedJobs = cache.queryState(STATUS_FOR_UPDATE);
+
+            for (var update : updatedJobs)
+                javaExecutor.submit(() -> processJobUpdate(update.key(), update.revision()));
+
+            // Jobs that are ready to launch get special handling
+
+            pollForLaunch();
+        }
+        catch (Exception e) {
+
+            log.warn("There was an error polling the job cache: " + e.getMessage(), e);
+            log.warn("Polling operation will be retried periodically");
+
+            // TODO: Track successive polling errors, take action after a threshold
+        }
+    }
+
+    public void pollForLaunch() {
+
+        try {
+
+            var runningJobs = cache.queryState(STATUS_FOR_RUNNING_JOBS);
+
+            var launchCapacity = jobLimit - runningJobs.size();
+            if (launchCapacity <= 0)
+                return;
+
+            var launchableJobs = cache.queryState(STATUS_FOR_LAUNCH);
+
+            // Select the first jobs in the queue up to the available capacity
+            // Launch selection could be a lot more intelligent!!!
+
+            var selectedJobs = launchableJobs.size() > launchCapacity
+                    ? launchableJobs.subList(0, launchCapacity)
+                    : launchableJobs;
+
+            for (var job : selectedJobs)
+                javaExecutor.submit(() -> launchJob(job.key(), job.revision()));
+        }
+        catch (Exception e) {
+
+            log.warn("There was an error polling the executor: " + e.getMessage(), e);
+            log.warn("Polling operation will be retried periodically");
+
+            // TODO: Track successive polling errors, take action after a threshold
+        }
+    }
+
+    public void pollExecutor() {
+
+        try {
+
+            var runningStates = List.of("");
+            var runningJobs = cache.queryState(runningStates);
+
+            var pollRequests = runningJobs.stream()
+                    .map(j -> Map.entry(j.key(), j.value()))
+                    .collect(Collectors.toList());
+
+            var pollResults = processor.pollExecutorJobs(pollRequests);
+
+            for (var i = 0; i < runningJobs.size(); i++) {
+
+                var job = runningJobs.get(i);
+                var pollResult = pollResults.get(i);
+
+                if (pollResult.getStatus() != job.value().batchStatus)
+                    javaExecutor.submit(() -> recordPollResult(job.key(), job.revision(), pollResult));
+            }
+        }
+        catch (Exception e) {
+
+            log.warn("There was an error polling the executor: " + e.getMessage());
+            log.warn("Polling operation will be retried periodically");
+
+            // TODO: Track successive polling errors, take action after a threshold
+        }
+    }
+
+    public JobState addNewJob(JobState jobState) {
+
+        var jobKey = MetadataUtil.objectKey(jobState.jobId);
+
+        try (var ticket = cache.openNewTicket(jobKey)) {
+
+            // Duplicate job key, should never happen
+            if (ticket.superseded())
+                throw new ECacheTicket("Job could not be created because it already exists");
+
+            var newState = processor.saveInitialMetadata(jobState);
+
+            newState.tracStatus = JobStatusCode.QUEUED;
+            newState.cacheStatus = CacheStatus.QUEUED_IN_TRAC;
+
+            cache.addEntry(ticket, newState.cacheStatus, newState);
+
+            return newState;
+        }
+        catch (Exception e) {
+
+            // This method is called from the public API
+            // Let errors propagate back to the client
+
+            var message = String.format("Job was not accepted: [%s] %s", jobState.jobKey, e.getMessage());
+            log.error(message);
+
+            throw new EJobFailure(message, e);
+        }
+    }
+
+    public JobState queryJob(String jobKey) {
+
+        var cacheEntry = cache.getLatestEntry(jobKey);
+
+        return cacheEntry != null ? cacheEntry.value() : null;
+    }
+
+    public void launchJob(String jobKey, int revision) {
+
+        try (var ticket = cache.openTicket(jobKey, revision, LAUNCH_DURATION)) {
+
+            if (ticket.superseded())
+                return;
+
+            var cacheEntry = cache.getEntry(ticket);
+            var jobState = cacheEntry.value();
+            var newState = processor.launchJob(jobState);
+
+            cache.updateEntry(ticket, newState.cacheStatus, newState);
+        }
+        catch (Exception e) {
+
+            log.warn("There was a problem launching the job: " + e.getMessage());
+            log.warn("Launch operation will be retried");
+        }
+    }
+
+    public void recordPollResult(String jobKey, int revision, ExecutorJobInfo executorJobInfo) {
+
+        try (var ticket = cache.openTicket(jobKey, revision)) {
+
+            if (ticket.superseded())
+                return;
+
+            var cacheEntry = cache.getEntry(ticket);
+            var jobState = cacheEntry.value();
+            var newState = processor.recordPollStatus(jobState, executorJobInfo);
+
+            cache.updateEntry(ticket, newState.cacheStatus, newState);
+        }
+        catch (Exception e) {
+
+            log.warn("There was a problem polling the job: " + e.getMessage());
+            log.warn("Poll operation will be retried");
+        }
+    }
+
+    public void processJobUpdate(String jobKey, int revision) {
+
+        try (var ticket = cache.openTicket(jobKey, revision)) {
+
+            if (ticket.superseded())
+                return;
+
+            var cacheEntry = cache.getEntry(ticket);
+            var jobState = cacheEntry.value();
+
+            if (jobState.cacheStatus.equals(CacheStatus.READY_TO_REMOVE)) {
+                cache.removeEntry(ticket);
+                return;
+            }
+
+            var updateFunc = getUpdateFunc(jobState.cacheStatus);
+            var newState = updateFunc.apply(jobState);
+
+            cache.updateEntry(ticket, newState.cacheStatus, newState);
+        }
+        catch (Exception e) {
+
+            log.warn("There was a problem processing the job: " + e.getMessage());
+            log.warn("The operation will be retried");
+        }
+    }
+
+    private Function<JobState, JobState> getUpdateFunc(String cacheStatus) {
+
+        if (!STATUS_FOR_UPDATE.contains(cacheStatus))
+            throw new EUnexpected();
+
+        if (cacheStatus.equals(CacheStatus.EXECUTOR_COMPLETE) || cacheStatus.equals(CacheStatus.EXECUTOR_SUCCEEDED))
+            return processor::fetchJobResult;
+
+        if (cacheStatus.equals(CacheStatus.EXECUTOR_FAILED))
+            return processor::saveResultMetadata;
+
+        if (cacheStatus.equals(CacheStatus.RESULTS_RECEIVED) || cacheStatus.equals(CacheStatus.RESULTS_INVALID))
+            return processor::saveResultMetadata;
+
+        if (cacheStatus.equals(CacheStatus.RESULTS_SAVED))
+            return processor::cleanUpJob;
+
+        // Should never be called, but not an error
+        if (cacheStatus.equals(CacheStatus.READY_TO_REMOVE))
+            return Function.identity();
+
+        throw new EUnexpected();
+    }
+
+    private static final List<String> STATUS_FOR_LAUNCH = List.of(
+            CacheStatus.QUEUED_IN_TRAC);
+
+    private static final List<String> STATUS_FOR_RUNNING_JOBS = List.of(
+            CacheStatus.SENT_TO_EXECUTOR,
+            CacheStatus.QUEUED_IN_EXECUTOR,
+            CacheStatus.RUNNING_IN_EXECUTOR);
+
+    private static final List<String> STATUS_FOR_UPDATE = List.of(
+            CacheStatus.EXECUTOR_COMPLETE,
+            CacheStatus.EXECUTOR_SUCCEEDED,
+            CacheStatus.EXECUTOR_FAILED,
+            CacheStatus.RESULTS_RECEIVED,
+            CacheStatus.RESULTS_INVALID,
+            CacheStatus.RESULTS_SAVED,
+            CacheStatus.READY_TO_REMOVE);
+}
---
+++ tracdap-services/tracdap-svc-orch/src/main/java/org/finos/tracdap/svc/orch/service/JobProcessor.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright 2023 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.svc.orch.service;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+import org.finos.tracdap.common.config.ConfigFormat;
+import org.finos.tracdap.common.config.ConfigParser;
+import org.finos.tracdap.common.exception.*;
+import org.finos.tracdap.common.exec.*;
+import org.finos.tracdap.common.validation.Validator;
+import org.finos.tracdap.config.JobResult;
+import org.finos.tracdap.metadata.JobStatusCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+public class JobProcessor {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final IBatchExecutor<?> executor;
+    private final Validator validator = new Validator();
+
+    // TODO: Refactor into this class
+    private final JobLifecycle lifecycle;
+
+
+    public JobProcessor(IBatchExecutor<?> executor, JobLifecycle lifecycle) {
+
+        this.executor = executor;
+        this.lifecycle = lifecycle;
+    }
+
+
+    // Metadata actions
+
+    public JobState assembleAndValidate(JobState jobState) {
+
+        var newState = jobState.clone();
+
+        newState = lifecycle.assembleAndValidate(newState);
+
+        newState.tracStatus = JobStatusCode.VALIDATED;
+
+        return newState;
+    }
+
+    public JobState saveInitialMetadata(JobState jobState) {
+
+        var newState = jobState.clone();
+
+        newState = lifecycle.saveInitialMetadata(newState);
+
+        newState.tracStatus = JobStatusCode.QUEUED;
+        newState.cacheStatus = CacheStatus.QUEUED_IN_TRAC;
+
+        return newState;
+    }
+
+    public JobState updateMetadata(JobState jobState) {
+
+        // TODO: Intermediary job updates not currently reported
+
+        return jobState;
+    }
+
+    public JobState saveResultMetadata(JobState jobState) {
+
+        // TRAC job status must already be set before calling lifecycle
+
+        lifecycle.processJobResult(jobState);
+
+        var newState = jobState.clone();
+        newState.cacheStatus = CacheStatus.RESULTS_SAVED;
+
+        return newState;
+    }
+
+
+    // Executor actions
+
+    public JobState launchJob(JobState jobState) {
+
+        // TODO: Use a submission ID to avoid clash on repeat
+
+        var jobKey =  jobState.jobKey;
+
+        log.info("LAUNCH JOB: [{}]", jobKey);
+
+        var batchExecutor = stronglyTypedExecutor();
+        var batchState = batchExecutor.createBatch(jobKey);
+
+        batchState = batchExecutor.createVolume(jobKey, batchState, "config", ExecutorVolumeType.CONFIG_DIR);
+        batchState = batchExecutor.createVolume(jobKey, batchState, "result", ExecutorVolumeType.RESULT_DIR);
+        batchState = batchExecutor.createVolume(jobKey, batchState, "log", ExecutorVolumeType.RESULT_DIR);
+        batchState = batchExecutor.createVolume(jobKey, batchState, "scratch", ExecutorVolumeType.SCRATCH_DIR);
+
+        // No specialisation is needed to build the job config
+        // This may change in the future, in which case add IJobLogic.buildJobConfig()
+
+        var jobConfigJson = ConfigParser.quoteConfig(jobState.jobConfig, ConfigFormat.JSON);
+        var sysConfigJson = ConfigParser.quoteConfig(jobState.sysConfig, ConfigFormat.JSON);
+        batchState = batchExecutor.writeFile(jobKey, batchState, "config", "job_config.json", jobConfigJson);
+        batchState = batchExecutor.writeFile(jobKey, batchState, "config", "sys_config.json", sysConfigJson);
+
+        var launchCmd = LaunchCmd.trac();
+
+        var launchArgs = List.of(
+                LaunchArg.string("--sys-config"), LaunchArg.path("config", "sys_config.json"),
+                LaunchArg.string("--job-config"), LaunchArg.path("config", "job_config.json"),
+                LaunchArg.string("--job-result-dir"), LaunchArg.path("result", "."),
+                LaunchArg.string("--job-result-format"), LaunchArg.string("json"),
+                LaunchArg.string("--scratch-dir"), LaunchArg.path("scratch", "."));
+
+        batchState = batchExecutor.startBatch(jobKey, batchState, launchCmd, launchArgs);
+
+        var newState = jobState.clone();
+        newState.tracStatus = JobStatusCode.SUBMITTED;
+        newState.cacheStatus = CacheStatus.SENT_TO_EXECUTOR;
+        newState.batchStatus = ExecutorJobStatus.STATUS_UNKNOWN;
+        newState.batchState = batchState.toByteArray();
+
+        log.info("LAUNCH JOB SUCCEEDED: [{}]", jobKey);
+
+        return newState;
+    }
+
+    public JobState recordPollStatus(JobState jobState, ExecutorJobInfo batchInfo) {
+
+        var newState = jobState.clone();
+        newState.batchStatus = batchInfo.getStatus();
+
+        log.info("EXECUTOR STATE UPDATE: [{}] {}", newState.jobKey, batchInfo.getStatus());
+
+        switch (batchInfo.getStatus()) {
+
+            case QUEUED:
+                newState.tracStatus = JobStatusCode.PENDING;  // QUEUED_IN_EXECUTOR todo
+                newState.cacheStatus = CacheStatus.QUEUED_IN_EXECUTOR;
+                break;
+
+            case RUNNING:
+                newState.tracStatus = JobStatusCode.RUNNING;
+                newState.cacheStatus = CacheStatus.RUNNING_IN_EXECUTOR;
+                break;
+
+            case COMPLETE:
+                newState.tracStatus = JobStatusCode.FINISHING;
+                newState.cacheStatus = CacheStatus.EXECUTOR_COMPLETE;
+                break;
+
+            case SUCCEEDED:
+                newState.tracStatus = JobStatusCode.FINISHING;
+                newState.cacheStatus = CacheStatus.EXECUTOR_SUCCEEDED;
+                break;
+
+            case FAILED:
+                newState.tracStatus = JobStatusCode.FAILED;
+                newState.cacheStatus = CacheStatus.EXECUTOR_FAILED;
+                newState.statusMessage = batchInfo.getStatusMessage();
+                newState.errorDetail = batchInfo.getErrorDetail();
+
+                log.error("Execution failed for [{}]: {}", newState.jobKey, batchInfo.getStatusMessage());
+                log.error("Error detail for [{}]\n{}", newState.jobKey, batchInfo.getErrorDetail());
+
+                break;
+
+            // TODO: Handling for CANCELLED
+            // Cancellation is not supported yet, but will need to be handled here when it is
+
+            // If executor status could not be determined, treat this as a failure
+            // This will cause the job to be reported as failed and trigger job cleanup
+
+            // TODO: Can we allow STATUS_UNKNOWN to happen a few times before recording a failure?
+            // E.g. to handle intermittent errors talking to the executor
+
+            case STATUS_UNKNOWN:
+            default:
+
+                newState.tracStatus = JobStatusCode.FAILED;
+                newState.cacheStatus = CacheStatus.EXECUTOR_FAILED;
+                newState.statusMessage = "Job status could not be determined";
+        }
+
+        return newState;
+    }
+
+    public JobState fetchJobResult(JobState jobState) {
+
+        var jobKey = jobState.jobKey;
+
+        log.info("FETCH JOB RESULT: [{}]", jobKey);
+
+        var batchExecutor = stronglyTypedExecutor();
+        var batchState = stronglyTypedState(batchExecutor, jobState.batchState);
+
+        try {
+
+            var resultFile = String.format("job_result_%s.json", jobKey);
+            var resultBytes = batchExecutor.readFile(jobKey, batchState, "result", resultFile);
+
+            var results = ConfigParser.parseConfig(resultBytes, ConfigFormat.JSON, JobResult.class);
+
+            // If the validator is extended to cover the config interface,
+            // The top level job result could be validated directly
+
+            for (var result : results.getResultsMap().entrySet()) {
+
+                log.info("Validating job result [{}]", result.getKey());
+                validator.validateFixedObject(result.getValue());
+            }
+
+            var newState = jobState.clone();
+            newState.jobResult = results;
+            newState.tracStatus = JobStatusCode.SUCCEEDED;
+            newState.cacheStatus = CacheStatus.RESULTS_RECEIVED;
+
+            log.info("FETCH JOB RESULT SUCCEEDED: [{}]", jobKey);
+
+            return newState;
+        }
+        catch (EConfigParse | EValidation e) {
+
+            // Parsing and validation failures mean the job has definitely failed
+            // Handle these as part of the result processing
+            // These are not executor / communication errors and should not be retried
+
+            var errorMessage = e.getMessage();
+            var shortMessage = errorMessage.lines().findFirst().orElse("No details available");
+
+            var newState = jobState.clone();
+            newState.tracStatus = JobStatusCode.FAILED;
+            newState.cacheStatus = CacheStatus.RESULTS_INVALID;
+            newState.statusMessage = shortMessage;
+
+            log.error("FETCH JOB RESULT FAILED: [{}] {}", jobKey, shortMessage, e);
+
+            return newState;
+        }
+    }
+
+    public JobState cleanUpJob(JobState jobState) {
+
+        log.info("CLEAN UP JOB: [{}]", jobState.jobKey);
+
+        var batchExecutor = stronglyTypedExecutor();
+        var batchState = stronglyTypedState(batchExecutor, jobState.batchState);
+
+        batchExecutor.destroyBatch(jobState.jobKey, batchState);
+
+        var newState = jobState.clone();
+        newState.cacheStatus = CacheStatus.READY_TO_REMOVE;
+        newState.batchStatus = ExecutorJobStatus.STATUS_UNKNOWN;
+        newState.batchState = null;
+
+        log.info("CLEAN UP JOB SUCCEEDED: [{}]", jobState.jobKey);
+
+        return newState;
+    }
+
+    // Executor polling
+
+    public List<ExecutorJobInfo> pollExecutorJobs(List<Map.Entry<String, JobState>> jobs) {
+
+        var executor = stronglyTypedExecutor();
+
+        var jobState = jobs.stream()
+                .map(j -> Map.entry(j.getKey(), stronglyTypedState(executor, j.getValue().batchState)))
+                .collect(Collectors.toList());
+
+        return executor.pollBatches(jobState);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <TState extends Message>
+    IBatchExecutor<TState> stronglyTypedExecutor() {
+        return (IBatchExecutor<TState>) executor;
+    }
+
+    private <TState extends Message>
+    TState stronglyTypedState(IBatchExecutor<TState> batchExecutor, byte[] stateBytes) {
+        try {
+            return batchExecutor.stateDecoder().parseFrom(stateBytes);
+        }
+        catch (InvalidProtocolBufferException e) {
+            throw new ETracInternal("Invalid job state: " + e.getMessage(), e);
+        }
+    }
+}
