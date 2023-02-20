ad12091960be4457498c627d9c3ee860458de187
Martin Traverse <martin.traverse@accenture.com>
2023-02-20 18:23:21 +0000

    Feature / Shim loader update (#332)
    * Updated version of the shim loader
    
    * Fix embedded jobs test (path changed when test was moved recently)
    
    * Update shim loading tests
    
    * Add shim loader test for multiple scopes

--- tracdap-runtime/python/test_data/shim_test_2/scope_2/pkg_1/sub_1/model.py
+++ tracdap-runtime/python/test_data/shim_test_2/scope_2/pkg_1/sub_1/model.py
@@ -1,4 +1,4 @@
-#  Copyright 2022 Accenture Global Solutions Limited
+#  Copyright 2023 Accenture Global Solutions Limited
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -11,3 +11,11 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
+
+import pkg_2.sub_2.utils as utils
+
+
+class SampleModel:
+
+    def use_utils(self):
+        return utils.get_scope()
