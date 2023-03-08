b3f014f4948bffb1a586b9bdeecf3f32f6da08e6
Piotr Szajowski <16238431+pszajows@users.noreply.github.com>
2023-03-08 07:54:28 +0100

    WIP

--- tracdap-services/tracdap-svc-meta/src/test/java/org/finos/tracdap/svc/meta/dal/MetadataDalEncodingTest.java
+++ tracdap-services/tracdap-svc-meta/src/test/java/org/finos/tracdap/svc/meta/dal/MetadataDalEncodingTest.java
@@ -97,9 +97,6 @@ abstract class MetadataDalEncodingTest implements IDalTestable {
         var attrValue = objectOfType(attrType);
 
         var testTag = origTag.toBuilder()
-                .setHeader(origTag.getHeader().toBuilder()
-                        .setIsLatestTag(true) // TODO: replicate this in other tests
-                )
                 .putAttrs(attrName, MetadataCodec.encodeNativeObject(attrValue))
                 .build();
 
