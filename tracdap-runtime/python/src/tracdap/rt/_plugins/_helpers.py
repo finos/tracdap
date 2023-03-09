627c3509c4df9ab09ae84972462827916e6d6171
Martin Traverse <martin.traverse@accenture.com>
2023-03-09 18:54:01 +0000

    Plugin helper for reading boolean properties

--- tracdap-runtime/python/src/tracdap/rt/_plugins/_helpers.py
+++ tracdap-runtime/python/src/tracdap/rt/_plugins/_helpers.py
@@ -22,6 +22,8 @@ import platform
 import urllib.parse
 import typing as tp
 
+import tracdap.rt.exceptions as _ex
+
 
 def get_plugin_property(properties: tp.Dict[str, str], property_name: str):
 
@@ -38,6 +40,30 @@ def get_plugin_property(properties: tp.Dict[str, str], property_name: str):
                 return value
 
     return None
+
+
+def get_plugin_property_boolean(properties: tp.Dict[str, str], property_name: str, property_default: bool = False):
+
+    property_value = get_plugin_property(properties, property_name)
+
+    if property_value is None:
+        return property_default
+
+    if isinstance(property_value, bool):
+        return property_value
+
+    if isinstance(property_value, str):
+
+        if len(property_value.strip()) == 0:
+            return property_default
+
+        if property_value.strip().lower() == "true":
+            return True
+
+        if property_value.strip().lower() == "false":
+            return False
+
+    raise _ex.EConfigParse(f"Invalid value for [{property_name}]: Expected a boolean value, got [{property_value}]")
 
 
 # Handling for credentials supplied via HTTP(S) URLs
