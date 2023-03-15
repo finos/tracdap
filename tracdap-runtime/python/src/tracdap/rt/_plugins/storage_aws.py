b7e2f6c07ebc1b42678276749585b93483c0b522
Martin Traverse <martin.traverse@accenture.com>
2023-03-15 12:34:04 +0000

    Handle missing bucket name AWS runtime storage

--- tracdap-runtime/python/src/tracdap/rt/_plugins/storage_aws.py
+++ tracdap-runtime/python/src/tracdap/rt/_plugins/storage_aws.py
@@ -91,6 +91,12 @@ class AwsStorageProvider(IStorageProvider):
 
         bucket = _helpers.get_plugin_property(self._properties, self.BUCKET_PROPERTY)
         prefix = _helpers.get_plugin_property(self._properties, self.PREFIX_PROPERTY)
+
+        if bucket is None or len(bucket.strip()) == 0:
+            message = f"Missing required config property [{self.BUCKET_PROPERTY}] for S3 storage"
+            self._log.error(message)
+            raise ex.EConfigParse(message)
+
         root_path = f"{bucket}/{prefix}" if prefix else bucket
 
         return afs.SubTreeFileSystem(root_path, s3fs)
@@ -179,6 +185,11 @@ if boto_available:
             self._properties = config.properties
             self._bucket = _helpers.get_plugin_property(self._properties, AwsStorageProvider.BUCKET_PROPERTY)
             self._prefix = _helpers.get_plugin_property(self._properties, AwsStorageProvider.PREFIX_PROPERTY) or ""
+
+            if self._bucket is None or len(self._bucket.strip()) == 0:
+                message = f"Missing required config property [{AwsStorageProvider.BUCKET_PROPERTY}] for S3 storage"
+                self._log.error(message)
+                raise ex.EConfigParse(message)
 
             self._client = boto3.client(**client_args)
 
