b14f31c87bbf295d2362297fd429e3a6f23cd7f8
Martin Traverse <martin.traverse@accenture.com>
2023-04-07 13:00:34 +0100

    Work on common file storage

--- tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/storage/CommonFileStorage.java
+++ tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/storage/CommonFileStorage.java
@@ -16,14 +16,17 @@
 
 package org.finos.tracdap.common.storage;
 
-import io.netty.buffer.ByteBuf;
+import io.netty.channel.EventLoopGroup;
 import org.finos.tracdap.common.concurrent.IExecutionContext;
 import org.finos.tracdap.common.data.IDataContext;
+import org.finos.tracdap.common.exception.ETrac;
+
+import io.netty.buffer.ByteBuf;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.InvalidPathException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,49 +36,152 @@ import java.util.concurrent.Flow;
 
 import static org.finos.tracdap.common.storage.StorageErrors.ExplicitError.*;
 
-public abstract class CommonFileStorage implements IFileStorage {
 
-    protected final StoragePath rootPath;
+public class CommonFileStorage implements IFileStorage {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final String storageType;
+    private final String storageKey;
+    private final String rootPath;
+
+    private final IFileStorage fs;
     private final StorageErrors errors;
 
-    protected CommonFileStorage(StoragePath rootPath) {
+    protected CommonFileStorage(
+            String storageType, String storageKey, String rootPath,
+            IFileStorage fs, StorageErrors errors) {
+
+        this.storageType = storageType;
+        this.storageKey = storageKey;
         this.rootPath = rootPath;
-    }
 
-    protected abstract CompletionStage<Boolean> _exists(StoragePath storagePath);
-
-
-
-    @Override
-    public CompletionStage<Boolean> exists(String storagePath, IExecutionContext execContext) {
-
-
+        this.fs = fs;
+        this.errors = errors;
     }
 
     @Override
-    public CompletionStage<Long> size(String storagePath, IExecutionContext execContext) {
-        return null;
+    public void start(EventLoopGroup eventLoopGroup) {
+
+        log.info("INIT [{}]: Common file storage, fs = [{}], root = [{}]", storageKey, storageType, rootPath);
+
+        fs.start(eventLoopGroup);
     }
 
     @Override
-    public CompletionStage<FileStat> stat(String storagePath, IExecutionContext execContext) {
-        return null;
+    public void stop() {
+
+        fs.stop();
     }
 
     @Override
-    public CompletionStage<List<FileStat>> ls(String storagePath, IExecutionContext execContext) {
-        return null;
+    public CompletionStage<Boolean>
+    exists(String storagePath, IExecutionContext ctx) {
+
+        return wrapOperation("EXISTS", storagePath, (op, path) -> _exists(op, path, ctx));
+    }
+
+    private CompletionStage<Boolean>
+    _exists(String operationName, String storagePath, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, true);
+
+        return fs.exists(resolvedPath, ctx);
     }
 
     @Override
-    public CompletionStage<Void> mkdir(String storagePath, boolean recursive, IExecutionContext execContext) {
-        return null;
+    public CompletionStage<Long>
+    size(String storagePath, IExecutionContext ctx) {
+
+        return wrapOperation("SIZE", storagePath, (op, path) -> _size(op, path, ctx));
+    }
+
+    private CompletionStage<Long>
+    _size(String operationName, String storagePath, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, true);
+        var stat = fs.stat(resolvedPath, ctx);
+
+        return stat.thenApply(stat_ -> {
+
+            if (stat_.fileType != FileType.FILE)
+                throw errors.explicitError(SIZE_OF_DIR, storagePath, operationName);  // Todo
+
+            return stat_.size;
+        });
     }
 
     @Override
-    public CompletionStage<Void> rm(String storagePath, boolean recursive, IExecutionContext execContext) {
+    public CompletionStage<FileStat>
+    stat(String storagePath, IExecutionContext ctx) {
+
+        return wrapOperation("STAT", storagePath, (op, path) -> _stat(op, path, ctx));
+    }
+
+    private CompletionStage<FileStat>
+    _stat(String operationName, String storagePath, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, true);
+
+        return fs.stat(resolvedPath, ctx);
+    }
+
+
+    @Override
+    public CompletionStage<List<FileStat>>
+    ls(String storagePath, IExecutionContext ctx) {
+
+        return wrapOperation("LS", storagePath, (op, path) -> _ls(op, path, false, ctx));
+    }
+
+    private CompletionStage<List<FileStat>>
+    _ls(String operationName, String storagePath, boolean recursive, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, true);
+        var stat = fs.stat(resolvedPath, ctx);
+
+        return stat.thenCompose(fi -> {
+
+            if (fi.fileType == FileType.FILE)
+                return CompletableFuture.completedFuture(List.of(fi));
+
+            return fs.ls(resolvedPath, ctx);  // todo recursive
+        });
+    }
+
+    @Override
+    public CompletionStage<Void> mkdir(String storagePath, boolean recursive, IExecutionContext ctx) {
+
+        return wrapOperation("MKDIR", storagePath, (op, path) -> _mkdir(op, path, recursive, ctx));
+    }
+
+    private CompletionStage<Void>
+    _mkdir(String operationName, String storagePath, Boolean recursive, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, false);
+        var exists = fs.exists(resolvedPath, ctx);
+
+        return exists.thenCompose(exists_ -> {
+
+            if (exists_)
+                throw errors.explicitError(FILE_ALREADY_EXISTS_EXCEPTION, storagePath, operationName);
+
+            return fs.mkdir(resolvedPath, recursive, ctx);
+        });
+    }
+
+    @Override
+    public CompletionStage<Void>
+    rm(String storagePath, boolean recursive, IExecutionContext ctx) {
+
+        return wrapOperation("RM", storagePath, (op, path) -> _rm(op, path, recursive, ctx));
+    }
+
+    private CompletionStage<Void>
+    _rm(String operationName, String storagePath, boolean recursive, IExecutionContext ctx) {
+
+        var resolvedPath = resolvePath(operationName, storagePath, false);
+
         return null;
     }
 
@@ -89,7 +195,30 @@ public abstract class CommonFileStorage implements IFileStorage {
         return null;
     }
 
-    private String resolvePath(String storagePath, boolean allowRootDir, String operationName) {
+    private <TResult> CompletionStage<TResult>
+    wrapOperation(String operationName, String storagePath, FsOperation<TResult> func) {
+
+        var storageKey = "";
+        var operation = String.format("%s %s [%s]", operationName, storageKey, storagePath);
+
+        try {
+
+            log.info(operation);
+
+            return func.call(operationName, storagePath);
+        }
+        catch (ETrac e) {
+            log.error("{}: {}", operation, e.getMessage());
+            throw e;
+        }
+        catch (Exception e) {
+            var error = errors.handleException(e, storagePath, operationName);
+            log.error("{}: {}", operation, error.getMessage());
+            throw error;
+        }
+    }
+
+    private String resolvePath(String operationName, String storagePath, boolean allowRootDir) {
 
 
         try {
@@ -132,7 +261,13 @@ public abstract class CommonFileStorage implements IFileStorage {
         }
         catch (InvalidPathException e) {
 
-            throw errors.explicitError(STORAGE_PATH_INVALID, storagePath, operationName);  // todo: cause
+            throw errors.exception(STORAGE_PATH_INVALID, e, storagePath, operationName);
         }
+    }
+
+    @FunctionalInterface
+    private interface FsOperation<TResult> {
+
+        CompletionStage<TResult> call(String operationName, String storagePath);
     }
 }
