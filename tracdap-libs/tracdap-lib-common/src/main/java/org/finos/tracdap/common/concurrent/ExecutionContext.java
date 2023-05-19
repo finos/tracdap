9697a988158b515325e907a8fdbda65811e20d9d
Martin Traverse <martin.traverse@accenture.com>
2023-05-19 12:48:58 +0100

    Remove old code for handling EL mapping

--- tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/concurrent/ExecutionContext.java
+++ tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/concurrent/ExecutionContext.java
@@ -16,14 +16,10 @@
 
 package org.finos.tracdap.common.concurrent;
 
-import io.grpc.*;
 import io.netty.util.concurrent.OrderedEventExecutor;
 
-import java.util.concurrent.Executor;
 
 public class ExecutionContext implements IExecutionContext {
-
-    public static final Context.Key<IExecutionContext> EXEC_CONTEXT_KEY = Context.key("TRAC_EXEC_CONTEXT");
 
     private final OrderedEventExecutor eventLoopExecutor;
 
@@ -34,35 +30,5 @@ public class ExecutionContext implements IExecutionContext {
     @Override
     public OrderedEventExecutor eventLoopExecutor() {
         return eventLoopExecutor;
-    }
-
-    public ServerCallExecutorSupplier eventLoopServerCall() {
-
-        return new ServerCallExecutorSupplier() {
-
-            @Override
-            public <ReqT, RespT>
-            Executor getExecutor(ServerCall<ReqT, RespT> call, Metadata metadata) {
-
-                return eventLoopExecutor;
-            }
-        };
-    }
-
-    public ClientInterceptor eventLoopClientCall() {
-
-        return new ClientInterceptor() {
-
-            @Override
-            public <ReqT, RespT>
-            ClientCall<ReqT, RespT> interceptCall(
-                    MethodDescriptor<ReqT, RespT> method,
-                    CallOptions callOptions,
-                    Channel next) {
-
-                var options = callOptions.withExecutor(eventLoopExecutor);
-                return next.newCall(method, options);
-            }
-        };
     }
 }
