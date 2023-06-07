cd83faddf64d0fa6c13cad3a3634fd709540c4f1
Martin Traverse <martin.traverse@accenture.com>
2023-06-07 16:20:47 +0100

    Add a test case for the REST API to create, search for and retrieve a FLOW

--- tracdap-services/tracdap-gateway/src/test/java/org/finos/tracdap/gateway/proxy/rest/RestProxyTest.java
+++ tracdap-services/tracdap-gateway/src/test/java/org/finos/tracdap/gateway/proxy/rest/RestProxyTest.java
@@ -16,33 +16,41 @@
 
 package org.finos.tracdap.gateway.proxy.rest;
 
-import org.finos.tracdap.api.ListTenantsResponse;
-import org.finos.tracdap.api.PlatformInfoResponse;
+import org.finos.tracdap.api.*;
 import org.finos.tracdap.gateway.proxy.http.Http1Client;
+import org.finos.tracdap.metadata.Tag;
+import org.finos.tracdap.metadata.TagHeader;
 import org.finos.tracdap.test.helpers.PlatformTest;
 
 import com.google.protobuf.Message;
 import com.google.protobuf.util.JsonFormat;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
+import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpHeaderNames;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpScheme;
 
+import org.finos.tracdap.test.helpers.ResourceHelpers;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 import static io.netty.util.NetUtil.LOCALHOST;
 import static org.finos.tracdap.test.meta.TestData.TEST_TENANT;
+import static org.finos.tracdap.test.meta.TestData.selectorForTag;
 
 
 public class RestProxyTest {
 
     public static final short TEST_GW_PORT = 8080;
-    private static final long TEST_TIMEOUT = 10 * 1000;  // 10 second timeout
+    public static final long TEST_TIMEOUT = 10 * 1000;  // 10 second timeout
 
     public static final String TRAC_CONFIG_UNIT = "config/trac-unit.yaml";
     public static final String TRAC_GW_CONFIG_UNIT = "config/trac-gw-unit.yaml";
@@ -53,6 +61,8 @@ public class RestProxyTest {
             .startMeta()
             .startGateway()
             .build();
+
+    private static final Path tracRepoDir = ResourceHelpers.findTracProjectRoot();
 
     @Test
     void platformInfo() throws Exception {
@@ -107,6 +117,71 @@ public class RestProxyTest {
         Assertions.assertTrue(acmeTenant.isPresent());
 
         System.out.println("List tenants found the testing tenant: " + acmeTenant.get().getDescription());
+    }
+
+    @Test
+    void createSearchAndGet() throws Exception {
+
+        var client = new Http1Client(HttpScheme.HTTP, LOCALHOST, TEST_GW_PORT);
+        var commonHeaders = Map.<CharSequence, Object>ofEntries(
+                Map.entry(HttpHeaderNames.CONTENT_TYPE, "application/json"),
+                Map.entry(HttpHeaderNames.ACCEPT, "application/json"));
+
+        // Create a new FLOW object
+
+        var createMethod = "/trac-meta/api/v1/ACME_CORP/create-object";
+        var createJson = "examples/rest_calls/create_flow.json";
+
+        var createBody = Files.readAllBytes(tracRepoDir.resolve(createJson));
+        var createCall = client.postRequest(createMethod, commonHeaders, Unpooled.wrappedBuffer(createBody));
+        createCall.await(TEST_TIMEOUT);
+
+        var createResponse = createCall.getNow();
+        Assertions.assertEquals(HttpResponseStatus.OK, createResponse.status());
+
+        var objectId = parseJson(createResponse.content(), TagHeader.class);
+
+        // Search for FLOW objects matching a set of criteria
+
+        var searchMethod = "/trac-meta/api/v1/ACME_CORP/search";
+        var searchJson = "examples/rest_calls/search.json";
+
+        var searchBody = Files.readAllBytes(tracRepoDir.resolve(searchJson));
+        var searchCall = client.postRequest(searchMethod, commonHeaders, Unpooled.wrappedBuffer(searchBody));
+        searchCall.await(TEST_TIMEOUT);
+
+        var searchResponse = searchCall.getNow();
+        Assertions.assertEquals(HttpResponseStatus.OK, searchResponse.status());
+
+        var results = parseJson(searchResponse.content(), MetadataSearchResponse.class);
+        var expectedResult = results.getSearchResultList().stream()
+                .filter(record -> record.getHeader().equals(objectId))
+                .findFirst();
+
+        Assertions.assertTrue(results.getSearchResultCount() > 0);
+        Assertions.assertTrue(expectedResult.isPresent());
+
+        // Get the FLOW object with its full definition
+
+        var readMethod = "/trac-meta/api/v1/ACME_CORP/read-object";
+        var selector = selectorForTag(objectId);
+
+        var readBody = JsonFormat.printer().print(selector).getBytes(StandardCharsets.UTF_8);
+        var readCall = client.postRequest(readMethod, commonHeaders, Unpooled.wrappedBuffer(readBody));
+        readCall.await(TEST_TIMEOUT);
+
+        var readResponse = readCall.getNow();
+        Assertions.assertEquals(HttpResponseStatus.OK, readResponse.status());
+
+        // Compare the definition after round-trip with the original that was saved
+
+        var tag = parseJson(readResponse.content(), Tag.class);
+        var definition = tag.getDefinition();
+
+        var originalRequest = Files.readAllBytes(tracRepoDir.resolve(createJson));
+        var original = parseJson(Unpooled.wrappedBuffer(originalRequest), MetadataWriteRequest.class).getDefinition();
+
+        Assertions.assertEquals(original, definition);
     }
 
     @SuppressWarnings("unchecked")
