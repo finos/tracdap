0298e384afdd7d611847d07575c867bd25677dd4
Martin Traverse <martin.traverse@accenture.com>
2023-06-15 17:43:19 +0100

    Improve error mapping in error mapping interceptor

--- tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/grpc/ErrorMappingInterceptor.java
+++ tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/grpc/ErrorMappingInterceptor.java
@@ -21,34 +21,45 @@ import io.grpc.*;
 public class ErrorMappingInterceptor implements ServerInterceptor {
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(
-            ServerCall<ReqT, RespT> call,
-            Metadata headers,
-            ServerCallHandler<ReqT, RespT> next
-    ) {
-        var loggingCall = new ErrorMappingServerCall<>(call);
-        return next.startCall(loggingCall, headers);
+            ServerCall<ReqT, RespT> call, Metadata headers,
+            ServerCallHandler<ReqT, RespT> next) {
+
+        var errorHandler = new ErrorMappingServerCall<>(call);
+        return next.startCall(errorHandler, headers);
     }
 
 
     private static class ErrorMappingServerCall<ReqT, RespT> extends ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT> {
+
         protected ErrorMappingServerCall(ServerCall<ReqT, RespT> delegate) {
             super(delegate);
         }
 
         @Override
         public void close(Status status, Metadata trailers) {
-            var reportedStatus = status;
 
-            if (!status.isOk()) {
-                var cause = status.getCause();
-                if (cause == null) {
-                    cause = status.asRuntimeException();
-                }
-
-                reportedStatus = GrpcErrorMapping.processErrorToStatus(cause);
+            // For an OK result, there is no need to apply error mapping
+            if (status.isOk()) {
+                delegate().close(status, trailers);
             }
 
-            super.close(reportedStatus, trailers);
+            // If the cause of the failure is known, use the cause for error mapping
+            // This will preserve the application stack trace
+            else if (status.getCause() != null) {
+
+                var mappedStatus  = GrpcErrorMapping.processErrorToStatus(status.getCause());
+                delegate().close(mappedStatus, trailers);
+            }
+
+            // For a failure with no cause, create an exception from the status object
+            // The application stack trace will not be preserved, because it is not present in the status object
+            // Typically this happens when the original exception is a gRPC status exception
+            // For better error reporting, application code should avoid raising gRPC status exceptions directly
+            else {
+
+                var mappedStatus = GrpcErrorMapping.processErrorToStatus(status.asRuntimeException());
+                delegate().close(mappedStatus, trailers);
+            }
         }
     }
 }
