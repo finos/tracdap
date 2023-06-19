e9e7616f7d14af68fc43617ac657c1e6bcefd409
Martin Traverse <martin.traverse@accenture.com>
2023-06-19 16:42:34 +0100

    Use a separate installer script to prepare the Azure CLI tools

--- .github/scripts/int-storage-azure-setup.sh
+++ .github/scripts/int-storage-azure-setup.sh
@@ -14,27 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Install CLI tools
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update
-apt-get upgrade -y
-apt-get install -y  ca-certificates curl apt-transport-https lsb-release gnupg
-
-mkdir -p /etc/apt/keyrings
-curl -sLS https://packages.microsoft.com/keys/microsoft.asc |
-    gpg --dearmor |
-    tee /etc/apt/keyrings/microsoft.gpg > /dev/null
-chmod go+r /etc/apt/keyrings/microsoft.gpg
-
-AZ_REPO=$(lsb_release -cs)
-echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" |
-    tee /etc/apt/sources.list.d/azure-cli.list
-
-apt-get update
-apt-get install azure-cli
-
 # List bucket contents to check the connection
 # (bucket may be empty, otherwise limit to first 10 objects)
 
