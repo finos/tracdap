9dd0fa1a1b0544c9666a9dfff2ad6ac98ba861d1
Martin Traverse <martin.traverse@accenture.com>
2023-08-09 11:33:29 +0100

    Skeleton classes for Azure blob storage

---
+++ tracdap-plugins/azure-storage/src/main/java/org/finos/tracdap/plugins/azure/storage/AzureBlobStorage.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2023 Accenture Global Solutions Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.finos.tracdap.plugins.azure.storage;
+
+import org.finos.tracdap.common.data.IDataContext;
+import org.finos.tracdap.common.data.IExecutionContext;
+import org.finos.tracdap.common.storage.CommonFileStorage;
+import org.finos.tracdap.common.storage.FileStat;
+
+import io.netty.channel.EventLoopGroup;
+import org.apache.arrow.memory.ArrowBuf;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Flow;
+
+
+public class AzureBlobStorage extends CommonFileStorage {
+
+    public AzureBlobStorage(String storageKey, Properties properties) {
+        super(BUCKET_SEMANTICS, storageKey, properties, new AzureStorageErrors(storageKey));
+    }
+
+    @Override
+    protected CompletionStage<Boolean> fsExists(String objectKey, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<Boolean> fsDirExists(String prefix, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<FileStat> fsGetFileInfo(String objectKey, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<FileStat> fsGetDirInfo(String prefix, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<List<FileStat>> fsListContents(String prefix, String startAfter, int maxKeys, boolean recursive, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<Void> fsCreateDir(String prefix, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<Void> fsDeleteFile(String objectKey, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<Void> fsDeleteDir(String directoryKey, IExecutionContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected CompletionStage<ArrowBuf> fsReadChunk(String objectKey, long offset, int size, IDataContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected Flow.Publisher<ArrowBuf> fsOpenInputStream(String objectKey, IDataContext ctx) {
+        return null;
+    }
+
+    @Override
+    protected Flow.Subscriber<ArrowBuf> fsOpenOutputStream(String objectKey, CompletableFuture<Long> signal, IDataContext ctx) {
+        return null;
+    }
+
+    @Override
+    public void start(EventLoopGroup eventLoopGroup) {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
