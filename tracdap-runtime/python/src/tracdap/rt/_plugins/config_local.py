5c50a528b2539caf5e90390b02345b62718aa3ea
Martin Traverse <martin.traverse@accenture.com>
2024-09-22 23:54:31 +0100

    Fix path resolution in the config manager on Windows

--- tracdap-runtime/python/src/tracdap/rt/_plugins/config_local.py
+++ tracdap-runtime/python/src/tracdap/rt/_plugins/config_local.py
@@ -28,14 +28,14 @@ class LocalConfigLoader(IConfigLoader):
         pass
 
     def has_config_file(self, config_url: str) -> bool:
-        if config_url.startswith("file://"):
-            config_url = config_url[7:]
+        if config_url.startswith("file:"):
+            config_url = config_url[5:]
         config_path = pathlib.Path(config_url).resolve()
         return config_path.exists() and config_path.is_file()
 
     def load_config_file(self, config_url: str) -> bytes:
-        if config_url.startswith("file://"):
-            config_url = config_url[7:]
+        if config_url.startswith("file:"):
+            config_url = config_url[5:]
         config_path = pathlib.Path(config_url).resolve()
         return config_path.read_bytes()
 
