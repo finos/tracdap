2285f1309301d243f04c1dafbba9461f62456331
Martin Traverse <martin.traverse@accenture.com>
2024-10-13 22:32:50 +0100

    Add bulk data import to examples running in CI

--- tracdap-runtime/python/test/tracdap_examples/test_tutorial.py
+++ tracdap-runtime/python/test/tracdap_examples/test_tutorial.py
@@ -139,3 +139,12 @@ class TutorialModelsTest(unittest.TestCase):
         sys_config = self.examples_root.joinpath("config/sys_config.yaml")
 
         launch.launch_job(job_config, sys_config, dev_mode=True)
+
+    def test_data_import(self):
+
+        from tutorial.data_import import BulkDataImport  # noqa
+
+        job_config = self.examples_root.joinpath("config/data_import.yaml")
+        sys_config = self.examples_root.joinpath("config/sys_config.yaml")
+
+        launch.launch_model(BulkDataImport, job_config, sys_config, dev_mode=True)
