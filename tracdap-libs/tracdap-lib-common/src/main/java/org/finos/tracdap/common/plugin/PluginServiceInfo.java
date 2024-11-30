22e8cb9c346e3755e39359ba0df65c9e86ee3599
Martin Traverse <martin@fintrac.co.uk>
2024-11-30 10:50:49 +0000

    Include both auth and logic providers as plugin types

--- tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/plugin/PluginServiceInfo.java
+++ tracdap-libs/tracdap-lib-common/src/main/java/org/finos/tracdap/common/plugin/PluginServiceInfo.java
@@ -37,8 +37,11 @@ public class PluginServiceInfo {
     /** Standard service type for secret-loading services **/
     public static final String SECRETS_SERVICE_TYPE = "SECRETS";
 
-    /** Standard service type for secret-loading services **/
+    /** Standard service type for generic authentication providers **/
     public static final String AUTH_PROVIDER_SERVICE_TYPE = "AUTH_PROVIDER";
+
+    /** Standard service type for TRAC login providers **/
+    public static final String LOGIN_PROVIDER_SERVICE_TYPE = "LOGIN_PROVIDER";
 
     /** Standard service type for file storage services **/
     public static final String FILE_STORAGE_SERVICE_TYPE = "FILE_STORAGE";
@@ -68,6 +71,7 @@ public class PluginServiceInfo {
             Map.entry("org.finos.tracdap.common.config.IConfigLoader", CONFIG_SERVICE_TYPE),
             Map.entry("org.finos.tracdap.common.config.ISecretLoader", SECRETS_SERVICE_TYPE),
             Map.entry("org.finos.tracdap.common.auth.provider.IAuthProvider", AUTH_PROVIDER_SERVICE_TYPE),
+            Map.entry("org.finos.tracdap.common.auth.login.ILoginProvider", LOGIN_PROVIDER_SERVICE_TYPE),
             Map.entry("org.finos.tracdap.common.codec.ICodec", FORMAT_SERVICE_TYPE),
             Map.entry("org.finos.tracdap.common.storage.IFileStorage", FILE_STORAGE_SERVICE_TYPE),
             Map.entry("org.finos.tracdap.common.storage.IDataStorage", DATA_STORAGE_SERVICE_TYPE),
