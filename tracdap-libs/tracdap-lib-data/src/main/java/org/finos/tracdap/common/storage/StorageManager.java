60cfee55155280f241ff9dc3df19badbffe03927
Martin Traverse <martin@fintrac.co.uk>
2025-03-21 23:23:43 +0000

    Update storage manager for dynamic storage

--- tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/storage/StorageManager.java
+++ tracdap-libs/tracdap-lib-data/src/main/java/org/finos/tracdap/common/storage/StorageManager.java
@@ -17,12 +17,14 @@
 
 package org.finos.tracdap.common.storage;
 
+import org.finos.tracdap.common.config.ConfigHelpers;
 import org.finos.tracdap.common.config.ConfigManager;
-import org.finos.tracdap.config.StorageConfig;
 import org.finos.tracdap.common.codec.ICodecManager;
 import org.finos.tracdap.common.exception.EStartup;
 import org.finos.tracdap.common.exception.EStorageConfig;
 import org.finos.tracdap.common.plugin.IPluginManager;
+import org.finos.tracdap.config.PluginConfig;
+import org.finos.tracdap.metadata.ResourceDefinition;
 
 import io.netty.channel.EventLoopGroup;
 
@@ -30,6 +32,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 public class StorageManager implements IStorageManager, AutoCloseable {
@@ -38,111 +41,159 @@ public class StorageManager implements IStorageManager, AutoCloseable {
 
     private final IPluginManager plugins;
     private final ConfigManager configManager;
+    private final ICodecManager formats;
+    private final EventLoopGroup eventLoopGroup;
+
     private final Map<String, StorageBackend> storage;
 
-    public StorageManager(IPluginManager plugins, ConfigManager configManager) {
+    public StorageManager(
+            IPluginManager plugins, ConfigManager configManager,
+            ICodecManager formats, EventLoopGroup eventLoopGroup) {
+
         this.plugins = plugins;
         this.configManager = configManager;
-        this.storage = new HashMap<>();
+        this.formats = formats;
+        this.eventLoopGroup = eventLoopGroup;
+
+        this.storage = new ConcurrentHashMap<>();
     }
 
-    public void initStorage(StorageConfig storageConfig, ICodecManager formats, EventLoopGroup eventLoopGroup) {
+    public void addStorage(String storageKey, ResourceDefinition resourceConfig) {
+        addStorage(storageKey, ConfigHelpers.resourceToPluginConfig(resourceConfig));
+    }
 
-        log.info("Configuring storage...");
+    public void addStorage(String storageKey, PluginConfig resourceConfig) {
 
-        for (var bucket: storageConfig.getBucketsMap().entrySet()) {
+        log.info("Add storage: [{}] (protocol: {})", storageKey, resourceConfig.getProtocol());
 
-            var bucketKey = bucket.getKey();
-            var bucketConfig = bucket.getValue();
-            var backend = new StorageBackend();
+        var storageBackend = createStorageBackend(storageKey, resourceConfig);
+        storage.put(storageKey, storageBackend);
+    }
 
-            bucketConfig = bucketConfig.toBuilder()
-                    .putProperties(PROP_STORAGE_KEY, bucketKey)
-                    .build();
+    public void updateStorage(String storageKey, ResourceDefinition resourceConfig) {
+        updateStorage(storageKey, ConfigHelpers.resourceToPluginConfig(resourceConfig));
+    }
 
-            log.info("Attach storage: [{}] (protocol: {})", bucketKey, bucketConfig.getProtocol());
+    public void updateStorage(String storageKey, PluginConfig resourceConfig) {
 
-            if (plugins.isServiceAvailable(IFileStorage.class, bucketConfig.getProtocol())) {
+        log.info("Update storage: [{}] (protocol: {})", storageKey, resourceConfig.getProtocol());
 
-                var fileInstance = plugins.createService(IFileStorage.class, bucketConfig, configManager);
-                fileInstance.start(eventLoopGroup);
+        var storageBackend = createStorageBackend(storageKey, resourceConfig);
+        var previousBackend = storage.put(storageKey, storageBackend);
 
-                backend.fileInstances.add(fileInstance);
-            }
-
-            if (plugins.isServiceAvailable(IDataStorage.class, bucketConfig.getProtocol())) {
-
-                var dataInstance = plugins.createService(IDataStorage.class, bucketConfig, configManager);
-                dataInstance.start(eventLoopGroup);
-
-                backend.dataInstances.add(dataInstance);
-            }
-            else if (!backend.fileInstances.isEmpty()) {
-
-                log.info("Using flat data storage (datasets will be saved as files)");
-
-                for (var fileInstance : backend.fileInstances) {
-
-                    var dataInstance = new CommonDataStorage(bucketConfig, fileInstance, formats);
-                    dataInstance.start(eventLoopGroup);
-
-                    backend.dataInstances.add(dataInstance);
-                }
-            }
-
-            if (backend.fileInstances.isEmpty() && backend.dataInstances.isEmpty()) {
-
-                var message = String.format("No plugin found to support storage protocol [%s]", bucketConfig.getProtocol());
-                var error = new EStartup(message);
-
-                log.error(message, error);
-                throw error;
-            }
-
-            storage.put(bucketKey, backend);
+        if (previousBackend != null) {
+            stopStorageBackend(storageKey, previousBackend);
         }
+    }
+
+    public void removeStorage(String storageKey) {
+
+        log.info("Remove storage: [{}]", storageKey);
+
+        var previousBackend = storage.remove(storageKey);
+
+        if (previousBackend != null)
+            stopStorageBackend(storageKey, previousBackend);
+    }
+
+    public List<String> listStorageKeys() {
+        return List.copyOf(storage.keySet());
     }
 
     @Override
     public void close() {
 
-        for (var storage : this.storage.entrySet()) {
+        for (var i = this.storage.entrySet().iterator(); i.hasNext(); i.remove()) {
 
+            var storage = i.next();
+            var storageKey = storage.getKey();
             var backend = storage.getValue();
 
-            log.info("Detach storage: [{}]", storage.getKey());
+            log.info("Close storage: [{}]", storageKey);
 
-            // For errors during shutdown, log the error and continue shutting down other instances
-            // Don't propagate the error, that can only prevent shutdown being called for other resources
+            stopStorageBackend(storageKey, backend);
+        }
+    }
 
-            for (var dataImpl : backend.dataInstances) {
+    private StorageBackend createStorageBackend(String storageKey, PluginConfig storageConfig) {
 
-                try {
+        var storageConfigWithKey = storageConfig.toBuilder()
+                .putProperties(PROP_STORAGE_KEY, storageKey)
+                .build();
 
-                    dataImpl.close();
-                }
-                catch (Exception error) {
+        var backend = new StorageBackend();
 
-                    log.error(
+        if (plugins.isServiceAvailable(IFileStorage.class, storageConfig.getProtocol())) {
+
+            var fileInstance = plugins.createService(IFileStorage.class, storageConfigWithKey, configManager);
+            fileInstance.start(eventLoopGroup);
+
+            backend.fileInstances.add(fileInstance);
+        }
+
+        if (plugins.isServiceAvailable(IDataStorage.class, storageConfig.getProtocol())) {
+
+            var dataInstance = plugins.createService(IDataStorage.class, storageConfigWithKey, configManager);
+            dataInstance.start(eventLoopGroup);
+
+            backend.dataInstances.add(dataInstance);
+        }
+        else if (!backend.fileInstances.isEmpty()) {
+
+            log.info("Using flat data storage (datasets will be saved as files)");
+
+            for (var fileInstance : backend.fileInstances) {
+
+                var dataInstance = new CommonDataStorage(storageConfigWithKey, fileInstance, formats);
+                dataInstance.start(eventLoopGroup);
+
+                backend.dataInstances.add(dataInstance);
+            }
+        }
+
+        if (backend.fileInstances.isEmpty() && backend.dataInstances.isEmpty()) {
+
+            var message = String.format("No plugin found to support storage protocol [%s]", storageConfig.getProtocol());
+            var error = new EStartup(message);
+
+            log.error(message, error);
+            throw error;
+        }
+
+        return backend;
+    }
+
+    private void stopStorageBackend(String storageKey, StorageBackend backend) {
+
+        // For errors during shutdown, log the error and continue shutting down other instances
+        // Don't propagate the error, that can only prevent shutdown being called for other resources
+
+        for (var dataImpl : backend.dataInstances) {
+
+            try {
+
+                dataImpl.close();
+            }
+            catch (Exception error) {
+
+                log.error(
                         "There was an error shutting down the storage backend: [{}] {}",
-                        storage.getKey(), error.getMessage(), error);
-                }
+                        storageKey, error.getMessage(), error);
             }
+        }
 
-            for (var fileImpl : backend.fileInstances) {
+        for (var fileImpl : backend.fileInstances) {
 
-                try {
+            try {
 
-                    fileImpl.close();
-                }
-                catch (Exception error) {
-
-                    log.error(
-                            "There was an error shutting down the storage backend: [{}] {}",
-                            storage.getKey(), error.getMessage(), error);
-                }
+                fileImpl.close();
             }
+            catch (Exception error) {
 
+                log.error(
+                        "There was an error shutting down the storage backend: [{}] {}",
+                        storageKey, error.getMessage(), error);
+            }
         }
     }
 
