5fb76b05be1661b0e58e797c76ad365b44d5a97f
Martin Traverse <martin@fintrac.co.uk>
2025-05-18 18:48:38 +0100

    Allow inheriting cache ticket for JDBC

--- tracdap-libs/tracdap-lib-orch/src/main/java/org/finos/tracdap/common/cache/CacheTicket.java
+++ tracdap-libs/tracdap-lib-orch/src/main/java/org/finos/tracdap/common/cache/CacheTicket.java
@@ -22,10 +22,8 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 
-public final class CacheTicket implements AutoCloseable {
+public class CacheTicket implements AutoCloseable {
 
-    public static final int FIRST_REVISION = 0;
-    public static final Duration DEFAULT_DURATION = Duration.of(30, ChronoUnit.SECONDS);
     public static final Duration MAX_DURATION = Duration.of(5, ChronoUnit.MINUTES);
 
     private static final Instant NO_EXPIRY = Instant.MAX;
@@ -57,7 +55,7 @@ public final class CacheTicket implements AutoCloseable {
         return new CacheTicket(cache, key, revision, grantTime, grantTime.plus(grantDuration), false, false);
     }
 
-    private CacheTicket(
+    protected CacheTicket(
             IJobCache<?> cache,
             String key, int iteration,
             Instant grantTime, Instant expiry,
