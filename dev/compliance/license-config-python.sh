9371922d540d75ee0faf4954d0da4ce4d3d24156
Martin Traverse <martin@fintrac.co.uk>
2025-07-02 15:25:47 +0100

    Add missing license aliases in Python compliance config

--- dev/compliance/license-config-python.sh
+++ dev/compliance/license-config-python.sh
@@ -16,6 +16,8 @@
 ALLOWED_LICENSES="Apache Software License"
 ALLOWED_LICENSES="${ALLOWED_LICENSES};Apache 2.0"
 ALLOWED_LICENSES="${ALLOWED_LICENSES};Apache-2.0"
+ALLOWED_LICENSES="${ALLOWED_LICENSES};Apache License 2.0"
+ALLOWED_LICENSES="${ALLOWED_LICENSES};MIT"
 ALLOWED_LICENSES="${ALLOWED_LICENSES};MIT License"
 ALLOWED_LICENSES="${ALLOWED_LICENSES};MIT No Attribution License (MIT-0)"
 ALLOWED_LICENSES="${ALLOWED_LICENSES};BSD License"
@@ -27,6 +29,9 @@ ALLOWED_LICENSES="${ALLOWED_LICENSES};The Unlicense (Unlicense)"
 
 # License for greenlet is specified this way
 ALLOWED_LICENSES="${ALLOWED_LICENSES};MIT AND Python-2.0"
+
+# License for cryptography package is specified this way
+ALLOWED_LICENSES="${ALLOWED_LICENSES};Apache-2.0 OR BSD-3-Clause"
 
 # The "certifi" package is a dependency of Python Safety, licensed under MPL 2.0
 # It is OK to use since the compliance tools are not distributed
