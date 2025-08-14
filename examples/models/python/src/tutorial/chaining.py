aa3c84722c0ac3cd5df94376cae7a00092cc5f3b
Martin Traverse <martin@fintrac.co.uk>
2025-08-14 17:53:59 +0100

    Update chaining examples

--- examples/models/python/src/tutorial/chaining.py
+++ examples/models/python/src/tutorial/chaining.py
@@ -12,14 +12,6 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 
 import tracdap.rt.api as trac
 import typing as tp
@@ -37,27 +29,27 @@ class CustomerDataFilter(trac.TracModel):
     def define_inputs(self) -> tp.Dict[str, trac.ModelInputSchema]:
 
         customer_loans_schema = trac.load_schema(schemas, "customer_loans.csv")
-        unfiltered_loans = trac.define_input(customer_loans_schema, label="Unfiltered loans data")
+        customer_loans = trac.define_input(customer_loans_schema, label="Unfiltered loans data")
 
-        return { "unfiltered_loans": unfiltered_loans }
+        return { "customer_loans": customer_loans }
 
     def define_outputs(self) -> tp.Dict[str, trac.ModelOutputSchema]:
 
         customer_loans_schema = trac.load_schema(schemas, "customer_loans.csv")
-        customer_loans = trac.define_output(customer_loans_schema, label="Filtered loans data")
+        filtered_loans = trac.define_output(customer_loans_schema, label="Filtered loans data")
 
-        return { "customer_loans": customer_loans }
+        return { "filtered_loans": filtered_loans }
 
     def run_model(self, ctx: trac.TracContext):
 
-        unfilter_loans = ctx.get_pandas_table("unfiltered_loans")
+        customer_loans = ctx.get_pandas_table("customer_loans")
         filter_region = ctx.get_parameter("filter_region")
 
-        customer_loans = unfilter_loans[unfilter_loans["region"] != filter_region]
+        filtered_loans = customer_loans[customer_loans["region"] != filter_region]
 
-        ctx.put_pandas_table("customer_loans", customer_loans)
+        ctx.put_pandas_table("filtered_loans", filtered_loans)
 
 
 if __name__ == "__main__":
     import tracdap.rt.launch as launch
-    launch.launch_job( "config/chaining.yaml", "config/sys_config.yaml")
+    launch.launch_job("config/chaining.yaml", "config/sys_config.yaml")
