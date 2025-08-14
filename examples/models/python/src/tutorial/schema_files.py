e1a8692212b15d3af75b62d96ec8e3dd7dc0250a
Martin Traverse <martin@fintrac.co.uk>
2025-08-14 12:22:48 +0100

    Use latest conventions for using data and schema examples

--- examples/models/python/src/tutorial/schema_files.py
+++ examples/models/python/src/tutorial/schema_files.py
@@ -22,14 +22,6 @@ import tutorial.schemas as schemas
 
 class SchemaFilesModel(trac.TracModel):
 
-    def define_attributes(self) -> tp.List[trac.TagUpdate]:
-
-        return trac.define_attributes(
-            trac.A("model_description", "A example model, for testing purposes"),
-            trac.A("business_segment", "retail_products", categorical=True),
-            trac.A("classifiers", ["loans", "uk", "examples"], attr_type=trac.STRING)
-        )
-
     def define_parameters(self) -> tp.Dict[str, trac.ModelParameter]:
 
         return trac.define_parameters(
@@ -46,15 +38,17 @@ class SchemaFilesModel(trac.TracModel):
 
     def define_inputs(self) -> tp.Dict[str, trac.ModelInputSchema]:
 
-        customer_loans = trac.load_schema(schemas, "customer_loans.csv")
+        customer_loans_schema = trac.load_schema(schemas, "customer_loans.csv")
+        customer_loans = trac.define_input(customer_loans_schema, label="Customer loans data")
 
-        return {"customer_loans": trac.ModelInputSchema(customer_loans)}
+        return {"customer_loans": customer_loans}
 
     def define_outputs(self) -> tp.Dict[str, trac.ModelOutputSchema]:
 
-        profit_by_region = trac.load_schema(schemas, "profit_by_region.csv")
+        profit_by_region_schema = trac.load_schema(schemas, "profit_by_region.csv")
+        profit_by_region = trac.define_output(profit_by_region_schema, label="Profit by region")
 
-        return {"profit_by_region": trac.ModelOutputSchema(profit_by_region)}
+        return {"profit_by_region": profit_by_region}
 
     def run_model(self, ctx: trac.TracContext):
 
--- examples/models/python/src/tutorial/using_data.py
+++ examples/models/python/src/tutorial/using_data.py
@@ -72,7 +72,8 @@ class UsingDataModel(trac.TracModel):
             trac.F("loan_amount", trac.DECIMAL, label="Principal loan amount"),
             trac.F("total_pymnt", trac.DECIMAL, label="Total amount repaid"),
             trac.F("region", trac.STRING, label="Customer home region", categorical=True),
-            trac.F("loan_condition_cat", trac.INTEGER, label="Loan condition category"))
+            trac.F("loan_condition_cat", trac.INTEGER, label="Loan condition category"),
+            label="Customer loans data")
 
         return {"customer_loans": customer_loans}
 
@@ -80,7 +81,8 @@ class UsingDataModel(trac.TracModel):
 
         profit_by_region = trac.define_output_table(
             trac.F("region", trac.STRING, label="Customer home region", categorical=True),
-            trac.F("gross_profit", trac.DECIMAL, label="Total gross profit"))
+            trac.F("gross_profit", trac.DECIMAL, label="Total gross profit"),
+            label="Profit by region")
 
         return {"profit_by_region": profit_by_region}
 
