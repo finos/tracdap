e695edb6b89f99d428fd4705d23103fe150bfa05
Martin Traverse <martin@fintrac.co.uk>
2025-09-05 11:40:42 +0100

    Add external systems to resource manager

--- tracdap-runtime/python/src/tracdap/rt/_impl/core/resources.py
+++ tracdap-runtime/python/src/tracdap/rt/_impl/core/resources.py
@@ -13,7 +13,13 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import typing as _tp
+
+import tracdap.rt.metadata as _meta
 import tracdap.rt.config as _config
+import tracdap.rt.exceptions as _ex
+import tracdap.rt.ext.plugins as _plugins
+import tracdap.rt.ext.external as _external
 import tracdap.rt._impl.core.models as _models
 import tracdap.rt._impl.core.repos as _repos
 import tracdap.rt._impl.core.storage as _storage
@@ -33,9 +39,33 @@ class ResourceManager:
             model_loader: _models.ModelLoader):
 
         self.__log = _log.logger_for_object(self)
+        self.__sys_config = sys_config
         self.__storage = storage_manager
         self.__repositories = repository_manager
         self.__models = model_loader
+        self.__external_systems = self._load_external_systems(sys_config)
+
+    @classmethod
+    def _load_external_systems(cls, sys_config: _config.RuntimeConfig) -> _tp.Dict[str, _external.IExternalSystem]:
+
+        external_systems = dict()
+
+        for resource_name, resource_def in sys_config.resources.items():
+            if resource_def.resourceType == _meta.ResourceType.EXTERNAL_SYSTEM:
+
+                config = _config.PluginConfig(
+                    protocol=resource_def.protocol,
+                    publicProperties=resource_def.publicProperties,
+                    properties=resource_def.properties,
+                    secrets=resource_def.secrets)
+
+                system = _plugins.PluginManager.load_plugin(_external.IExternalSystem, config)
+                external_systems[resource_name] = system
+
+        return external_systems
+
+    def get_resource_definition(self, resource_key: str) -> _tp.Optional[_meta.ResourceDefinition]:
+        return self.__sys_config.resources.get(resource_key)
 
     def get_storage(self) -> _storage.StorageManager:
         return self.__storage
@@ -45,3 +75,12 @@ class ResourceManager:
 
     def get_models(self):
         return self.__models
+
+    def get_external_system(self, system_name: str) -> _external.IExternalSystem:
+
+        system = self.__external_systems.get(system_name)
+
+        if system is None:
+            raise _ex.ETracInternal(f"External system [{system_name}] is not available")
+
+        return system
