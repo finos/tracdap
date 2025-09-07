1d9973a87878cf812ba81002254015451a1ca2e7
Martin Traverse <martin@fintrac.co.uk>
2025-09-07 16:59:30 +0100

    Flesh out http plugin (init not done yet)

--- tracdap-runtime/python-ext/src/tracdap/ext/http/http_plugin.py
+++ tracdap-runtime/python-ext/src/tracdap/ext/http/http_plugin.py
@@ -13,28 +13,108 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
-import typing as _tp
+import http.client as _hc
 
+try:
+    import urllib3 as _ul3
+except ModuleNotFoundError:
+    _ul3 = None
+
+import tracdap.rt.exceptions as _ex
 import tracdap.rt.ext.external as _external
 import tracdap.rt.ext.plugins as _plugins
 
 
 class HttpPlugin(_external.IExternalSystem):
 
-    def __init__(self, properties: _tp.Dict[str, str]):
+    __HC_CLIENT_ARGS = ["timeout"]
+    __UL3_CLIENT_ARGS = ["timeout"]
+
+    def __init__(self, properties: dict[str, str]):
+
         self.__properties = properties
 
-    def supported_types(self) -> _tp.List[type]:
-        pass
+        self.__resource_name = "http_resource"
+        self.__host = "github.com"
+        self.__port = None
+        self.__tls = True
 
-    def supported_args(self) -> _tp.Optional[_tp.Dict[str, type]]:
-        pass
+    def supported_types(self) -> list[type]:
 
-    def create_client(self, client_type: type, **client_args) -> _tp.Any:
-        pass
+        supported_types: list[type] = list()
 
-    def close_client(self, client: _tp.Any):
-        pass
+        if _hc:
+            supported_types.append(_hc.HTTPConnection)
+            supported_types.append(_hc.HTTPSConnection)
+
+        if _ul3:
+            supported_types.append(_ul3.HTTPConnectionPool)
+            supported_types.append(_ul3.HTTPSConnectionPool)
+
+        return supported_types
+
+    def supported_args(self) -> dict[str, type] | None:
+
+        return {
+            "timeout": float
+        }
+
+    def create_client(self, client_type: type, **client_args) -> object:
+
+        if client_type == _hc.HTTPConnection:
+            if self.__tls:
+                return self._create_client_hc_https(**client_args)
+            else:
+                return self._create_client_hc_http(**client_args)
+
+        if client_type == _hc.HTTPSConnection:
+            if self.__tls:
+                return self._create_client_hc_https(**client_args)
+            else:
+                raise self._error_tls_not_enabled()
+
+        if _ul3 and client_type == _ul3.HTTPConnectionPool:
+            if self.__tls:
+                return self._create_client_ul3_https(**client_args)
+            else:
+                return self._create_client_ul3_http(**client_args)
+
+        if _ul3 and client_type == _ul3.HTTPSConnectionPool:
+            if self.__tls:
+                return self._create_client_ul3_https(**client_args)
+            else:
+                raise self._error_tls_not_enabled()
+
+        raise _ex.EPluginNotAvailable(f"Client type [{client_type.__qualname__}] is not available in {self.__class__.__name__}")
+
+    def _create_client_hc_http(self, **client_args) -> _hc.HTTPConnection:
+        hc_args = self._filter_args_hc(**client_args)
+        return _hc.HTTPSConnection(self.__host, self.__port, **hc_args)
+
+    def _create_client_hc_https(self, **client_args) -> _hc.HTTPSConnection:
+        hc_args = self._filter_args_hc(**client_args)
+        return _hc.HTTPSConnection(self.__host, self.__port, **hc_args)
+
+    def _filter_args_hc(self, **client_args):
+        return {k: v for k, v in client_args.items() if k in self.__HC_CLIENT_ARGS}
+
+    def _create_client_ul3_http(self, **client_args) -> _ul3.HTTPConnectionPool:
+        ul3_args = self._filter_args_ul3(**client_args)
+        return _ul3.HTTPSConnectionPool(self.__host, self.__port, **ul3_args)
+
+    def _create_client_ul3_https(self, **client_args) -> _ul3.HTTPSConnectionPool:
+        ul3_args = self._filter_args_ul3(**client_args)
+        return _ul3.HTTPSConnectionPool(self.__host, self.__port, **ul3_args)
+
+    def _filter_args_ul3(self, **client_args):
+        return {k: v for k, v in client_args.items() if k in self.__HC_CLIENT_ARGS}
+
+    def _error_tls_not_enabled(self):
+        detail = f"The resource [{self.__resource_name }] does not have TLS enabled"
+        return _ex.ERuntimeValidation(f"Cannot create HTTPS connection: {detail}")
+
+    def close_client(self, client: object):
+        client.close()  # noqa
 
 
 _plugins.PluginManager.register_plugin(
