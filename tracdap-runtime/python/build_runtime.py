25a69792cf08f324e9dc3a0af174a07068129d3f
Martin Traverse <martin@fintrac.co.uk>
2025-09-13 12:54:01 +0100

    Resolve customer dirs from args in build_runtime.py

--- tracdap-runtime/python/build_runtime.py
+++ tracdap-runtime/python/build_runtime.py
@@ -432,15 +432,15 @@ def main():
     global BUILD_PATH, WORK_PATH, DIST_PATH
 
     if args.build_dir is not None:
-        BUILD_PATH = args.build_dir
+        BUILD_PATH = args.build_dir.resolve()
         WORK_PATH = BUILD_PATH.joinpath("work")
         DIST_PATH = BUILD_PATH.joinpath("dist")
 
     if args.work_dir is not None:
-        WORK_PATH = args.work_dir
+        WORK_PATH = args.work_dir.resolve()
 
     if args.dist_dir is not None:
-        DIST_PATH = args.dist_dir
+        DIST_PATH = args.dist_dir.resolve()
 
     if any(map(lambda target: target in args.target, ["clean", "dist", "ext"])):
         reset_build_dir()
