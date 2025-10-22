3e36e8b45bc483f38a21ef4c858fae0bed5b25b0
Martin Traverse <martin@fintrac.co.uk>
2025-10-22 17:20:18 +0100

    Fix Polars example model to work with Polars 1.34

--- examples/models/python/src/tutorial/using_polars.py
+++ examples/models/python/src/tutorial/using_polars.py
@@ -33,7 +33,7 @@ def calculate_profit_by_region_polars(
     # Build a weighting vector, use default_weighting for bad loans and 1.0 for good loans
     condition_weighting = customer_loans \
             .get_column("loan_condition_cat") \
-            .map_elements(lambda c: default_weighting if c > 0 else 1.0, polars.Decimal)
+            .map_elements(lambda c: default_weighting if c > 0 else 1.0, polars.Decimal(38, 10))
 
     # Use lazy processing
     customer_loans = customer_loans.lazy() \
