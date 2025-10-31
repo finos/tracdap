84390ce4728aebc9e362f0fc80dbcb84cd831ed1
Martin Traverse <martin@fintrac.co.uk>
2025-10-31 22:49:00 +0000

    Move git tests into ext package

---
+++ tracdap-runtime/python-ext/test/tracdap_ext_test/git/int_git.py
@@ -0,0 +1,210 @@
+#  Licensed to the Fintech Open Source Foundation (FINOS) under one or
+#  more contributor license agreements. See the NOTICE file distributed
+#  with this work for additional information regarding copyright ownership.
+#  FINOS licenses this file to you under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with the
+#  License. You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import tempfile
+import unittest
+import pathlib
+import subprocess as sp
+
+import tracdap.rt.metadata as meta
+import tracdap.rt.config as config
+import tracdap.rt.exceptions as ex
+import tracdap.rt._impl.core.plugins as plugins  # noqa
+import tracdap.rt._impl.core.logging as log  # noqa
+import tracdap.rt._impl.core.models as models  # noqa
+import tracdap.rt._impl.static_api as api_hook  # noqa
+import tracdap.rt._impl.core.repos as repos  # noqa
+import tracdap.rt._impl.core.util as util  # noqa
+
+
+class ModelRepositoriesTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        plugins.PluginManagerImpl.register_core_plugins()
+        api_hook.StaticApiImpl.register_impl()
+        log.configure_logging()
+
+    def setUp(self) -> None:
+        self.test_scope = f"{self.__class__.__name__}.{self._testMethodName}"
+
+        repo_url_proc = sp.run(["git", "config", "--get", "remote.origin.url"], stdout=sp.PIPE)
+        commit_hash_proc = sp.run(["git", "rev-parse", "HEAD"], stdout=sp.PIPE)
+
+        if repo_url_proc.returncode != 0 or commit_hash_proc.returncode != 0:
+            raise RuntimeError("Could not discover details of the current git repo")
+
+        self.repo_url = repo_url_proc.stdout.decode('utf-8').strip()
+        self.commit_hash = commit_hash_proc.stdout.decode('utf-8').strip()
+
+        self.scratch_dir = pathlib.Path(tempfile.mkdtemp())
+
+    def tearDown(self) -> None:
+
+        util.try_clean_dir(self.scratch_dir, remove=True)
+
+    def test_checkout_git_native(self):
+
+        checkout_key = "git_native_short"
+        self._test_checkout_git_native(checkout_key)
+
+    # This test has broken in CI without any changes made to the codebase
+    # Could be related to the build image but might indicate a more fundamental problem on recent Windows OS
+    # TODO: Investigation required if this issue persists
+    @unittest.skipIf(util.is_windows(), "Native long-path checkout failing on Windows, 5th March 2025")
+    def test_checkout_git_native_long_path(self):
+
+        # Using native Git on Windows, the checkout dir cannot exceed the Windows max path length
+        # Specifically, checkout_dir/.git/config must be reachable, to enable the long paths setting
+        # However, paths for the items inside the repo can (and should) exceed the max path length
+
+        prefix_length = len(str(self.scratch_dir.joinpath("git_test", "git_native_long_")))
+        checkout_key = "git_native_long_" + "A" * (240 - prefix_length)
+
+        self._test_checkout_git_native(checkout_key)
+
+
+    def _test_checkout_git_native(self, checkout_key):
+
+        sys_config = config.RuntimeConfig()
+        sys_config.resources["git_test"] = meta.ResourceDefinition(
+            resourceType=meta.ResourceType.MODEL_REPOSITORY,
+            protocol="git",
+            properties={
+                "repoUrl": "https://github.com/finos/tracdap",
+                "nativeGit": "true"})
+
+        model_def = meta.ModelDefinition(
+            language="python",
+            repository="git_test",
+            packageGroup="finos",
+            package="tracdap",
+            version="v0.6.0",
+            entryPoint="tutorial.hello_world.HelloWorldModel",
+            path="examples/models/python/src"
+        )
+
+        repo_mgr = repos.RepositoryManager(sys_config)
+        repo = repo_mgr.get_repository("git_test")
+
+        checkout_dir = self.scratch_dir.joinpath(model_def.repository, checkout_key)
+        safe_checkout_dir = util.windows_unc_path(checkout_dir)
+        safe_checkout_dir.mkdir(mode=0o750, parents=True, exist_ok=False)
+
+        package_dir = repo.do_checkout(model_def, checkout_dir)
+        safe_package_dir = util.windows_unc_path(package_dir)
+
+        self.assertTrue(safe_package_dir.joinpath("tutorial/hello_world.py").exists())
+
+    def test_checkout_git_native_failure(self):
+
+        sys_config = config.RuntimeConfig()
+        sys_config.resources["git_test"] = meta.ResourceDefinition(
+            resourceType=meta.ResourceType.MODEL_REPOSITORY,
+            protocol="git",
+            properties={
+                "repoUrl": "https://github.noexist/finos/tracdap",
+                "nativeGit": "true"})
+
+        model_def = meta.ModelDefinition(
+            language="python",
+            repository="git_test",
+            packageGroup="finos",
+            package="tracdap",
+            version="v0.6.0",
+            entryPoint="tutorial.hello_world.HelloWorldModel",
+            path="examples/models/python/src"
+        )
+
+        repo_mgr = repos.RepositoryManager(sys_config)
+        repo = repo_mgr.get_repository("git_test")
+
+        checkout_dir = self.scratch_dir.joinpath(model_def.repository, "git_native_failure")
+        safe_checkout_dir = util.windows_unc_path(checkout_dir)
+        safe_checkout_dir.mkdir(mode=0o750, parents=True, exist_ok=False)
+
+        self.assertRaises(ex.EModelRepo, lambda: repo.do_checkout(model_def, checkout_dir))
+
+    def test_checkout_git_python(self):
+
+        checkout_key = "git_python_short"
+        self._test_checkout_git_python(checkout_key)
+
+    def test_checkout_git_python_long_path(self):
+
+        # Using Python Git on Windows, the checkout dir path is allowed to exceed Windows MAX_PATH
+        # However no individual path segment can be longer than 255 chars
+
+        checkout_key = "git_python_long_" + "A" * 230
+        self._test_checkout_git_python(checkout_key)
+
+    def _test_checkout_git_python(self, checkout_key):
+
+        sys_config = config.RuntimeConfig()
+        sys_config.resources["git_test"] = meta.ResourceDefinition(
+            resourceType=meta.ResourceType.MODEL_REPOSITORY,
+            protocol="git",
+            properties={
+                "repoUrl": "https://github.com/finos/tracdap"})
+
+        model_def = meta.ModelDefinition(
+            language="python",
+            repository="git_test",
+            packageGroup="finos",
+            package="tracdap",
+            version="v0.6.0",
+            entryPoint="tutorial.hello_world.HelloWorldModel",
+            path="examples/models/python/src"
+        )
+
+        repo_mgr = repos.RepositoryManager(sys_config)
+        repo = repo_mgr.get_repository("git_test")
+
+        checkout_dir = self.scratch_dir.joinpath(model_def.repository, checkout_key)
+        safe_checkout_dir = util.windows_unc_path(checkout_dir)
+        safe_checkout_dir.mkdir(mode=0o750, parents=True, exist_ok=False)
+
+        package_dir = repo.do_checkout(model_def, checkout_dir)
+        safe_package_dir = util.windows_unc_path(package_dir)
+
+        self.assertTrue(safe_package_dir.joinpath("tutorial/hello_world.py").exists())
+
+    def test_checkout_git_python_failure(self):
+
+        sys_config = config.RuntimeConfig()
+        sys_config.resources["git_test"] = meta.ResourceDefinition(
+            resourceType=meta.ResourceType.MODEL_REPOSITORY,
+            protocol="git",
+            properties={
+                "repoUrl": "https://github.noexist/finos/tracdap"})
+
+        model_def = meta.ModelDefinition(
+            language="python",
+            repository="git_test",
+            packageGroup="finos",
+            package="tracdap",
+            version="v0.6.0",
+            entryPoint="tutorial.hello_world.HelloWorldModel",
+            path="examples/models/python/src"
+        )
+
+        repo_mgr = repos.RepositoryManager(sys_config)
+        repo = repo_mgr.get_repository("git_test")
+
+        checkout_dir = self.scratch_dir.joinpath(model_def.repository, "git_python_failure")
+        safe_checkout_dir = util.windows_unc_path(checkout_dir)
+        safe_checkout_dir.mkdir(mode=0o750, parents=True, exist_ok=False)
+
+        self.assertRaises(ex.EModelRepo, lambda: repo.do_checkout(model_def, checkout_dir))
